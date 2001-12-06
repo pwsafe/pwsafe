fd7c6db085e78f1cc1e94384a964072c3d3175e2
Jim Russell <jrussell@users.sourceforge.net>
2001-12-06 21:40:57 +0000

    Preliminary skeletal code for some new objects -- if we're only going to use MFC for the UI, then CString and CList have to go
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@16 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/PasswordDb.h
@@ -0,0 +1,38 @@
+// PasswordDb.h
+//-----------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "PasswordSafe.h"
+
+#include <vector>
+
+//-----------------------------------------------------------------------------
+class PasswordDb
+{
+public:
+   PasswordDb()
+   {}
+
+   ~PasswordDb()
+   {}
+
+   void Clear()
+   { m_db.clear(); }
+
+   void Add(const CItemData& a_item);
+   CItemData Item(size_t a_ix);
+   void Remove(size_t a_ix);
+   size_t Find(const string& a_key);
+   short Version();
+
+private:
+   vector<CItemData> m_db;
+   string m_defaultUsername;
+
+
+};
+
+//-----------------------------------------------------------------------------
+// Local variables:
+// mode: c++
+// End:
