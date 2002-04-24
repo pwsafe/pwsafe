e4688f4e33e81320f0ee9c5ebceb41fbb51b0092
Jim Russell <jrussell@users.sourceforge.net>
2002-04-24 18:54:43 +0000

    code cleanup
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@29 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/BlowFish.h
+++ pwsafe/pwsafe/BlowFish.h
@@ -1,8 +1,19 @@
+// BlowFish.h
 //-----------------------------------------------------------------------------
 
-#include "util.h"
+//#include "Util.h"
+
+#define bf_N 16
+
+extern unsigned long bf_S[4][256];
+extern unsigned long bf_P[bf_N + 2];
+extern unsigned long tempbf_S[4][256];
+extern unsigned long tempbf_P[bf_N + 2];
 
 #define MAXKEYBYTES 56
+
+typedef unsigned char block[8];
+
 
 union aword
 {
@@ -20,12 +31,12 @@ union aword
 class BlowFish
 {
 public:
-   BlowFish(unsigned char *key,int keylen);
-   void Encrypt(block in,block out);
-   void Decrypt(block in,block out);
+   BlowFish(unsigned char* key, int keylen);
+   void Encrypt(block in, block out);
+   void Decrypt(block in, block out);
 private:
-   void Blowfish_encipher(unsigned long *xl, unsigned long *xr);
-   void Blowfish_decipher(unsigned long *xl, unsigned long *xr);
+   void Blowfish_encipher(unsigned long* xl, unsigned long* xr);
+   void Blowfish_decipher(unsigned long* xl, unsigned long* xr);
    void InitializeBlowfish(unsigned char key[], short keybytes);
 };
 //-----------------------------------------------------------------------------
