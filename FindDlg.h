131c1423d7cc2e48732b321748a7e4f1c524ec92
Rony Shapiro <ronys@users.sourceforge.net>
2003-04-24 17:56:57 +0000

    First implementation of Search functionality (RFE 639439, item #25 in old todo list)
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@69 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ FindDlg.h
@@ -0,0 +1,57 @@
+#if !defined(AFX_FINDDLG_H__D63D8AE1_8908_401A_8770_CBA6AC9F3D84__INCLUDED_)
+#define AFX_FINDDLG_H__D63D8AE1_8908_401A_8770_CBA6AC9F3D84__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// FindDlg.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CFindDlg dialog
+
+class CFindDlg : public CDialog
+{
+  // Construction
+ public:
+  static void Doit(CWnd* pParent = NULL); // implement Singleton pattern
+  ~CFindDlg();
+  // Dialog Data
+  //{{AFX_DATA(CFindDlg)
+  enum { IDD = IDD_FIND };
+  BOOL	m_cs_search;
+  CString	m_search_text;
+  CString	m_status;
+  //}}AFX_DATA
+
+
+  // Overrides
+  // ClassWizard generated virtual function overrides
+  //{{AFX_VIRTUAL(CFindDlg)
+ protected:
+  virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+  //}}AFX_VIRTUAL
+
+  // Implementation
+ protected:
+
+  // Generated message map functions
+  //{{AFX_MSG(CFindDlg)
+  afx_msg void OnFind();
+	afx_msg void OnClose();
+	//}}AFX_MSG
+  DECLARE_MESSAGE_MAP()
+ private:
+  CFindDlg(CWnd* pParent = NULL);   // standard constructor
+  static CFindDlg *self;
+  int *m_indices; // array of found items
+  int m_lastshown; // last index selected, -1 indicates no search done yet
+  int m_numFound; // number of items that matched, as returned by DboxMain::FindAll
+  CString m_last_search_text;
+  BOOL m_last_cs_search;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_FINDDLG_H__D63D8AE1_8908_401A_8770_CBA6AC9F3D84__INCLUDED_)
