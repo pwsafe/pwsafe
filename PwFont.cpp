645dfaecaba936fd71d1ad7f180576deba771ad0
gregg conklin <gconklin@users.sourceforge.net>
2003-04-30 02:42:31 +0000

    reduced resource leak when calling CreateFontIndirect to just one leaked item
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@102 1f79f812-37fb-46fe-a122-30589dd2bf55

--- PwFont.cpp
+++ PwFont.cpp
@@ -13,18 +13,27 @@ static char THIS_FILE[] = __FILE__;
 void
 SetPasswordFont(CWnd* pDlgItem)
 {
-	// Initialize a CFont object with the characteristics given
-	// in a LOGFONT structure.
-	LOGFONT lf;
-	memset(&lf, 0, sizeof(LOGFONT));          // clear out structure
-	lf.lfHeight = 14;                         // request a 14-pixel-height font
-	strcpy(lf.lfFaceName, "Courier");         // request a face name "Courier"
-	HFONT hfont = ::CreateFontIndirect(&lf);  // create the font
-	if (hfont != NULL) {
-	  // Convert the HFONT to CFont*.
-	  CFont* pfont = CFont::FromHandle(hfont);
-	  if (pfont != NULL)
-	    pDlgItem->SetFont(pfont);
+	// for now, this keeps the leak down to just one
+	static HFONT hfont		= NULL;
+
+	if ( hfont == NULL )
+	{
+		// Initialize a CFont object with the characteristics given
+		// in a LOGFONT structure.
+
+		LOGFONT lf;
+		memset(&lf, 0, sizeof(LOGFONT));		  // clear out structure
+		lf.lfHeight = 14;						  // request a 14-pixel-height font
+		strcpy(lf.lfFaceName, "Courier");		  // request a face name "Courier"
+		hfont = ::CreateFontIndirect(&lf);		  // create the font (must be deleted with ::DeleteObject()
+	}
+
+	if (hfont != NULL)
+	{
+		// Convert the HFONT to CFont*.
+		CFont* pfont = CFont::FromHandle(hfont);
+		if (pfont != NULL)
+			pDlgItem->SetFont(pfont);
 	}
 }
 
