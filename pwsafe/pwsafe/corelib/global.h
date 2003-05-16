3c307c8f638fb7048c4c81d64282ccdca5a2bff4
Rony Shapiro <ronys@users.sourceforge.net>
2003-05-16 09:05:51 +0000

    Moved version 1.1 from parent directory to here.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@146 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/corelib/global.h
@@ -0,0 +1,25 @@
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+//  Temporary global object, used to separate out pwsafe logic from
+//  MFC logic
+
+#include "MyString.h"
+#include "Util.h"
+
+class Global
+{
+public:
+	Global() {};
+	~Global() {};
+
+	CMyString m_passkey; // the main one, in memory?!? yikes {jpr}
+
+	// Following used to verify passkey against file's passkey
+	unsigned char m_randstuff[StuffSize];
+	unsigned char m_randhash[20];   // HashSize
+
+};
+
+#endif  //  GLOBAL_H
