e013a865b994a063ec4b1e79f13aedcfd10f9d25
Rony Shapiro <ronys@users.sourceforge.net>
2003-06-08 13:37:24 +0000

    Preapre for version 2.0 support
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@169 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ corelib/PWSfile.cpp
@@ -0,0 +1,242 @@
+#include "PWSfile.h"
+
+#include <io.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+bool PWSfile::FileExists(const CMyString &filename)
+{
+  struct _stat statbuf;
+  int status;
+
+  status = ::_tstat(filename, &statbuf);
+  return (status == 0);
+}
+
+PWSfile::PWSfile(const CMyString &filename, const CMyString &passkey)
+  : m_filename(filename), m_passkey(passkey), m_curversion(UNKNOWN_VERSION), m_fd(-1)
+{
+}
+
+PWSfile::~PWSfile()
+{
+  CloseFile(); // idempotent
+}
+
+int PWSfile::OpenWriteFile(VERSION v)
+{
+  if (v != V17) // XXX add V20 support!
+    return UNSUPPORTED_VERSION;
+
+  m_fd = _open((LPCTSTR)m_filename,
+	       _O_BINARY|_O_WRONLY|_O_SEQUENTIAL|_O_TRUNC|_O_CREAT,
+	       _S_IREAD | _S_IWRITE);
+
+  if (m_fd == -1)
+    return CANT_OPEN_FILE;
+
+  m_curversion = v;
+
+  // Following used to verify passkey against file's passkey
+  unsigned char randstuff[StuffSize];
+  unsigned char randhash[20];   // HashSize
+  int x;
+
+  for (x=0; x<8; x++)
+    randstuff[x] = newrand();
+  randstuff[8] = randstuff[9] = '\0';
+  GenRandhash(m_passkey, randstuff, randhash);
+
+  _write(m_fd, randstuff, 8);
+  _write(m_fd, randhash, 20);
+
+  for (x=0; x<SaltLength; x++)
+    m_salt[x] = newrand();
+
+  _write(m_fd, m_salt, SaltLength);
+	
+  for (x=0; x<8; x++)
+    m_ipthing[x] = newrand();
+  _write(m_fd, m_ipthing, 8);
+
+  return SUCCESS;
+}
+
+int PWSfile::OpenReadFile(VERSION v)
+{
+  if (v != V17) // XXX add V20 support!
+    return UNSUPPORTED_VERSION;
+
+  m_fd = _open((LPCTSTR) m_filename,
+	       _O_BINARY |_O_RDONLY | _O_SEQUENTIAL,
+	       S_IREAD | _S_IWRITE);
+
+  if (m_fd == -1)
+    return CANT_OPEN_FILE;
+  m_curversion = v;
+
+  int status = CheckPassword();
+
+  if (status != SUCCESS) {
+    CloseFile();
+    return status;
+  }
+
+   _read(m_fd, m_salt, SaltLength);
+   _read(m_fd, m_ipthing, 8);
+
+  return SUCCESS;
+}
+
+void PWSfile::CloseFile()
+{
+  if (m_fd != -1) {
+    _close(m_fd);
+    m_fd = -1;
+  }
+}
+
+PWSfile::VERSION PWSfile::GetFileVersion()
+{
+  // XXX TBD
+  return V17;
+}
+
+int PWSfile::CheckPassword()
+{
+  // if file was opened, leave it open,
+  // else open it for check, close when done
+  const bool was_open = (m_fd != -1);
+
+  if (!was_open) {
+    m_fd = _open((LPCTSTR) m_filename,
+		 _O_BINARY |_O_RDONLY | _O_SEQUENTIAL,
+		 S_IREAD | _S_IWRITE);
+
+    if (m_fd == -1)
+      return CANT_OPEN_FILE;
+  }
+
+  unsigned char randstuff[StuffSize];
+  unsigned char randhash[20];   // HashSize
+
+   _read(m_fd, randstuff, 8);
+   randstuff[8] = randstuff[9] = '\0'; // Gross fugbix
+   _read(m_fd, randhash, 20);
+
+  if (!was_open)
+    CloseFile();
+
+  unsigned char temphash[20]; // HashSize
+  GenRandhash(m_passkey, randstuff, temphash);
+
+  if (0 != ::memcmp((char*)randhash,
+		    (char*)temphash,
+		    20)) {// HashSize
+    return WRONG_PASSWORD;
+  } else {
+    return SUCCESS;
+  }
+}
+
+int PWSfile::WriteCBC(const CString &data)
+{
+  // We do a double cast because the LPCSTR cast operator is overridden by the CString class
+  // to access the pointer we need,
+  // but we in fact need it as an unsigned char. Grrrr.
+  LPCSTR passstr = LPCSTR(m_passkey);
+  LPCSTR datastr = LPCSTR(data);
+
+  return _writecbc(m_fd, (const unsigned char *)datastr, data.GetLength(),
+		   (const unsigned char *)passstr, m_passkey.GetLength(),
+		   m_salt, SaltLength, m_ipthing);
+}
+
+
+int PWSfile::WriteRecord(const CItemData &item)
+{
+  ASSERT(m_fd != -1);
+  ASSERT(m_curversion != UNKNOWN_VERSION);
+
+  switch (m_curversion) {
+  case V17: {
+    CMyString tempdata;
+
+    item.GetName(tempdata);
+    WriteCBC(tempdata);
+    item.GetPassword(tempdata);
+    WriteCBC(tempdata);
+    item.GetNotes(tempdata);
+    WriteCBC(tempdata);
+    return SUCCESS;
+  }
+  break;
+  case V20:
+    // TBD
+    return UNSUPPORTED_VERSION;
+  default:
+    ASSERT(0);
+    return UNSUPPORTED_VERSION;
+  }
+  return SUCCESS;
+}
+
+int
+PWSfile::ReadCBC(CMyString &data)
+{
+  // We do a double cast because the LPCSTR cast operator is overridden by the CString class
+  // to access the pointer we need,
+  // but we in fact need it as an unsigned char. Grrrr.
+  LPCSTR passstr = LPCSTR(m_passkey);
+
+  unsigned char *buffer = NULL;
+  unsigned int buffer_len = 0;
+  int retval;
+
+  retval = _readcbc(m_fd, buffer, buffer_len,
+		   (const unsigned char *)passstr, m_passkey.GetLength(),
+		   m_salt, SaltLength, m_ipthing);
+  if (buffer_len > 0) {
+    CMyString str(LPCSTR(buffer), buffer_len);
+    data = str;
+    trashMemory(buffer, buffer_len);
+    delete[] buffer;
+  } else {
+    data = _T("");
+    // no need to delete[] buffer, since _readcbc will not allocate if
+    // buffer_len is zero
+  }
+  return retval;
+}
+
+
+
+int PWSfile::ReadRecord(CItemData &item)
+{
+  ASSERT(m_fd != -1);
+  ASSERT(m_curversion != UNKNOWN_VERSION);
+
+  switch (m_curversion) {
+  case V17: {
+    CMyString tempdata;
+    
+    int numread = 0;
+    numread += ReadCBC(tempdata);
+    item.SetName(tempdata);
+    numread += ReadCBC(tempdata);
+    item.SetPassword(tempdata);
+    numread += ReadCBC(tempdata);
+    item.SetNotes(tempdata);
+
+    return (numread > 0) ? SUCCESS : END_OF_FILE;
+  }
+  case V20:
+    // TBD
+    return UNSUPPORTED_VERSION;
+  default:
+    ASSERT(0);
+    return UNSUPPORTED_VERSION;
+  }
+}
+
---
+++ corelib/PWSfile.h
@@ -0,0 +1,41 @@
+// PWSfile.h
+// Abstract the gory details of reading and writing an encrypted database
+//-----------------------------------------------------------------------------
+
+#ifndef PWSfile_h
+#define PWSfile_h
+#include "ItemData.h"
+#include "MyString.h"
+
+class PWSfile {
+ public:
+  enum VERSION {V17, V20, UNKNOWN_VERSION}; // supported file versions: V17 is last pre-2.0
+  enum {CANT_OPEN_FILE, UNSUPPORTED_VERSION, WRONG_PASSWORD, END_OF_FILE, SUCCESS};
+
+  static bool FileExists(const CMyString &filename);
+
+  PWSfile(const CMyString &filename, const CMyString &passkey);
+  ~PWSfile();
+
+  int OpenWriteFile(VERSION v); //writes header
+  int OpenReadFile(VERSION v);
+  void CloseFile();
+  VERSION GetFileVersion();
+  int CheckPassword(); // opens for read and then closes iff not already open
+  int WriteRecord(const CItemData &item);
+  int ReadRecord(CItemData &item);
+
+ private:
+  const CMyString m_filename;
+  const CMyString m_passkey;
+  int m_fd;
+  VERSION m_curversion;
+  // crypto stuff for reading/writing files:
+  unsigned char m_salt[SaltLength];
+  unsigned char m_ipthing[8]; // for CBC
+  int WriteCBC(const CString &data);
+  int ReadCBC(CMyString &data);
+};
+
+#endif PWSfile_h
+
