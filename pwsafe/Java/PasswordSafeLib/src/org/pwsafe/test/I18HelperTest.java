9759fe4209362f38d630b53995781526a4da370b
Kevin Preece <preecek@users.sourceforge.net>
2004-02-24 22:26:01 +0000

    Initial add
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@295 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/Java/PasswordSafeLib/src/org/pwsafe/test/I18HelperTest.java
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ * 
+ * This file is provided under the standard terms of the Artistic Licence.  See the
+ * LICENSE file that comes with this package for details.
+ */
+package org.pwsafe.test;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class I18HelperTest extends TestCase
+{
+	/**
+	 *
+	 */
+	public final void testFormatMessage()
+	{
+	}
+}
