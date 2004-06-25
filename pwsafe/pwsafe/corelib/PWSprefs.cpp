c12d320406be9085d3bdec2b73f49dd598422592
Rony Shapiro <ronys@users.sourceforge.net>
2004-06-25 13:34:06 +0000

    Added useTrayIcon resource
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@367 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/corelib/PWSprefs.cpp
+++ pwsafe/pwsafe/corelib/PWSprefs.cpp
@@ -32,6 +32,7 @@ const PWSprefs::boolPref PWSprefs::m_bool_prefs[NumBoolPrefs] = {
   {_T("DontAskSaveMinimize"), false},
   {_T("QuerySetDef"), true},
   {_T("UseNewToolbar"), true},
+  {_T("UseSystemTray"), true},
 };
 
 const PWSprefs::intPref PWSprefs::m_int_prefs[NumIntPrefs] = {
