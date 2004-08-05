f96e11648b89a672b76dfb94e17ebeda236bdd1d
Dave Collins <thedavecollins@users.sourceforge.net>
2004-08-05 13:59:29 +0000

    Moved keystroke functionality into this class.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@437 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/KeySend.h
@@ -0,0 +1,30 @@
+// 
+// KeySend.h
+// thedavecollins 2004-08-07
+// sends keystrokes
+//-----------------------------------------------------------------------------
+
+#ifndef CKeySend_h
+#define CKeySend_h
+
+#include "corelib/PWScore.h"
+
+#include "PasswordSafe.h"
+
+
+class CKeySend
+{
+public:
+	CKeySend(void);
+	~CKeySend(void);
+	void SendString(CMyString data);
+	void ResetKeyboardState();
+	void SendChar(TCHAR c);
+	void SetDelay(int d);
+	void SetAndDelay(int d);
+private:
+	int m_delay;
+	HKL m_hlocale;
+};
+
+#endif
