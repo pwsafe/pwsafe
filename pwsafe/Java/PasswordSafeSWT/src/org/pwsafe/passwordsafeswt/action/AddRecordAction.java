769816e6208a155c7db3a5e65af656409ce87b11
Glen Smith <glen_a_smith@users.sourceforge.net>
2005-07-27 21:12:23 +0000

    Added support for default usernames.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@572 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/Java/PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/action/AddRecordAction.java
+++ pwsafe/Java/PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/action/AddRecordAction.java
@@ -5,9 +5,11 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.pwsafe.passwordsafeswt.PasswordSafeJFace;
 import org.pwsafe.passwordsafeswt.dialog.EditDialog;
 import org.pwsafe.passwordsafeswt.dto.PwsEntryDTO;
+import org.pwsafe.passwordsafeswt.preference.UsernamePreferences;
+import org.pwsafe.passwordsafeswt.util.UserPreferences;
 
 /**
- * Adds a new record to the safe.
+ * Adds a new record to the password safe.
  *
  * @author Glen Smith
  */
@@ -25,6 +27,10 @@ public class AddRecordAction extends Action {
     public void run() {
         PasswordSafeJFace app = PasswordSafeJFace.getApp();
         PwsEntryDTO newEntry = new PwsEntryDTO();
+		UserPreferences prefs = UserPreferences.getInstance();
+		if (prefs.getBoolean(UsernamePreferences.USE_DEFAULT_USERNAME)) {
+			newEntry.setUsername(prefs.getString(UsernamePreferences.DEFAULT_USERNAME));
+		}
         EditDialog ed = new EditDialog(app.getShell(), newEntry);
         newEntry = (PwsEntryDTO) ed.open();
         if (newEntry != null) {
