82f68a34cf370257a839200ec7b7547f0767a7b6
Unknown <unknown@nowhere.com>
2005-08-26 16:18:15 +0000

    This commit was manufactured by cvs2svn to create tag 'V2_12'.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/tags/V2_12@588 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/Java/PasswordSafeSWT/platform/linux/pwsafe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Classpath builder pinched from http://www.devdaily.com/blog/Content/2/17/404/
-JAR_CLASSPATH=
-for i in `ls *.jar`
-do
-  JAR_CLASSPATH=${JAR_CLASSPATH}:${i}
-done
-java -Djava.library.path=. -cp ${JAR_CLASSPATH} org.pwsafe.passwordsafeswt.PasswordSafeJFace
