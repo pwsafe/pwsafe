30254a15747ccbf02e3cd92c7934a682e7996cd6
Glen Smith <glen_a_smith@users.sourceforge.net>
2005-10-20 11:48:16 +0000

    Added version info and url launcher
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@608 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/Java/PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/dialog/AboutDialog.java
+++ pwsafe/Java/PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/dialog/AboutDialog.java
@@ -1,6 +1,8 @@
 package org.pwsafe.passwordsafeswt.dialog;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -12,8 +14,10 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.pwsafe.passwordsafeswt.action.VisitPasswordSafeWebsiteAction;
 import org.pwsafe.passwordsafeswt.util.ShellHelpers;
 import org.pwsafe.passwordsafeswt.util.VersionInfo;
+import com.swtdesigner.SWTResourceManager;
 
 /**
  * AboutDialog shows author/contributor/contact details.
@@ -53,10 +57,29 @@ public class AboutDialog extends Dialog {
 		group.setLayoutData(new GridData(GridData.FILL_BOTH));
 		group.setLayout(new GridLayout());
 
+		final Label lblLogo = new Label(group, SWT.NONE);
+		lblLogo.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_VERTICAL));
+		lblLogo.setImage(SWTResourceManager.getImage(AboutDialog.class, "/org/pwsafe/passwordsafeswt/images/psafetxt.gif"));
+
+		final Label lblAuthor = new Label(group, SWT.NONE);
+		lblAuthor.setAlignment(SWT.CENTER);
+		lblAuthor.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+		lblAuthor.setText("(c) 2005 by Glen Smith and others");
+
 		final Label lblVersion = new Label(group, SWT.NONE);
 		lblVersion.setAlignment(SWT.CENTER);
-		lblVersion.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_FILL));
+		lblVersion.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_END));
 		lblVersion.setText("Version: " + VersionInfo.getVersion());
+
+		final Label lblWebsite = new Label(group, SWT.CENTER);
+		lblWebsite.addMouseListener(new MouseAdapter() {
+			public void mouseDown(MouseEvent e) {
+				new VisitPasswordSafeWebsiteAction().run();
+			}
+		});
+		lblWebsite.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_BLUE));
+		lblWebsite.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+		lblWebsite.setText("Visit Password Safe Website");
 
 		final Composite composite = new Composite(shell, SWT.NONE);
 		composite.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
