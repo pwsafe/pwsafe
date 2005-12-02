9c45c6698ceaec6c2d87c4015a24970d369d12c4
Rony Shapiro <ronys@users.sourceforge.net>
2005-12-02 16:23:48 +0000

    nitpick
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@649 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/corelib/sha1.h
+++ pwsafe/pwsafe/corelib/sha1.h
@@ -7,7 +7,6 @@ typedef struct {
     unsigned char buffer[64];
 } SHA1_CTX;
 
-void SHA1Transform(unsigned long state[5], const unsigned char buffer[64]);
 void SHA1Init(SHA1_CTX* context);
 void SHA1Update(SHA1_CTX* context, const unsigned char* data, unsigned int len);
 void SHA1Final(unsigned char digest[20], SHA1_CTX* context);
