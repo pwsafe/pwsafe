3b506ab61982366d35964875cc428b08de98ae4f
Rony Shapiro <ronys@users.sourceforge.net>
2005-12-17 08:09:35 +0000

    pre-3.0 cosmetics
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@666 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/corelib/BlowFish.h
+++ pwsafe/pwsafe/corelib/BlowFish.h
@@ -1,6 +1,7 @@
 // BlowFish.h
 //-----------------------------------------------------------------------------
-
+#ifndef __BLOWFISH_H
+#define __BLOWFISH_H
 
 #define MAXKEYBYTES 56 // unused
 
@@ -30,8 +31,8 @@ public:
   ~BlowFish();
   void Encrypt(const block in, block out);
   void Decrypt(const block in, block out);
-  enum {bf_N = 16};
 private:
+  enum {bf_N = 16};
   unsigned long bf_S[4][256];
   unsigned long bf_P[bf_N + 2];
   const static unsigned long tempbf_S[4][256];
@@ -40,6 +41,7 @@ private:
   void Blowfish_decipher(unsigned long* xl, unsigned long* xr);
   void InitializeBlowfish(unsigned char key[], short keybytes);
 };
+#endif /* __BLOWFISH_H */
 //-----------------------------------------------------------------------------
 // Local variables:
 // mode: c++
