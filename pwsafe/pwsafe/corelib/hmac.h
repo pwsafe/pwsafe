20ec4aab0348c2d5d6a9df43cbc889a77ad46cf1
Rony Shapiro <ronys@users.sourceforge.net>
2005-12-17 08:20:37 +0000

    Added HMAC functionality
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@669 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/corelib/hmac.h
@@ -0,0 +1,34 @@
+// hmac.h
+// HMAC for PasswordSafe
+//-----------------------------------------------------------------------------
+#ifndef _HMAC_H_
+#define _HMAC_H_
+
+// Currently implemented only for sha256, as required by version 3
+// of the database format.
+// HMAC algorithm as per RFC2104
+// Generalizing this to other hashes is left as an exercise to the reader...
+
+#include "sha256.h" 
+
+class HMAC_SHA256
+{
+public:
+  enum {HASHLEN = 32};
+  HMAC_SHA256(unsigned char *key, unsigned long keylen);
+  ~HMAC_SHA256();
+  void Update(const unsigned char *in, unsigned long inlen);
+  void Final(unsigned char digest[HASHLEN]);
+private:
+  SHA256 H;
+  /* for SHA256 hashsize(L) = 32, blocksize(B) = 64 */
+  enum {L = 32, B = 64};
+  unsigned char K[B];
+};
+
+#endif /* _HMAC_H_ */
+//-----------------------------------------------------------------------------
+// Local variables:
+// mode: c++
+// End:
+
