a3653858f130180e0ef32aa7341b78e32bf1bf06
Rony Shapiro <ronys@users.sourceforge.net>
2006-03-08 14:48:24 +0000

    [ 1445513 ] Add (u)int* typedef's for FreeBSD (applied Andrew Turner's patch)
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@755 1f79f812-37fb-46fe-a122-30589dd2bf55

--- corelib/Util.h
+++ corelib/Util.h
@@ -24,6 +24,7 @@
 #define V10 0
 #define V15 1
 
+#ifdef _WIN32
 //Some extra typedefs -- I'm addicted to typedefs
 typedef char    int8;
 typedef short   int16;
@@ -37,7 +38,18 @@ typedef unsigned __int64 uint64;
 
 typedef unsigned __int64   ulong64;
 typedef unsigned long      ulong32;
+#else
+#include <sys/types.h>
+typedef int8_t	int8;
+typedef int16_t	int16;
+typedef int32_t	int32;
+typedef int64_t	int64;
 
+typedef u_int8_t	uint8;
+typedef u_int16_t	uint16;
+typedef u_int32_t	uint32;
+typedef u_int64_t	uint64;
+#endif
 
 extern void trashMemory(void* buffer, long length );
 extern void trashMemory( LPTSTR buffer, long length );
