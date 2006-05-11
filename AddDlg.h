0488e054180c27a1c188a99955f13403ae65c1f1
Rony Shapiro <ronys@users.sourceforge.net>
2006-05-11 07:56:14 +0000

    Minor cosmetics, plus update more/less preference.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@793 1f79f812-37fb-46fe-a122-30589dd2bf55

--- AddDlg.h
+++ AddDlg.h
@@ -51,6 +51,7 @@ protected:
 	afx_msg void OnBnClickedClearLTime();
 	afx_msg void OnBnClickedSetLTime();
 	CButton m_moreLessBtn;
+    afx_msg void OnStnClickedStaticLtime();
 };
 //-----------------------------------------------------------------------------
 // Local variables:
