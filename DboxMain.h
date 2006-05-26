98a6dd48ca25a3763e2e231bb651358abaec210e
Rony Shapiro <ronys@users.sourceforge.net>
2006-05-26 04:57:32 +0000

    Got rid of m_LockDisabled logic. Works fine w/o it on XP...
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@806 1f79f812-37fb-46fe-a122-30589dd2bf55

--- DboxMain.h
+++ DboxMain.h
@@ -304,7 +304,6 @@ private:
   CItemData *m_selectedAtMinimize; // to restore selection upon un-minimize
   BOOL IsWorkstationLocked() const;
   void startLockCheckTimer();
-  bool m_LockDisabled; // set when a dialog box is open, to avoid confusion
   UINT m_IdleLockCountDown;
   void SetIdleLockCounter(UINT i) {m_IdleLockCountDown = i;}
   void ResetIdleLockCounter();
