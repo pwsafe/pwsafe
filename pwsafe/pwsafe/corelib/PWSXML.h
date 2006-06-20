8ce716143f4c9b8564751cfef33e6137ce5eb23b
Rony Shapiro <ronys@users.sourceforge.net>
2006-06-20 14:54:35 +0000

    merge into main trunk
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@844 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/corelib/PWSXML.h
@@ -0,0 +1,25 @@
+// PWSXML.h : header file
+//
+
+#pragma once
+
+#include "MyString.h"
+
+class PWSXML {
+public:
+	PWSXML();
+	~PWSXML();
+
+	void SetCore(void *core) {m_core = core;};
+	bool XMLValidate(const CString &strXMLFileName, const CString &strXSDFileName);
+	bool XMLImport(const CString &ImportedPrefix, const CString &strXMLFileName);
+
+	CString m_strResultText;
+	int m_numEntriesValidated, m_numEntriesImported;
+
+private:
+	void *m_core;
+	TCHAR m_delimiter;
+	bool m_bValidation;
+};
+
