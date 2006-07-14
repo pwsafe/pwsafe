81bf6671fd6a8d8671f94ac0fd8bee53c55f8580
Rony Shapiro <ronys@users.sourceforge.net>
2006-07-14 02:14:58 +0000

    refactoring EditDlg/MainEdit - compiles cleanly but not complete - checkpoint
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@896 1f79f812-37fb-46fe-a122-30589dd2bf55

--- EditDlg.h
+++ EditDlg.h
@@ -2,85 +2,95 @@
 //-----------------------------------------------------------------------------
 
 #include "afxwin.h"
+class CItemData;
+
 class CEditDlg
-   : public CDialog
+  : public CDialog
 {
 
 public:
-   // default constructor
-   CEditDlg(CWnd* pParent = NULL);
-   virtual ~CEditDlg();
+  // default constructor
+  CEditDlg(CItemData *ci, CWnd* pParent = NULL);
+  virtual ~CEditDlg();
 
-   enum { IDD = IDD_EDIT };
-   CMyString m_notes;
-   CMyString m_password, m_password2;
-   CMyString m_username;
-   CMyString m_title;
-   CMyString m_group;
-   CMyString m_URL;
-   CMyString m_autotype;
-   CMyString m_ascCTime;
-   CMyString m_ascPMTime;
-   CMyString m_ascATime;
-   CMyString m_ascLTime;
-   CMyString m_ascRMTime;
-   time_t m_tttLTime;
-   int m_MaxPWHistory;
-   int m_NumPWHistory;
+  enum { IDD = IDD_EDIT };
+  CMyString m_defusername, m_username;
+  CMyString m_ascCTime;
+  CMyString m_ascPMTime;
+  CMyString m_ascATime;
+  CMyString m_ascLTime;
+  CMyString m_ascRMTime;
+  time_t m_tttLTime;
+  int m_MaxPWHistory;
+  int m_NumPWHistory;
 
-   CListCtrl m_PWHistListCtrl;
+  CListCtrl m_PWHistListCtrl;
 
-   PWHistList* m_pPWHistList;
+  PWHistList* m_pPWHistList;
 
-   CMyString m_realpassword;
+  CMyString m_realpassword;
 
-   POSITION  m_listindex;
+  POSITION  m_listindex;
 
-   bool m_IsReadOnly;
-   void  ShowPassword();
-   void  HidePassword();
-   bool m_ClearPWHistory;
-   BOOL m_SavePWHistory;
-   int m_iSortedColumn;
-   BOOL m_bSortAscending;
-   bool m_bSavePWHistory;
+  bool m_IsReadOnly;
+  void  ShowPassword();
+  void  HidePassword();
+  bool IsModified() const {return m_bIsModified;}
+  bool IsPswdModified() const {return m_bPswdModified;}
+  bool m_ClearPWHistory;
+  BOOL m_SavePWHistory;
+  int m_iSortedColumn;
+  BOOL m_bSortAscending;
+  bool m_bSavePWHistory;
 
 private:
-   TCHAR m_passwordchar;
-   bool m_isPwHidden;
-   // Are we showing more or less details?
-   bool m_isExpanded;
-   void ResizeDialog();
+  CItemData *m_ci; // The entry being edited
+  CMyString m_notes;
+  CMyString m_password, m_password2;
+  CMyString m_title;
+  CMyString m_group;
+  CMyString m_URL;
+  CMyString m_autotype;
+  bool m_bIsModified;
+  bool m_bPswdModified;
+  TCHAR m_passwordchar;
+  bool m_isPwHidden;
+  // Are we showing more or less details?
+  bool m_isExpanded;
+  // following two are not directly derived from CItemData
+  CMyString m_oldascLTime;
+  int m_oldMaxPWHistory;
+  void ResizeDialog();
 
 protected:
-   virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+  virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
-   afx_msg void OnShowpassword();
-   virtual void OnOK();
-   virtual void OnCancel();
-   virtual BOOL OnInitDialog();
-   afx_msg void OnRandom();
-   afx_msg void OnHelp();
+  afx_msg void OnShowpassword();
+  virtual void OnOK();
+  virtual void OnCancel();
+  virtual BOOL OnInitDialog();
+  afx_msg void OnRandom();
+  afx_msg void OnHelp();
 #if defined(POCKET_PC)
-   afx_msg void OnPasskeySetfocus();
-   afx_msg void OnPasskeyKillfocus();
+  afx_msg void OnPasskeySetfocus();
+  afx_msg void OnPasskeyKillfocus();
 #endif
-   DECLARE_MESSAGE_MAP()
-public:
-	afx_msg void OnBnClickedOk();
-	afx_msg void OnBnClickedMore();
-	afx_msg void OnBnClickedClearLTime();
-	afx_msg void OnBnClickedSetLTime();
-	afx_msg void OnBnClickedShowPasswordHistory();
-	afx_msg void OnCheckedSavePasswordHistory();
-	afx_msg void OnHeaderClicked(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnHistListClick(NMHDR* pNMHDR, LRESULT* pResult);
-    afx_msg void OnBnClickedClearPWHist();
+  DECLARE_MESSAGE_MAP()
+  public:
+  afx_msg void OnBnClickedOk();
+  afx_msg void OnBnClickedMore();
+  afx_msg void OnBnClickedClearLTime();
+  afx_msg void OnBnClickedSetLTime();
+  afx_msg void OnBnClickedShowPasswordHistory();
+  afx_msg void OnCheckedSavePasswordHistory();
+  afx_msg void OnHeaderClicked(NMHDR* pNMHDR, LRESULT* pResult);
+  afx_msg void OnHistListClick(NMHDR* pNMHDR, LRESULT* pResult);
+  afx_msg void OnBnClickedClearPWHist();
 
-	CButton m_MoreLessBtn;
+  CButton m_MoreLessBtn;
 
 private:
-	static int CALLBACK CompareFunc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
+  static int CALLBACK CompareFunc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
 };
 //-----------------------------------------------------------------------------
 // Local variables:
