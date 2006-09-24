09521169dc252c0968803a2fda3e8e9e1d27b16a
minus273c <minus273c@users.sourceforge.net>
2006-09-24 18:38:30 +0000

    Add missing files Remove "UK" resources (all the rest are US)
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@974 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ Properties.cpp
@@ -0,0 +1,48 @@
+// Properties.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Properties.h"
+
+
+// CProperties dialog
+
+IMPLEMENT_DYNAMIC(CProperties, CDialog)
+
+CProperties::CProperties(CWnd* pParent /*=NULL*/)
+	: CDialog(CProperties::IDD, pParent)
+{
+}
+
+CProperties::~CProperties()
+{
+}
+
+void CProperties::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CProperties, CDialog)
+	ON_BN_CLICKED(IDOK, &CProperties::OnOK)
+END_MESSAGE_MAP()
+
+
+// CProperties message handlers
+
+void CProperties::OnOK()
+{
+	CDialog::OnOK();
+}
+
+BOOL CProperties::OnInitDialog()
+{
+	GetDlgItem(IDC_DATABASENAME)->SetWindowText(m_database);
+	GetDlgItem(IDC_NUMGROUPS)->SetWindowText(m_numgroups);
+	GetDlgItem(IDC_NUMENTRIES)->SetWindowText(m_numentries);
+	GetDlgItem(IDC_SAVEDON)->SetWindowText(m_whenlastsaved);
+	GetDlgItem(IDC_SAVEDBY)->SetWindowText(m_wholastsaved);
+
+	return TRUE;
+}
