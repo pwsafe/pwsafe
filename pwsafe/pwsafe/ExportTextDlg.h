89ed3f01c919a45cabd89d06b31c68ceb7520f47
minus273c <minus273c@users.sourceforge.net>
2006-10-21 15:10:17 +0000

    Make file names consistent (Dialog = ....Dlg.) Remove unneeded reference in pwsafe.vcproj to mystring.h
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1047 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/ExportTextDlg.h
@@ -0,0 +1,66 @@
+#if !defined(AFX_EXPORTTEXT_H__CCC6EEF6_3DF0_41A2_BDA0_0437BBFAC672__INCLUDED_)
+#define AFX_EXPORTTEXT_H__CCC6EEF6_3DF0_41A2_BDA0_0437BBFAC672__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// ExportText.h : header file
+//
+
+#include <bitset>
+
+/////////////////////////////////////////////////////////////////////////////
+// CExportText dialog
+
+// JHF : added PocketPC switch
+#if defined(POCKET_PC)
+  #include "pocketpc/resource.h"
+#else
+  #include "resource.h"
+#endif
+
+class CExportTextDlg : public CDialog
+{
+// Construction
+public:
+	CExportTextDlg(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CExportTextDlg)
+	enum { IDD = IDD_EXPORT_TEXT };
+	CString	m_exportTextPassword;
+	CString m_defexpdelim;
+	int m_querysetexpdelim;
+	int m_export_hdr;
+	//}}AFX_DATA
+
+	std::bitset<16> m_bsExport;
+	CString m_subgroup;
+	int m_subgroup_object, m_subgroup_function;
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CExportTextDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	virtual BOOL OnInitDialog();
+	// Generated message map functions
+	//{{AFX_MSG(CExportTextDlg)
+	afx_msg void OnSetMultilineExportNotesDelimiter();
+	afx_msg void OnAdvanced();
+	afx_msg void OnHelp();
+	virtual void OnOK();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+private:
+	void AFXAPI DDV_CheckExpDelimiter(CDataExchange* pDX, const CString &delimiter);
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_EXPORTTEXT_H__CCC6EEF6_3DF0_41A2_BDA0_0437BBFAC672__INCLUDED_)
