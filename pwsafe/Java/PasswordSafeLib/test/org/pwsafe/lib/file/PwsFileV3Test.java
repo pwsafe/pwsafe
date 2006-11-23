082da994b11a024a3e91fc1a77151ce70a539295
Glen Smith <glen_a_smith@users.sourceforge.net>
2006-11-23 02:56:35 +0000

    Added test for large files.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1141 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/Java/PasswordSafeLib/test/org/pwsafe/lib/file/PwsFileV3Test.java
+++ pwsafe/Java/PasswordSafeLib/test/org/pwsafe/lib/file/PwsFileV3Test.java
@@ -2,7 +2,9 @@ package org.pwsafe.lib.file;
 
 import java.io.IOException;
 
+import org.pwsafe.lib.UUID;
 import org.pwsafe.lib.exception.EndOfFileException;
+import org.pwsafe.lib.exception.PasswordSafeException;
 import org.pwsafe.lib.exception.UnsupportedFileVersionException;
 
 import junit.framework.TestCase;
@@ -23,5 +25,33 @@ public class PwsFileV3Test extends TestCase {
 		System.out.println(file.getRecordCount());
 		
 	}
+	
+	public void testLargeFile() throws EndOfFileException, IOException, UnsupportedFileVersionException, Exception {
+		String filename = "/data/Java/eclipse/PasswordSafeLib/bigsample3.psafe3";
+		String password = "password";
+		
+		PwsFileV3 file = new PwsFileV3();
+		for (int i = 0; i < 1000; i++) {
+			if (i%10 == 0) { System.out.print("."); }
+			PwsRecordV3 v3 = new PwsRecordV3();
+			
+            v3.setField(new PwsStringUnicodeField(PwsRecordV3.GROUP , ""));
+            v3.setField(new PwsStringUnicodeField(PwsRecordV3.TITLE , "title" + i));
+            v3.setField(new PwsStringUnicodeField(PwsRecordV3.USERNAME , "user"+i));
+            v3.setField(new PwsStringUnicodeField(PwsRecordV3.PASSWORD , "pw" + i));
+            v3.setField(new PwsStringUnicodeField(PwsRecordV3.NOTES , "notes"+i));
+			file.add(v3);
+		}
+		file.setFilename(filename);
+		file.setPassphrase(password);
+
+		System.out.println("\nDone.");
+		
+		file.save();
+		System.out.println(file.getRecordCount());
+		
+	}
+	
+	
 
 }
