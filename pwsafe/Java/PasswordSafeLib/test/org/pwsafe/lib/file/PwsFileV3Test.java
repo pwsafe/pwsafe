d687b6f1482daa8f1ff019c18a4cb2012c9da797
Glen Smith <glen_a_smith@users.sourceforge.net>
2006-11-27 20:01:33 +0000

    Large file write test.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1165 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/Java/PasswordSafeLib/test/org/pwsafe/lib/file/PwsFileV3Test.java
+++ pwsafe/Java/PasswordSafeLib/test/org/pwsafe/lib/file/PwsFileV3Test.java
@@ -2,12 +2,10 @@ package org.pwsafe.lib.file;
 
 import java.io.IOException;
 
-import org.pwsafe.lib.UUID;
-import org.pwsafe.lib.exception.EndOfFileException;
-import org.pwsafe.lib.exception.PasswordSafeException;
-import org.pwsafe.lib.exception.UnsupportedFileVersionException;
-
 import junit.framework.TestCase;
+
+import org.pwsafe.lib.exception.EndOfFileException;
+import org.pwsafe.lib.exception.UnsupportedFileVersionException;
 
 /**
  * Test cases for sample v3 record.
@@ -17,26 +15,29 @@ import junit.framework.TestCase;
 public class PwsFileV3Test extends TestCase {
 	
 	public void testPassphrase() throws EndOfFileException, IOException, UnsupportedFileVersionException {
-		String filename = "/data/Java/PasswordSafeLib/sample3.psafe3";
+		//String filename = "/data/Java/PasswordSafeLib/sample3.psafe3";
+		//String filename = "C:/Documents and Settings/Glen/Desktop/stuff216to3.psafe3";
+		String filename = "C:/Documents and Settings/Glen/Desktop/bigsample3.psafe3";
 		String password = "Pa$$word";
 		
 		PwsFileV3 file = new PwsFileV3(filename, password);
 		file.readAll();
 		System.out.println(file.getRecordCount());
+		file.close();
 		
 	}
 	
 	public void testLargeFile() throws EndOfFileException, IOException, UnsupportedFileVersionException, Exception {
-		String filename = "/data/Java/PasswordSafeLib/bigsample3.psafe3";
-		String password = "password";
+		String filename = "C:/Documents and Settings/Glen/Desktop/bigsample3.psafe3";
+		String password = "Pa$$word";
 		
 		PwsFileV3 file = (PwsFileV3) PwsFileFactory.newFile();
 		file.setPassphrase(password);
-		for (int i = 0; i < 10; i++) {
-			if (i%10 == 0) { System.out.print("."); }
+		for (int i = 0; i < 1000; i++) {
+			if (i%1 == 0) { System.out.print("."); }
 			PwsRecordV3 v3 = (PwsRecordV3) file.newRecord();
 			
-            v3.setField(new PwsStringUnicodeField(PwsRecordV3.GROUP , "group"));
+            v3.setField(new PwsStringUnicodeField(PwsRecordV3.GROUP , "group" + i%10));
             v3.setField(new PwsStringUnicodeField(PwsRecordV3.TITLE , "title" + i));
             v3.setField(new PwsStringUnicodeField(PwsRecordV3.USERNAME , "user"+i));
             v3.setField(new PwsStringUnicodeField(PwsRecordV3.PASSWORD , "pw" + i));
@@ -47,7 +48,12 @@ public class PwsFileV3Test extends TestCase {
 		System.out.println("\nDone.");
 		
 		file.save();
-		System.out.println(file.getRecordCount());
+		System.out.println("Wrote records: " + file.getRecordCount());
+		file.close();
+		
+		PwsFileV3 file2 = new PwsFileV3(filename, password);
+		file2.readAll();
+		System.out.println("Read records: " + file2.getRecordCount());
 		
 	}
 	
