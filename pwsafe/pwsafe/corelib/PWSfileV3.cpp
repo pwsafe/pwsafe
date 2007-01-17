c6a999c285a8625731c66c10aa2c3557e7e5e78b
Rony Shapiro <ronys@users.sourceforge.net>
2007-01-17 20:37:16 +0000

    More attempts to get non-English data to store/retrieve correctly.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1233 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/corelib/PWSfileV3.cpp
+++ pwsafe/pwsafe/corelib/PWSfileV3.cpp
@@ -729,150 +729,154 @@ int PWSfileV3::ReadHeader()
 
 bool PWSfileV3::ToUTF8(const CString &data)
 {
-  // If we're not in Unicode, call MultiByteToWideChar to get from
-  // current codepage to Unicode, and then WideCharToMultiByte to
-  // get to UTF-8 encoding.
-  // Output is in m_utf8 & m_utf8Len
+    // If we're not in Unicode, call MultiByteToWideChar to get from
+    // current codepage to Unicode, and then WideCharToMultiByte to
+    // get to UTF-8 encoding.
+    // Output is in m_utf8 & m_utf8Len
 
-  if (data.IsEmpty()) {
-    m_utf8Len = 0;
-    return true;
-  }
-  wchar_t *wcPtr; // to hide UNICODE differences
-  int wcLen; // number of wide chars needed
+    if (data.IsEmpty()) {
+        m_utf8Len = 0;
+        return true;
+    }
+    wchar_t *wcPtr; // to hide UNICODE differences
+    int wcLen; // number of wide chars needed
 #ifndef UNICODE
-  // first get needed wide char buffer size
-  wcLen = MultiByteToWideChar(CP_THREAD_ACP,      // code page
-                              0,                  // character-type options
-                              LPCSTR(data),       // string to map
-                              data.GetLength(),   // number of bytes in string
-                              NULL, 0);           // get needed buffer size
-  if (wcLen == 0) { // uh-oh
-    m_utf8Len = 0;
-    return false;
-  }
-  // Allocate buffer (if previous allocation was smaller)
-  if (wcLen > m_wcMaxLen) {
-    if (m_wc != NULL)
-      trashMemory(m_wc, m_wcMaxLen * sizeof(m_wc[0]));
-    delete[] m_wc;
-    m_wc = new wchar_t[wcLen];
-    m_wcMaxLen = wcLen;
-  }
-  // next translate to buffer
-  wcLen = MultiByteToWideChar(CP_THREAD_ACP,      // code page
-                              0,                  // character-type options
-                              LPCSTR(data),       // string to map
-                              data.GetLength(),   // number of bytes in string
-                              m_wc, wcLen);       // output buffer
-  ASSERT(wcLen != 0);
-  wcPtr = m_wc;
+    // first get needed wide char buffer size
+    wcLen = MultiByteToWideChar(CP_ACP,             // code page
+                                MB_PRECOMPOSED,     // character-type options
+                                LPCSTR(data),       // string to map
+                                -1,                 // -1 means null-terminated
+                                NULL, 0);           // get needed buffer size
+    if (wcLen == 0) { // uh-oh
+        ASSERT(0);
+        m_utf8Len = 0;
+        return false;
+    }
+    // Allocate buffer (if previous allocation was smaller)
+    if (wcLen > m_wcMaxLen) {
+        if (m_wc != NULL)
+            trashMemory(m_wc, m_wcMaxLen * sizeof(m_wc[0]));
+        delete[] m_wc;
+        m_wc = new wchar_t[wcLen];
+        m_wcMaxLen = wcLen;
+    }
+    // next translate to buffer
+    wcLen = MultiByteToWideChar(CP_ACP,             // code page
+                                MB_PRECOMPOSED,     // character-type options
+                                LPCSTR(data),       // string to map
+                                -1,                 // -1 means null-terminated
+                                m_wc, wcLen);       // output buffer
+    ASSERT(wcLen != 0);
+    wcPtr = m_wc;
 #else
-  wcPtr = LPCTSTR(data);
-  wcLen = data.GetLength();
+    wcPtr = LPCTSTR(data);
+    wcLen = data.GetLength()+1;
 #endif
-  // first get needed utf8 buffer size
-  int mbLen = WideCharToMultiByte(CP_UTF8,       // code page
-                                  0,             // performance and mapping flags
-                                  wcPtr,        // wide-character string
-                                  wcLen,         // number of chars in string
-                                  NULL, 0,       // get needed buffer size
-                                  NULL,NULL);    // use system default for unmappables
+    // first get needed utf8 buffer size
+    int mbLen = WideCharToMultiByte(CP_UTF8,       // code page
+                                    0,             // performance and mapping flags
+                                    wcPtr,         // wide-character string
+                                    -1,            // -1 means null-terminated
+                                    NULL, 0,       // get needed buffer size
+                                    NULL,NULL);    // use system default for unmappables
 
-  if (mbLen == 0) { // uh-oh
-    m_utf8Len = 0;
-    return false;
-  }
-  // Allocate buffer (if previous allocation was smaller)
-  if (mbLen > m_utf8MaxLen) {
-    if (m_utf8 != NULL)
-      trashMemory(m_utf8, m_utf8MaxLen);
-    delete[] m_utf8;
-    m_utf8 = new unsigned char[mbLen];
-    m_utf8MaxLen = mbLen;
-  }
-  // Finally get result
-  m_utf8Len = WideCharToMultiByte(CP_UTF8,      // code page
-                                  0,            // performance and mapping flags
-                                  wcPtr, wcLen, // wide-character string
-                                  LPSTR(m_utf8), mbLen,// buffer and length
-                                  NULL,NULL);   // use system default for unmappables
-  ASSERT(m_utf8Len != 0);
-  return true;
+    if (mbLen == 0) { // uh-oh
+        ASSERT(0);
+        m_utf8Len = 0;
+        return false;
+    }
+    // Allocate buffer (if previous allocation was smaller)
+    if (mbLen > m_utf8MaxLen) {
+        if (m_utf8 != NULL)
+            trashMemory(m_utf8, m_utf8MaxLen);
+        delete[] m_utf8;
+        m_utf8 = new unsigned char[mbLen];
+        m_utf8MaxLen = mbLen;
+    }
+    // Finally get result
+    m_utf8Len = WideCharToMultiByte(CP_UTF8,      // code page
+                                    0,            // performance and mapping flags
+                                    wcPtr, wcLen, // wide-character string
+                                    LPSTR(m_utf8), mbLen, // buffer and length
+                                    NULL,NULL);   // use system default for unmappables
+    ASSERT(m_utf8Len != 0);
+    return true;
 }
 
 bool PWSfileV3::FromUTF8(CMyString &data)
 {
-  // Call MultiByteToWideChar to get from UTF-8 to Unicode.
-  // If we're not in Unicode, call WideCharToMultiByte to
-  // get to current codepage.
-  // Input is in m_utf8 & m_utf8Len
+    // Call MultiByteToWideChar to get from UTF-8 to Unicode.
+    // If we're not in Unicode, call WideCharToMultiByte to
+    // get to current codepage.
+    // Input is in m_utf8 & m_utf8Len
 
-  if (m_utf8Len == 0) {
-    data = _T("");
-    return true;
-  }
-  // first get needed wide char buffer size
-  int wcLen = MultiByteToWideChar(CP_UTF8,      // code page
-                                  0,            // character-type options
-                                  LPSTR(m_utf8), // string to map
-                                  m_utf8Len,    // number of bytes in string
-                                  NULL, 0);     // get needed buffer size
-  if (wcLen == 0) { // uh-oh
-    data = _T("");
-    return false;
-  }
-  // Allocate buffer (if previous allocation was smaller)
-  if (wcLen > m_wcMaxLen) {
-    if (m_wc != NULL)
-      trashMemory(m_wc, m_wcMaxLen * sizeof(m_wc[0]));
-    delete[] m_wc;
-    m_wc = new wchar_t[wcLen+1];
-    m_wcMaxLen = wcLen;
-  }
-  // next translate to buffer
-  wcLen = MultiByteToWideChar(CP_UTF8,      // code page
-                              0,                  // character-type options
-                              LPSTR(m_utf8),       // string to map
-                              m_utf8Len,   // number of bytes in string
-                              m_wc, wcLen);       // output buffer
-  ASSERT(wcLen != 0);
+    if (m_utf8Len == 0) {
+        data = _T("");
+        return true;
+    }
+    // first get needed wide char buffer size
+    int wcLen = MultiByteToWideChar(CP_UTF8,      // code page
+                                    0,            // character-type options
+                                    LPSTR(m_utf8), // string to map
+                                    -1,           // -1 means null-terminated
+                                    NULL, 0);     // get needed buffer size
+    if (wcLen == 0) { // uh-oh
+        ASSERT(0);
+        data = _T("");
+        return false;
+    }
+    // Allocate buffer (if previous allocation was smaller)
+    if (wcLen > m_wcMaxLen) {
+        if (m_wc != NULL)
+            trashMemory(m_wc, m_wcMaxLen * sizeof(m_wc[0]));
+        delete[] m_wc;
+        m_wc = new wchar_t[wcLen];
+        m_wcMaxLen = wcLen;
+    }
+    // next translate to buffer
+    wcLen = MultiByteToWideChar(CP_UTF8,      // code page
+                                MB_ERR_INVALID_CHARS,  // character-type options
+                                LPSTR(m_utf8),       // string to map
+                                -1,           // -1 means null-terminated
+                                m_wc, wcLen);       // output buffer
+    ASSERT(wcLen != 0);
 #ifdef UNICODE
-  m_wc[wcLen] = TCHAR('\0');
-  data = m_wc;
+    m_wc[wcLen-1] = TCHAR('\0');
+    data = m_wc;
 #else /* Go from Unicode to Locale encoding */
       // first get needed utf8 buffer size
-  int mbLen = WideCharToMultiByte(CP_THREAD_ACP, // code page
-                                  0,             // performance and mapping flags
-                                  m_wc,        // wide-character string
-                                  wcLen,         // number of chars in string
-                                  NULL, 0,       // get needed buffer size
-                                  NULL,NULL);    // use system default for unmappables
+    int mbLen = WideCharToMultiByte(CP_ACP,       // code page
+                                    0, // performance and mapping flags
+                                    m_wc,         // wide-character string
+                                    -1,           // -1 means null-terminated
+                                    NULL, 0,      // get needed buffer size
+                                    NULL,NULL);   // use system default for unmappables
 
-  if (mbLen == 0) { // uh-oh
-    data = _T("");
-    return false;
-  }
-  // Allocate buffer (if previous allocation was smaller)
-  if (mbLen > m_tmpMaxLen) {
-    if (m_tmp != NULL)
-      trashMemory(m_tmp, m_utf8MaxLen);
-    delete[] m_tmp;
-    m_tmp = new unsigned char[mbLen+1];
-    m_tmpMaxLen = mbLen;
-  }
-  // Finally get result
-  int tmpLen = WideCharToMultiByte(CP_THREAD_ACP,      // code page
-                                   0,            // performance and mapping flags
-                                   m_wc, wcLen, // wide-character string
-                                   LPSTR(m_tmp), mbLen,// buffer and length
-                                   NULL,NULL);   // use system default for unmappables
-  ASSERT(tmpLen == mbLen);
-  m_tmp[mbLen] = TCHAR('\0');
-  data = m_tmp;
+    if (mbLen == 0) { // uh-oh
+        ASSERT(0);
+        data = _T("");
+        return false;
+    }
+    // Allocate buffer (if previous allocation was smaller)
+    if (mbLen > m_tmpMaxLen) {
+        if (m_tmp != NULL)
+            trashMemory(m_tmp, m_utf8MaxLen);
+        delete[] m_tmp;
+        m_tmp = new unsigned char[mbLen];
+        m_tmpMaxLen = mbLen;
+    }
+    // Finally get result
+    int tmpLen = WideCharToMultiByte(CP_ACP,      // code page
+                                     0, // performance and mapping flags
+                                     m_wc, -1, // wide-character string
+                                     LPSTR(m_tmp), mbLen,// buffer and length
+                                     NULL,NULL);   // use system default for unmappables
+    ASSERT(tmpLen == mbLen);
+    m_tmp[mbLen-1] = TCHAR('\0');
+    data = m_tmp;
 #endif /* !UNICODE */
-  ASSERT(data.GetLength() != 0);
-  return true;
+    ASSERT(data.GetLength() != 0);
+    return true;
 }
 
 
