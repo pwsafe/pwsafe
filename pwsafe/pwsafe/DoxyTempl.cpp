99d92948c385bc081be42b57e4a76bd6d32bba74
Steffen Ryll <sryll@users.sourceforge.net>
2007-03-30 17:39:06 +0000

    - added doxygen config file plus a pseudo class DoxyTempl, to demonstrate how to write documentation with doxygen - set svn:ignore property for some files generated by VS and doxygen - fixed small unicode problem in ThisMfcApp.cpp
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1335 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/DoxyTempl.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2003-2007 Rony Shapiro <ronys@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license.php
+ */
+
+/** \file
+ * pseudo-implementation file for the DoxyTempl class
+ */
+
+#include "DoxyTempl.h"
+
+/** This method ignores its parameter and returns zero.
+ * \param a A meaningless value
+ * \param b Another meaningless value
+ * \return Always zero
+ * 
+ * \note If you write docu for a class member (method or variable, 
+ * you must also write docu for the containing class. Otherwise, doxygen
+ * might your text.
+ *
+ * For complex methods, it might also make sense to add \\callgraph or 
+ * \\callergraph for a visualization.
+ */
+float DoxyTempl::doNothing(float a, double b)
+{
+  /** \todo Do something more meaningful here. */
+  return 0;
+}
+/** \overload
+ */
+float DoxyTempl::doNothing()
+{
+  return 0;
+}
+
+/** A detailed description should be written into the cpp file.
+ * This keeps the header file compact, but understandable (if you 
+ * use brief /// descriptions), while method docu is close to the code.
+ *
+ * \pre This function has no precondition, actually.
+ * \post This function has no postcondition, to be honest ;-)
+ * \invariant zero equals zero
+ */
+int returnZero()
+{
+  /** \bug Something goes wrong here... */
+  return 1;
+}
