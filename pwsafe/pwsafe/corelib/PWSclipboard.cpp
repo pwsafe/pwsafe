fc9c87cbef96aa1588d8f7ece3746f1ba8d72937
Rony Shapiro <ronys@users.sourceforge.net>
2007-07-29 17:35:46 +0000

    Bring clipboard implementation into the 21st century...
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1587 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/corelib/PWSclipboard.cpp
+++ pwsafe/pwsafe/corelib/PWSclipboard.cpp
@@ -10,6 +10,7 @@
  * Implementation file for the PWSclipboard class
  */
 
+#include <afxole.h>
 #include "PWSclipboard.h"
 #include "util.h"
 
@@ -29,40 +30,24 @@ bool
 PWSclipboard::SetData(const CMyString &data, bool isSensitive)
 {
   unsigned int uGlobalMemSize = (data.GetLength() + 1) * sizeof(TCHAR);
-  HGLOBAL hGlobalMemory = GlobalAlloc(GMEM_MOVEABLE|GMEM_DDESHARE,
-                                      uGlobalMemSize);
-  LPTSTR pGlobalLock = (LPTSTR)GlobalLock(hGlobalMemory);
+  HGLOBAL hGlobalMemory = ::GlobalAlloc(GMEM_MOVEABLE, uGlobalMemSize);
+  LPTSTR pGlobalLock = (LPTSTR)::GlobalLock(hGlobalMemory);
 
   PWSUtil::strCopy(pGlobalLock, data.GetLength() + 1, data ,data.GetLength());
+  ::GlobalUnlock(hGlobalMemory);
 
-  GlobalUnlock(hGlobalMemory);
-
-  m_set = false;
-  if (OpenClipboard(AfxGetMainWnd()->GetSafeHwnd()) == TRUE) {
-    if (EmptyClipboard() != TRUE) {
-      TRACE("The clipboard was not emptied correctly");
-    }
-    if (SetClipboardData(
-                         CLIPBOARD_TEXT_FORMAT,
-                         hGlobalMemory) == NULL) {
-      TRACE("The data was not pasted into the clipboard correctly");
-      GlobalFree(hGlobalMemory); // wasn't passed to Clipboard
-    } else {
-      // identify data in clipboard as ours, so as not to clear the wrong data later
-      // of course, we don't want an extra copy of a password floating around
-      // in memory, so we'll use the hash
-      const TCHAR *str = (const TCHAR *)data;
-      SHA256 ctx;
-      ctx.Update((const unsigned char *)str, data.GetLength());
-      ctx.Final(m_digest);
-      m_set = isSensitive; // don't set if !isSensitive, so won't be cleared
-    }
-    if (CloseClipboard() != TRUE) {
-      TRACE("The clipboard could not be closed");
-    }
-  } else {
-    TRACE("The clipboard could not be opened correctly");
-    GlobalFree(hGlobalMemory); // wasn't passed to Clipboard
+  COleDataSource *pods = new COleDataSource; // deleted automagically
+  pods->CacheGlobalData(CLIPBOARD_TEXT_FORMAT, hGlobalMemory);
+  pods->SetClipboard();
+  m_set = isSensitive; // don't set if !isSensitive, so won't be cleared
+  if (m_set) {
+    // identify data in clipboard as ours, so as not to clear the wrong data later
+    // of course, we don't want an extra copy of a password floating around
+    // in memory, so we'll use the hash
+    SHA256 ctx;
+    const TCHAR *str = (const TCHAR *)data;
+    ctx.Update((const unsigned char *)str, data.GetLength()*sizeof(TCHAR));
+    ctx.Final(m_digest);
   }
   return m_set;
 }
@@ -71,38 +56,26 @@ bool
 PWSclipboard::ClearData()
 {
   if (m_set) {
-    if (OpenClipboard(AfxGetMainWnd()->GetSafeHwnd()) != TRUE) {
-      TRACE("The clipboard could not be opened correctly");
-      return false;
-    }
-    
-    if (IsClipboardFormatAvailable(CLIPBOARD_TEXT_FORMAT) != 0) {
-      HGLOBAL hglb = GetClipboardData(CLIPBOARD_TEXT_FORMAT);
-      if (hglb != NULL) {
-        LPTSTR lptstr = (LPTSTR)GlobalLock(hglb);
-        if (lptstr != NULL) {
-          // check identity of data in clipboard
-          unsigned char digest[SHA256::HASHLEN];
-          SHA256 ctx;
-          ctx.Update((const unsigned char *)lptstr, PWSUtil::strLength(lptstr));
-          ctx.Final(digest);
-          if (memcmp(digest, m_digest, SHA256::HASHLEN) == 0) {
-            trashMemory( lptstr, PWSUtil::strLength(lptstr));
-            GlobalUnlock(hglb);
-            if (EmptyClipboard() == TRUE) {
-              memset(m_digest, '\0', SHA256::HASHLEN);
-              m_set = false;
-            } else {
-              TRACE("The clipboard was not emptied correctly");
-            }
-          } else { // hashes match
-            GlobalUnlock(hglb);
-          }
-        } // lptstr != NULL
-      } // hglb != NULL
-    } // IsClipboardFormatAvailable
-    if (CloseClipboard() != TRUE) {
-      TRACE("The clipboard could not be closed");
+    COleDataObject odo;
+    CMyString data;
+    odo.AttachClipboard();
+    HANDLE hData = odo.GetGlobalData(CLIPBOARD_TEXT_FORMAT);
+    if (hData != NULL) {
+      LPCTSTR pData = (LPCTSTR)::GlobalLock(hData);
+      data = pData;
+      // check if the data on the clipboard is the same we put there
+      unsigned char digest[SHA256::HASHLEN];
+      SHA256 ctx;
+      ctx.Update((unsigned char *)pData, data.GetLength()*sizeof(TCHAR));
+      ctx.Final(digest);
+      if (memcmp(digest, m_digest, SHA256::HASHLEN) == 0) {
+        trashMemory((void *)pData, data.GetLength()*sizeof(TCHAR));
+        CMyString blank(_T(""));
+        SetData(blank, false);
+        memset(m_digest, '\0', SHA256::HASHLEN);
+      }
+      ::GlobalUnlock(hData);
+      ::GlobalFree(hData);
     }
   }
   return !m_set;
