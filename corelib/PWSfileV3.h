b263b873d4fac8fe6183d7e18288b9b21bfba049
Rony Shapiro <ronys@users.sourceforge.net>
2007-10-10 08:09:43 +0000

    Changed license to Artistic License 2.0
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@1708 1f79f812-37fb-46fe-a122-30589dd2bf55

--- corelib/PWSfileV3.h
+++ corelib/PWSfileV3.h
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2003-2007 Rony Shapiro <ronys@users.sourceforge.net>.
  * All rights reserved. Use of the code is allowed under the
- * Artistic License terms, as specified in the LICENSE file
+ * Artistic License 2.0 terms, as specified in the LICENSE file
  * distributed with this code, or available from
- * http://www.opensource.org/licenses/artistic-license.php
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 #ifndef __PWSFILEV3_H
 #define __PWSFILEV3_H
