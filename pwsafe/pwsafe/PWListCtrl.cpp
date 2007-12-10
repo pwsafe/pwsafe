a00e6f5cb7305df877c3e7864bd20d234d4fa4c0
DrK <c-273@users.sourceforge.net>
2007-12-10 19:43:36 +0000

    Copy fixes from 3.11.2 branch: Fix when pressing characters in List View with icons shown and fix bad MS' implementation! Fix program abend on clicking CompareResults dialog header Try to get autotype working for Win2K
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@1851 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/PWListCtrl.cpp
+++ pwsafe/pwsafe/PWListCtrl.cpp
@@ -19,6 +19,7 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 CPWListCtrl::CPWListCtrl()
+  : m_FindTimerID(0), m_csFind(_T(""))
 {
 }
 
@@ -30,14 +31,30 @@ BEGIN_MESSAGE_MAP(CPWListCtrl, CListCtrl)
   //{{AFX_MSG_MAP(CPWListCtrl)
   ON_MESSAGE(WM_CHAR, OnCharItemlist)
   ON_WM_DESTROY()
+  ON_WM_TIMER()
   //}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 LRESULT CPWListCtrl::OnCharItemlist(WPARAM wParam, LPARAM /* lParam */)
 {
-  //DboxMain *pDbx = static_cast<DboxMain *>(GetParent());
-  CString cs_char = (TCHAR)wParam;
-  m_pDbx->FindNext(cs_char);
+  const int iSubItem = m_pDbx->IsImageVisible() ? 1 : 0;
+  bool bFirst;
+  if (m_FindTimerID != 0) {
+    KillTimer(TIMER_FIND);
+    m_csFind += (TCHAR)wParam;
+    bFirst = false;
+  } else {
+    m_csFind = (TCHAR)wParam;
+    bFirst = true;
+  }
+  if (!FindNext(m_csFind, iSubItem) && !bFirst) {
+    // Didn't find a match when more than one character
+    // Emulate CListCtrl and try again (once) with this matching the first character
+    m_csFind = (TCHAR)wParam;
+    FindNext(m_csFind, iSubItem);
+  }
+  // Set timer going again
+  m_FindTimerID = SetTimer(TIMER_FIND, 1000, NULL);
   return 0L;
 }
 
@@ -46,4 +63,64 @@ void CPWListCtrl::OnDestroy()
   // Remove dummy ImageList. PWTreeCtrl removes the real one!
   m_pDbx->m_pImageList0->DeleteImageList();
   delete m_pDbx->m_pImageList0;
+}
+
+void CPWListCtrl::OnTimer(UINT_PTR nIDEvent)
+{
+  if (nIDEvent == TIMER_FIND) {
+    KillTimer(TIMER_FIND);
+    m_FindTimerID = 0;
+  }
+}
+
+bool
+CPWListCtrl::FindNext(const CString &cs_find, const int iSubItem)
+{
+  int iItem;
+  bool bFound(false);
+  CString cs_text;
+  const int iNum = GetItemCount();
+  const int iFindLen = cs_find.GetLength();
+
+  // Get selected item, if any
+  POSITION pos = GetFirstSelectedItemPosition();
+
+  // First search down.
+  if (pos == NULL)
+    iItem = 0;
+  else
+    iItem = (int)pos;
+
+  do {
+    cs_text = GetItemText(iItem, iSubItem);
+    cs_text = cs_text.Mid(0, iFindLen);
+    if (cs_text.GetLength() > 0 && cs_find.CompareNoCase(cs_text) == 0) {
+      bFound = true;
+      break;
+    }
+    iItem++;
+  } while (iItem <= iNum);
+
+  // Not found searching down and we didn't start from the top, now start from the top until
+  // we get to where we started!
+  if (!bFound && pos != NULL) {
+    iItem = 0;
+    do {
+      cs_text = GetItemText(iItem, iSubItem);
+      cs_text = cs_text.Mid(0, iFindLen);
+      if (cs_text.GetLength() > 0 && cs_find.CompareNoCase(cs_text) == 0) {
+        bFound = true;
+        break;
+      }
+      iItem++;
+    } while (iItem != (int)pos);
+  }
+
+  if (bFound) {
+    SetItemState(iItem, LVIS_FOCUSED | LVIS_SELECTED, LVIS_FOCUSED | LVIS_SELECTED);
+    EnsureVisible(iItem, FALSE);
+    Invalidate();
+  }
+
+  return bFound;
 }
