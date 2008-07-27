10623ff42637adbcbb6d5086166b1482c73cb412
DrK <c-273@users.sourceforge.net>
2008-07-27 17:41:42 +0000

    Add missing files
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@2080 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/ViewReport.h
@@ -0,0 +1,49 @@
+/*
+* Copyright (c) 2003-2008 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+
+#pragma once
+
+#include "PWResizeDialog.h"
+#include "corelib/report.h"
+
+// CViewReport dialog
+
+class DboxMain;
+
+class CViewReport : public CPWResizeDialog
+{
+	DECLARE_DYNAMIC(CViewReport)
+
+public:
+	CViewReport(CWnd* pParent = NULL,
+    CReport *pRpt = NULL);   // standard constructor
+	virtual ~CViewReport();
+
+// Dialog Data
+	enum { IDD = IDD_VIEWREPORT };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+  virtual BOOL OnInitDialog();
+
+  DboxMain *m_pDbx;
+  CReport *m_pRpt;
+  BYTE *m_pData;
+  DWORD m_dwDatasize;
+  CEdit m_editreport;
+  CBrush m_backgroundbrush;
+  COLORREF m_textcolor, m_backgroundcolour;
+
+
+  afx_msg void Save();
+  afx_msg void Finish();
+  afx_msg void SendToClipBoard();
+  afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+
+  DECLARE_MESSAGE_MAP()
+};
