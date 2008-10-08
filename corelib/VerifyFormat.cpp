83f1d0026a0ca38d3c6e2c255b80a9ce0f3f4006
Rony Shapiro <ronys@users.sourceforge.net>
2008-10-08 22:37:24 +0000

    Cleanup & portability - WIP checkpoint
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@2215 1f79f812-37fb-46fe-a122-30589dd2bf55

--- corelib/VerifyFormat.cpp
+++ corelib/VerifyFormat.cpp
@@ -1,54 +1,12 @@
 #include "VerifyFormat.h"
 #include "corelib.h"
+#include "StringXStream.h"
 
-bool VerifyImportDateTimeString(const stringT &time_str, time_t &t)
+static bool verifyDTvalues(int yyyy, int mon, int dd,
+                           int hh, int min, int ss)
 {
-  //  String format must be "yyyy/mm/dd hh:mm:ss"
-  //                        "0123456789012345678"
-
-  stringT xtime_str;
-  const int month_lengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  const int idigits[14] = {0, 1, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18};
-  const int ndigits = 14;
-  int yyyy, mon, dd, hh, min, ss, nscanned;
-
-  t = (time_t)-1;
-
-  if (time_str.length() != 19)
-    return false;
-
-  // Validate time_str
-  if (time_str.substr(4,1) != _S("/") ||
-      time_str.substr(7,1) != _S("/") ||
-      time_str.substr(10,1) != _S(" ") ||
-      time_str.substr(13,1) != _S(":") ||
-      time_str.substr(16,1) != _S(":"))
-    return false;
-
-  for (int i = 0;  i < ndigits; i++)
-    if (!isdigit(time_str[idigits[i]]))
-      return false;
-
-  // Since white space is ignored with _stscanf, first verify that there are no invalid '#' characters
-  // Then take copy of the string and replace all blanks by '#' (should only be 1)
-  if (time_str.find(TCHAR('#')) != stringT::npos)
-    return false;
-
-  xtime_str = time_str;
-  if (Replace(xtime_str, TCHAR(' '), TCHAR('#')) != 1)
-    return false;
-
-#if _MSC_VER >= 1400
-  nscanned = _stscanf_s(xtime_str.c_str(), _T("%4d/%2d/%2d#%2d:%2d:%2d"),
-                        &yyyy, &mon, &dd, &hh, &min, &ss);
-#else
-  nscanned = _stscanf(xtime_str.c_str(), _T("%4d/%2d/%2d#%2d:%2d:%2d"),
-                      &yyyy, &mon, &dd, &hh, &min, &ss);
-#endif
-
-  if (nscanned != 6)
-    return false;
-
+  const int month_lengths[12] = {31, 28, 31, 30, 31, 30,
+                                 31, 31, 30, 31, 30, 31};
   // Built-in obsolesence for pwsafe in 2038?
   if (yyyy < 1970 || yyyy > 2038)
     return false;
@@ -69,6 +27,45 @@ bool VerifyImportDateTimeString(const stringT &time_str, time_t &t)
   if ((hh < 0 || hh > 23) ||
       (min < 0 || min > 59) ||
       (ss < 0 || ss > 59))
+    return false;
+
+  return true;
+}
+
+
+bool VerifyImportDateTimeString(const stringT &time_str, time_t &t)
+{
+  //  String format must be "yyyy/mm/dd hh:mm:ss"
+  //                        "0123456789012345678"
+
+  const int ndigits = 14;
+  const int idigits[ndigits] = {0, 1, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18};
+  int yyyy, mon, dd, hh, min, ss;
+
+  t = (time_t)-1;
+
+  if (time_str.length() != 19)
+    return false;
+
+  // Validate time_str
+  if (time_str[4] != TCHAR('/') ||
+      time_str[7] != TCHAR('/') ||
+      time_str[10] != TCHAR(' ') ||
+      time_str[13] != TCHAR(':') ||
+      time_str[16] != TCHAR(':'))
+    return false;
+
+  for (int i = 0;  i < ndigits; i++)
+    if (!isdigit(time_str[idigits[i]]))
+      return false;
+
+  istringstreamT is(time_str);
+  TCHAR dummy;
+
+  is >> yyyy >> dummy >> mon >> dummy >> dd
+     >> hh >> dummy >> min >> dummy >> ss;
+
+  if (!verifyDTvalues(yyyy, mon, dd, hh, min, ss))
     return false;
 
   // Accept 01/01/1970 as a special 'unset' value, otherwise there can be
@@ -89,18 +86,14 @@ bool VerifyASCDateTimeString(const stringT &time_str, time_t &t)
 {
   //  String format must be "ddd MMM dd hh:mm:ss yyyy"
   //                        "012345678901234567890123"
+  // e.g.,                  "Wed Oct 06 21:02:38 2008"
 
-  const int month_lengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
   const stringT str_months = _T("JanFebMarAprMayJunJulAugSepOctNovDec");
   const stringT str_days = _T("SunMonTueWedThuFriSat");
-  stringT xtime_str;
-  TCHAR cmonth[4], cdayofweek[4];
-  const int idigits[12] = {8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 22, 23};
   const int ndigits = 12;
-  stringT::size_type iMON, iDOW, nscanned;
+  const int idigits[ndigits] = {8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 22, 23};
+  stringT::size_type iMON, iDOW;
   int yyyy, mon, dd, hh, min, ss;
-
-  cmonth[3] = cdayofweek[3] = TCHAR('\0');
 
   t = (time_t)-1;
 
@@ -108,61 +101,26 @@ bool VerifyASCDateTimeString(const stringT &time_str, time_t &t)
     return false;
 
   // Validate time_str
-  if (time_str.substr(13,1) != _S(":") ||
-      time_str.substr(16,1) != _S(":"))
+  if (time_str[13] != TCHAR(':') ||
+      time_str[16] != TCHAR(':'))
     return false;
 
   for (int i = 0; i < ndigits; i++)
     if (!isdigit(time_str[idigits[i]]))
       return false;
 
-  // Since white space is ignored with _stscanf, first verify that there are no invalid '#' characters
-  // Then take copy of the string and replace all blanks by '#' (should be 4)
-  if (time_str.find(TCHAR('#')) != stringT::npos)
-    return false;
+  istringstreamT is(time_str);
+  stringT dow, mon_str;
+  TCHAR dummy;
+  is >> dow >> mon_str >> dd >> hh >> dummy >> min >> dummy >> ss >> yyyy;
 
-  xtime_str = time_str;
-  if (Replace(xtime_str, TCHAR(' '), TCHAR('#')) != 4)
-    return false;
-
-#if _MSC_VER >= 1400
-  nscanned = _stscanf_s(xtime_str.c_str(), _T("%3c#%3c#%2d#%2d:%2d:%2d#%4d"),
-                        cdayofweek, sizeof(cdayofweek), cmonth, sizeof(cmonth),
-                        &dd, &hh, &min, &ss, &yyyy);
-#else
-  nscanned = _stscanf(xtime_str.c_str(), _T("%3c#%3c#%2d#%2d:%2d:%2d#%4d"),
-                      cdayofweek, cmonth, &dd, &hh, &min, &ss, &yyyy);
-#endif
-
-  if (nscanned != 7)
-    return false;
-
-  iMON = str_months.find(cmonth);
+  iMON = str_months.find(mon_str);
   if (iMON == stringT::npos)
     return false;
 
   mon = (iMON / 3) + 1;
 
-  // Built-in obsolesence for pwsafe in 2038?
-  if (yyyy < 1970 || yyyy > 2038)
-    return false;
-
-  if ((mon < 1 || mon > 12) || (dd < 1))
-    return false;
-
-  if (mon == 2 && (yyyy % 4) == 0) {
-    // Feb and a leap year
-    if (dd > 29)
-      return false;
-  } else {
-    // Either (Not Feb) or (Is Feb but not a leap-year)
-    if (dd > month_lengths[mon - 1])
-      return false;
-  }
-
-  if ((hh < 0 || hh > 23) ||
-      (min < 0 || min > 59) ||
-      (ss < 0 || ss > 59))
+  if (!verifyDTvalues(yyyy, mon, dd, hh, min, ss))
     return false;
 
   // Accept 01/01/1970 as a special 'unset' value, otherwise there can be
@@ -174,7 +132,7 @@ bool VerifyASCDateTimeString(const stringT &time_str, time_t &t)
 
   const CTime ct(yyyy, mon, dd, hh, min, ss, -1);
 
-  iDOW = str_days.find(cdayofweek);
+  iDOW = str_days.find(dow);
   if (iDOW == stringT::npos)
     return false;
 
@@ -191,12 +149,13 @@ bool VerifyXMLDateTimeString(const stringT &time_str, time_t &t)
 {
   //  String format must be "yyyy-mm-ddThh:mm:ss"
   //                        "0123456789012345678"
+  // e.g.,                  "2008-10-06T21:20:56"
 
   stringT xtime_str;
-  const int month_lengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  const int idigits[14] = {0, 1, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18};
+
   const int ndigits = 14;
-  int yyyy, mon, dd, hh, min, ss, nscanned;
+  const int idigits[ndigits] = {0, 1, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18};
+  int yyyy, mon, dd, hh, min, ss;
 
   t = (time_t)-1;
 
@@ -204,11 +163,11 @@ bool VerifyXMLDateTimeString(const stringT &time_str, time_t &t)
     return false;
 
   // Validate time_str
-  if (time_str.substr(4,1) != _S("-") ||
-      time_str.substr(7,1) != _S("-") ||
-      time_str.substr(10,1) != _S("T") ||
-      time_str.substr(13,1) != _S(":") ||
-      time_str.substr(16,1) != _S(":"))
+  if (time_str[4] != TCHAR('-') ||
+      time_str[7] != TCHAR('-') ||
+      time_str[10] != TCHAR('T') ||
+      time_str[13] != TCHAR(':') ||
+      time_str[16] != TCHAR(':'))
     return false;
 
   for (int i = 0; i < ndigits; i++) {
@@ -216,50 +175,12 @@ bool VerifyXMLDateTimeString(const stringT &time_str, time_t &t)
       return false;
   }
 
-  // Since white space is ignored with _stscanf, first verify that there are no invalid '#' characters
-  // and no blanks.  Replace '-' & 'T' by '#'.
-  if (time_str.find(TCHAR('#')) != stringT::npos)
-    return false;
-  if (time_str.find(TCHAR(' ')) != stringT::npos)
-    return false;
+  istringstreamT is(time_str);
+  TCHAR dummy;
+  is >> yyyy >> dummy >> mon >> dummy >> dd >> dummy
+      >> hh >> dummy >> min >> dummy >> ss;
 
-  xtime_str = time_str;
-  if (Replace(xtime_str, TCHAR('-'), TCHAR('#')) != 2)
-    return false;
-  if (Replace(xtime_str, TCHAR('T'), TCHAR('#')) != 1)
-    return false;
-
-#if _MSC_VER >= 1400
-  nscanned = _stscanf_s(xtime_str.c_str(), _T("%4d#%2d#%2d#%2d:%2d:%2d"),
-                        &yyyy, &mon, &dd, &hh, &min, &ss);
-#else
-  nscanned = _stscanf(xtime_str.c_str(), _T("%4d#%2d#%2d#%2d:%2d:%2d"),
-                      &yyyy, &mon, &dd, &hh, &min, &ss);
-#endif
-
-  if (nscanned != 6)
-    return false;
-
-  // Built-in obsolesence for pwsafe in 2038?
-  if (yyyy < 1970 || yyyy > 2038)
-    return false;
-
-  if ((mon < 1 || mon > 12) || (dd < 1))
-    return false;
-
-  if (mon == 2 && (yyyy % 4) == 0) {
-    // Feb and a leap year
-    if (dd > 29)
-      return false;
-  } else {
-    // Either (Not Feb) or (Is Feb but not a leap-year)
-    if (dd > month_lengths[mon - 1])
-      return false;
-  }
-
-  if ((hh < 0 || hh > 23) ||
-      (min < 0 || min > 59) ||
-      (ss < 0 || ss > 59))
+  if (!verifyDTvalues(yyyy, mon, dd, hh, min, ss))
     return false;
 
   // Accept 01/01/1970 as a special 'unset' value, otherwise there can be
@@ -282,10 +203,9 @@ bool VerifyXMLDateString(const stringT &time_str, time_t &t)
   //                        "0123456789"
 
   stringT xtime_str;
-  const int month_lengths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  const int idigits[14] = {0, 1, 2, 3, 5, 6, 8, 9};
   const int ndigits = 8;
-  int yyyy, mon, dd, nscanned;
+  const int idigits[ndigits] = {0, 1, 2, 3, 5, 6, 8, 9};
+  int yyyy, mon, dd;
 
   t = (time_t)-1;
 
@@ -302,44 +222,13 @@ bool VerifyXMLDateString(const stringT &time_str, time_t &t)
       return false;
   }
 
-  // Since white space is ignored with _stscanf, first verify that there are no invalid '#' characters
-  // and no blanks.  Replace '-' by '#'.
-  if (time_str.find(TCHAR('#')) != stringT::npos)
-    return false;
-  if (time_str.find(TCHAR(' ')) != stringT::npos)
-    return false;
+  istringstreamT is(time_str);
+  TCHAR dummy;
 
-  xtime_str = time_str;
-  if (Replace(xtime_str, TCHAR('-'), TCHAR('#')) != 2)
+  is >> yyyy >> dummy >> mon >> dummy >> dd;
+
+  if (!verifyDTvalues(yyyy, mon, dd, 1, 2, 3))
     return false;
-
-#if _MSC_VER >= 1400
-  nscanned = _stscanf_s(xtime_str.c_str(), _T("%4d#%2d#%2d"),
-                        &yyyy, &mon, &dd);
-#else
-  nscanned = _stscanf(xtime_str.c_str(), _T("%4d#%2d#%2d"),
-                      &yyyy, &mon, &dd);
-#endif
-
-  if (nscanned != 3)
-    return false;
-
-  // Built-in obsolesence for pwsafe in 2038?
-  if (yyyy < 1970 || yyyy > 2038)
-    return false;
-
-  if ((mon < 1 || mon > 12) || (dd < 1))
-    return false;
-
-  if (mon == 2 && (yyyy % 4) == 0) {
-    // Feb and a leap year
-    if (dd > 29)
-      return false;
-  } else {
-    // Either (Not Feb) or (Is Feb but not a leap-year)
-    if (dd > month_lengths[mon - 1])
-      return false;
-  }
 
   // Accept 01/01/1970 as a special 'unset' value, otherwise there can be
   // issues with CTime constructor after apply daylight savings offset.
