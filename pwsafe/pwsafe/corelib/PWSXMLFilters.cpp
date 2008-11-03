782ff04468e57c40e094a10682846de69f996f79
DrK <c-273@users.sourceforge.net>
2008-11-03 10:27:07 +0000

    More Format String issues
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@2271 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/corelib/PWSXMLFilters.cpp
+++ pwsafe/pwsafe/corelib/PWSXMLFilters.cpp
@@ -219,13 +219,13 @@ bool PWSXMLFilters::XMLFilterProcess(const bool &bvalidation,
         m_strResultText = pEH->m_strValidationResult;
       } else {
         Format(m_strResultText, IDSC_XMLPARSEERROR, m_MSXML_Version, hr,
-               m_bValidation ? cs_validation : cs_import);
+               m_bValidation ? cs_validation.c_str() : cs_import.c_str());
       }
     }  // End Check for parsing errors
 
   } else {
     Format(m_strResultText, IDSC_XMLBADCREATESCHEMA, m_MSXML_Version, hr,
-           m_bValidation ? cs_validation : cs_import);
+           m_bValidation ? cs_validation.c_str() : cs_import.c_str());
   }  // End Create Schema Cache
 
 exit:
