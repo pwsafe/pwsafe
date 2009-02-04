001aa0897920fee58aa64bf245e4628147526307
Rony Shapiro <ronys@users.sourceforge.net>
2009-02-04 20:39:42 +0000

    wxWidgets - added version info
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@2414 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ src/ui/wxWidgets/version.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2003-2009 Rony Shapiro <ronys@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+
+/** \file version.cpp
+ * 
+ */
+
+#include "version.h"
+
+const wxString pwsafeAppName(APPNAME);
+#ifndef _DEBUG
+const wxString pwsafeVersionString = wxString::Format("v%d.%d (%s) %s",
+                                                      MAJORVERSION, MINORVERSION,
+                                                      SVN_VERSION, SPECIALBUILD);
+#else
+const wxString pwsafeVersionString = wxString::Format("v%d.%d (%s) [debug] %s",
+                                                      MAJORVERSION, MINORVERSION,
+                                                      SVN_VERSION, SPECIALBUILD);
+#endif
