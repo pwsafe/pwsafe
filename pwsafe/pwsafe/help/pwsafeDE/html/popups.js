422d64598220f9272f8b861dcd4aaec354e1f7ec
Karel Van der Gucht <karlo_vdg@users.sourceforge.net>
2009-02-06 22:14:16 +0000

    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@2416 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/help/pwsafeDE/html/popups.js
@@ -0,0 +1,15 @@
+// ************ Andrew/Oct2004 ************
+// really just a list of variables
+// used by the ActiveX Help control and the popup method
+// All popup text should appear here for easy editing
+//
+// Modified: 15/06/2005
+// VdG 3.15 DE 31/01/2009
+popFont="Arial,9,,ITALIC"
+strPasswordPolicies="Ein Passwortrichtlinie bestimmt wie ein Passwort generiert wird. L&auml;nge, Buchstaben, Ziffern und spezielle Zeichen k&ouml;nnen auf einfache Art eingestellt werden im Optionsmen&uuml; f&uuml;r Passwortrichtlinien."
+strBruce="Autor von Applied Cryptography und andere B&uuml;cher, Erfinder des Blowfish Algorithmus, und Gr&uuml;nder und CTO von Counterpane Internet Security."
+strSpecialChars="   `   ~   !   @   #   $   %   ^   &   *   (   )   _   +   -   =   |   }   {   [   ]   <   >   ?   /   \   :   ;   |   "
+strCombination="In unserem Sprachgebrauch ist die Safekombination das Passwort das Verwendung findet um Zugriff auf die Password Safe Datenbank zu erlangen."
+strPopText1="Blinder Platzhalter"
+strExternalFile="Unterst&uuml;tzte Dateiformate: Version 1.x Password Safe Datenbank; mit Tab getrennte Text (.txt) Dateien. Es wird DRINGEND empfohlen das Verwenden von Textdateien mit sensible Passwortinformationen nach M&ouml;glichkeit zu vermeiden. Hinterher sollten diese Dateien sicher gel&ouml;scht und vernichtet werden."
+strHexDec="Nur diese:\n 0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f"
