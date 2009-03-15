457badad99e6845e2f605d9ba82b050c830817ca
Rony Shapiro <ronys@users.sourceforge.net>
2009-03-15 18:49:28 +0000

    minor, but quite overdue, cleanup
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@2535 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/src/ui/Windows/main.cpp
+++ pwsafe/pwsafe/src/ui/Windows/main.cpp
@@ -6,27 +6,10 @@
 * http://www.opensource.org/licenses/artistic-license-2.0.php
 */
 
-#include "model.h"
-#include "view.h"
-
-#ifdef WIN32
 /*
-The one and only ThisMfcApp object.  In the MFC world, creating
-this global object is what starts the application.
+  The one and only ThisMfcApp object.  In the MFC world, creating
+  this global object is what starts the application.
 */
 #include "ThisMfcApp.h"
 
 ThisMfcApp app;
-
-#else
-
-int main( int argc, char** argv)
-{
-  Model * model = Model::Instance();
-
-  model->Main();
-
-  return 0;
-}
-
-#endif
