2d86402697a79821ff49a7e0def33bc05fef50b7
DrK <c-273@users.sourceforge.net>
2009-04-18 12:02:34 +0000

    Add missing copyright comments
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@2623 1f79f812-37fb-46fe-a122-30589dd2bf55

--- src/os/windows/pws_autotype/pws_at.cpp
+++ src/os/windows/pws_autotype/pws_at.cpp
@@ -1,3 +1,10 @@
+/*
+* Copyright (c) 2003-2009 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
 // pws_at.cpp : Defines the entry point for the DLL application.
 //
 
