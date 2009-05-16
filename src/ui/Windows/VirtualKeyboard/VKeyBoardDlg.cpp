1d40f1b05bc1f7e8bd05fefdbea8d28f96877bdc
DrK <c-273@users.sourceforge.net>
2009-05-16 09:36:50 +0000

    Add vsprops files. Update configure.vbs. Remove un-needed include.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@2646 1f79f812-37fb-46fe-a122-30589dd2bf55

--- src/ui/Windows/VirtualKeyboard/VKeyBoardDlg.cpp
+++ src/ui/Windows/VirtualKeyboard/VKeyBoardDlg.cpp
@@ -12,7 +12,6 @@
 //-----------------------------------------------------------------------------
 
 #include "../stdafx.h"
-#include "../PwFont.h"
 
 #include "VKeyBoardDlg.h"
 #include "VKShiftState.h"
