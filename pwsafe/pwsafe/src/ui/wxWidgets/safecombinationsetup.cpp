0ba999190894038060158f308374fbb0f11a29dc
Rony Shapiro <ronys@users.sourceforge.net>
2009-08-13 19:46:09 +0000

    wxWidgets: 'make unicoderelease' now builds correctly.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@2828 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/src/ui/wxWidgets/safecombinationsetup.cpp
+++ pwsafe/pwsafe/src/ui/wxWidgets/safecombinationsetup.cpp
@@ -237,20 +237,20 @@ void CSafeCombinationSetup::OnOkClick( wxCommandEvent& event )
     // (also used in CPasskeyChangeDlg)
 #ifndef _DEBUG // for debug, we want no checks at all, to save time
     StringX errmess;
-    if (!CPasswordCharPool::CheckPassword(m_password.c_str, errmess)) {
+    if (!CPasswordCharPool::CheckPassword(m_password.c_str(), errmess)) {
       wxString cs_msg;
       cs_msg = _("Weak passphrase:\n\n");
       cs_msg += errmess.c_str();
 #ifndef PWS_FORCE_STRONG_PASSPHRASE
       cs_msg += _("\nUse it anyway?");
-      wxMessageBox mb(this, _("Warning"), cs_msg,
+      wxMessageDialog mb(this, _("Warning"), cs_msg,
                       wxYES_NO | wxNO_DEFAULT | wxICON_HAND);
       int rc = mb.ShowModal();
     if (rc == wxID_NO)
       return;
 #else
     cs_msg += _("\nPlease try another");
-    wxMessageBox mb(this, _("Error"), cs_msg, wxOK | wxICON_HAND);
+    wxMessageDialog mb(this, _("Error"), cs_msg, wxOK | wxICON_HAND);
     mb.ShowModal();
     return;
 #endif // PWS_FORCE_STRONG_PASSPHRASE
