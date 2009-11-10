d729a01255148df1a966c6b6310205facea82fdc
Karel Van der Gucht <karlo_vdg@users.sourceforge.net>
2009-11-10 00:35:15 +0000

    did some echoing. Helpful if processing is stuck (e.g. with an "old" Tortoise)
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@3012 1f79f812-37fb-46fe-a122-30589dd2bf55

--- src/ui/Windows/pre_build.cmd
+++ src/ui/Windows/pre_build.cmd
@@ -5,16 +5,21 @@ rem Requires environment variables ProjectDir & TortoiseSVNDir
 rem  set in UserVariables.vsprops
 
 rem Other processing can be added as required.
+rem VdG some echoing of the process being done (helps if it stucks)
 
-if not exist "%TortoiseSVNDir%\bin\SubWCRev.exe" goto :Error1
+: goto weiter
+if not exist "%TortoiseSVNDir%\bin\SubWCRev.exe" goto Error1
+echo executing: "%TortoiseSVNDir%\bin\SubWCRev.exe" ..\..\.. version.in version.h
 "%TortoiseSVNDir%\bin\SubWCRev.exe" ..\..\.. version.in version.h
-goto :Done
-:Error1
-if not exist "%ProjectDir%\version.h" goto :Error2
+goto Done
+: Error1
+if not exist "%ProjectDir%\version.h" goto Error2
+: weiter
+echo "%ProjectDir%\version.h" does not exist.
 echo "Using static version.h"
-goto :Done
-:Error2
+goto Done
+: Error2
 echo "Can't find TortoiseSVN's SubWCRev.exe"
 echo "Please install it or create version.h from version.in manually"
-:Done
+: Done
 echo .
