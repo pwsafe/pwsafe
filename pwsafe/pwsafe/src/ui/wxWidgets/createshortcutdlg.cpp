44fbecc689c288a7dc3ff1b28ec1ebe8e96e4ab8
Rony Shapiro <ronys@users.sourceforge.net>
2010-01-21 19:12:16 +0000

    wxWidgets: started work on create shortcut dlg
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@3189 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/src/ui/wxWidgets/createshortcutdlg.cpp
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2003-2010 Rony Shapiro <ronys@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+
+/** \file createshortcutdlg.cpp
+* 
+*/
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+////@begin includes
+////@end includes
+
+#include "createshortcutdlg.h"
+
+////@begin XPM images
+////@end XPM images
+
+
+/*!
+ * CreateShortcutDlg type definition
+ */
+
+IMPLEMENT_DYNAMIC_CLASS( CreateShortcutDlg, wxDialog )
+
+
+/*!
+ * CreateShortcutDlg event table definition
+ */
+
+BEGIN_EVENT_TABLE( CreateShortcutDlg, wxDialog )
+
+////@begin CreateShortcutDlg event table entries
+////@end CreateShortcutDlg event table entries
+
+END_EVENT_TABLE()
+
+
+/*!
+ * CreateShortcutDlg constructors
+ */
+
+CreateShortcutDlg::CreateShortcutDlg()
+{
+  Init();
+}
+
+CreateShortcutDlg::CreateShortcutDlg( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+  Init();
+  Create(parent, id, caption, pos, size, style);
+}
+
+
+/*!
+ * CreateShortcutDlg creator
+ */
+
+bool CreateShortcutDlg::Create( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+////@begin CreateShortcutDlg creation
+  SetExtraStyle(wxWS_EX_BLOCK_EVENTS);
+  wxDialog::Create( parent, id, caption, pos, size, style );
+
+  CreateControls();
+  if (GetSizer())
+  {
+    GetSizer()->SetSizeHints(this);
+  }
+  Centre();
+////@end CreateShortcutDlg creation
+  return true;
+}
+
+
+/*!
+ * CreateShortcutDlg destructor
+ */
+
+CreateShortcutDlg::~CreateShortcutDlg()
+{
+////@begin CreateShortcutDlg destruction
+////@end CreateShortcutDlg destruction
+}
+
+
+/*!
+ * Member initialisation
+ */
+
+void CreateShortcutDlg::Init()
+{
+////@begin CreateShortcutDlg member initialisation
+  m_groupCtrl = NULL;
+////@end CreateShortcutDlg member initialisation
+}
+
+
+/*!
+ * Control creation for CreateShortcutDlg
+ */
+
+void CreateShortcutDlg::CreateControls()
+{    
+////@begin CreateShortcutDlg content construction
+  CreateShortcutDlg* itemDialog1 = this;
+
+  wxBoxSizer* itemBoxSizer2 = new wxBoxSizer(wxVERTICAL);
+  itemDialog1->SetSizer(itemBoxSizer2);
+
+  wxStaticText* itemStaticText3 = new wxStaticText( itemDialog1, wxID_STATIC, _("Please specify the name & group\n for shortcut to "), wxDefaultPosition, wxDefaultSize, 0 );
+  itemBoxSizer2->Add(itemStaticText3, 0, wxALIGN_LEFT|wxALL, 5);
+
+  wxGridSizer* itemGridSizer4 = new wxGridSizer(3, 2, 0, 0);
+  itemBoxSizer2->Add(itemGridSizer4, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
+
+  wxStaticText* itemStaticText5 = new wxStaticText( itemDialog1, wxID_STATIC, _("Group:"), wxDefaultPosition, wxDefaultSize, 0 );
+  itemGridSizer4->Add(itemStaticText5, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+  wxArrayString m_groupCtrlStrings;
+  m_groupCtrl = new wxComboBox( itemDialog1, ID_COMBOBOX4, wxEmptyString, wxDefaultPosition, wxDefaultSize, m_groupCtrlStrings, wxCB_DROPDOWN );
+  itemGridSizer4->Add(m_groupCtrl, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+  wxStaticText* itemStaticText7 = new wxStaticText( itemDialog1, wxID_STATIC, _("Title:"), wxDefaultPosition, wxDefaultSize, 0 );
+  itemGridSizer4->Add(itemStaticText7, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+  wxTextCtrl* itemTextCtrl8 = new wxTextCtrl( itemDialog1, ID_TEXTCTRL18, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
+  itemGridSizer4->Add(itemTextCtrl8, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+  wxStaticText* itemStaticText9 = new wxStaticText( itemDialog1, wxID_STATIC, _("Username:"), wxDefaultPosition, wxDefaultSize, 0 );
+  itemGridSizer4->Add(itemStaticText9, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+  wxTextCtrl* itemTextCtrl10 = new wxTextCtrl( itemDialog1, ID_TEXTCTRL19, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
+  itemGridSizer4->Add(itemTextCtrl10, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+  wxStdDialogButtonSizer* itemStdDialogButtonSizer11 = new wxStdDialogButtonSizer;
+
+  itemBoxSizer2->Add(itemStdDialogButtonSizer11, 0, wxGROW|wxALL, 5);
+  wxButton* itemButton12 = new wxButton( itemDialog1, wxID_OK, _("&OK"), wxDefaultPosition, wxDefaultSize, 0 );
+  itemStdDialogButtonSizer11->AddButton(itemButton12);
+
+  wxButton* itemButton13 = new wxButton( itemDialog1, wxID_CANCEL, _("&Cancel"), wxDefaultPosition, wxDefaultSize, 0 );
+  itemStdDialogButtonSizer11->AddButton(itemButton13);
+
+  wxButton* itemButton14 = new wxButton( itemDialog1, wxID_HELP, _("&Help"), wxDefaultPosition, wxDefaultSize, 0 );
+  itemStdDialogButtonSizer11->AddButton(itemButton14);
+
+  itemStdDialogButtonSizer11->Realize();
+
+  // Set validators
+  itemStaticText3->SetValidator( wxGenericValidator(& m_heading) );
+  itemTextCtrl8->SetValidator( wxGenericValidator(& m_title) );
+  itemTextCtrl10->SetValidator( wxGenericValidator(& m_user) );
+////@end CreateShortcutDlg content construction
+}
+
+
+/*!
+ * Should we show tooltips?
+ */
+
+bool CreateShortcutDlg::ShowToolTips()
+{
+  return true;
+}
+
+/*!
+ * Get bitmap resources
+ */
+
+wxBitmap CreateShortcutDlg::GetBitmapResource( const wxString& name )
+{
+  // Bitmap retrieval
+////@begin CreateShortcutDlg bitmap retrieval
+  wxUnusedVar(name);
+  return wxNullBitmap;
+////@end CreateShortcutDlg bitmap retrieval
+}
+
+/*!
+ * Get icon resources
+ */
+
+wxIcon CreateShortcutDlg::GetIconResource( const wxString& name )
+{
+  // Icon retrieval
+////@begin CreateShortcutDlg icon retrieval
+  wxUnusedVar(name);
+  return wxNullIcon;
+////@end CreateShortcutDlg icon retrieval
+}
