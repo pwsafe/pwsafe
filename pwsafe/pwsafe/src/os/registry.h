01b4250a4d70f0fa4b40f8c3cae27605c02fd0bd
Rony Shapiro <ronys@users.sourceforge.net>
2010-03-12 20:27:55 +0000

    refactor registry-related stuff to support wx over Windows
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@3356 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/src/os/registry.h
@@ -0,0 +1,43 @@
+/*
+* Copyright (c) 2003-2010 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+
+/**
+ *  Abstract registry functions:
+ * Dummy for Linux,
+ * MFC or wxWidget base for Windows
+ */
+
+#ifndef __REGISTRY_H
+#define __REGISTRY_H
+#include "typedefs.h" // for TCHAR
+namespace pws_os {
+  bool RegCheckExists(const TCHAR *stree = NULL); // check for app's registry subtree by default
+  bool RegWriteValue(const TCHAR *section, const TCHAR *entry, int value);
+  inline bool RegWriteValue(const TCHAR *section, const TCHAR *entry, bool value)
+  { return RegWriteValue(section, entry, value ? 1 : 0); }
+  bool RegWriteValue(const TCHAR *section, const TCHAR *entry, const TCHAR *value);
+  bool RegDeleteEntry(const TCHAR *name);
+  int RegReadValue(const TCHAR *section, const TCHAR *entry, int value);
+  inline bool RegReadValue(const TCHAR *section, const TCHAR *entry, bool value)
+  { return RegReadValue(section, entry, value ? 1 : 0) != 0; }
+  const TCHAR *RegReadValue(const TCHAR *section, const TCHAR *entry, const TCHAR *value);
+  void RegDeleteSubtree(const TCHAR *stree);
+  // Following should be called in following order:
+  // RegOpenSubtree(); RegReadSTValue() (repeatedly); RegCloseSubTree()
+  // Not very elegant, but needed only for importing old prefs
+  bool RegOpenSubtree(const TCHAR *stree);
+  bool RegReadSTValue(const TCHAR *name, bool &value);
+  bool RegReadSTValue(const TCHAR *name, int &value);
+  bool RegReadSTValue(const TCHAR *name, stringT &value);
+  bool RegCloseSubtree();
+};
+#endif /* __REGISTRY_H */
+//-----------------------------------------------------------------------------
+// Local variables:
+// mode: c++
+// End:
---
+++ pwsafe/pwsafe/src/os/windows/registry.cpp
@@ -0,0 +1,251 @@
+/*
+* Copyright (c) 2003-2010 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+
+/**
+ * \file Windows-specific implementation of registry.h
+ *
+ * Actually contains two implementations:
+ * One for Windows/MFC, and one for wxWidgets
+ */
+
+#include "../typedefs.h"
+#include "../registry.h"
+#include "../debug.h"
+
+#ifndef __WX__
+bool pws_os::RegCheckExists(const TCHAR *stree)
+{
+  if (stree == NULL)
+    stree = ::AfxGetApp()->m_pszRegistryKey;
+
+  const stringT csSubkey = _T("Software\\") + stringT(stree);
+  HKEY hSubkey;
+  bool bExists = ::RegOpenKeyEx(HKEY_CURRENT_USER, csSubkey.c_str(), 0L,
+                                KEY_READ, &hSubkey) == ERROR_SUCCESS;
+  if (bExists)
+    ::RegCloseKey(hSubkey);
+  return bExists;
+}
+
+bool pws_os::RegWriteValue(const TCHAR *section, const TCHAR *entry, int value)
+{
+  return ::AfxGetApp()->WriteProfileInt(section, entry, value) == TRUE;
+}
+
+bool pws_os::RegWriteValue(const TCHAR *section, const TCHAR *entry,
+                           const TCHAR *value)
+{
+  return ::AfxGetApp()->WriteProfileString(section, entry, value) == TRUE;
+}
+
+bool pws_os::RegDeleteEntry(const TCHAR *name)
+{
+  HKEY hSubkey;
+  DWORD dwResult, dwType;
+  bool bRetVal;
+
+  // Keys in registry are in:
+  // "HKEY_CURRENT_USER\Software\Password Safe\Password Safe\"
+  const stringT csSubkey = _T("Software\\") + 
+    stringT(::AfxGetApp()->m_pszRegistryKey)
+    + _T("\\") +
+    stringT(::AfxGetApp()->m_pszRegistryKey);
+
+  dwResult = RegOpenKeyEx(HKEY_CURRENT_USER,
+                          csSubkey.c_str(),
+                          NULL,
+                          KEY_ALL_ACCESS,
+                          &hSubkey);
+
+  if (dwResult != ERROR_SUCCESS)
+    return false; // may have been called due to OldPrefs
+
+  dwResult = RegQueryValueEx(hSubkey, name, NULL, &dwType, NULL, NULL);
+  if (dwResult == ERROR_SUCCESS) {
+    // Was there - now delete it
+    dwResult = RegDeleteValue(hSubkey, name);
+    ASSERT(dwResult == ERROR_SUCCESS);
+    bRetVal = (dwResult == ERROR_SUCCESS);
+  } else
+    bRetVal = true;
+
+  dwResult = RegCloseKey(hSubkey);
+  ASSERT(dwResult == ERROR_SUCCESS);
+  return bRetVal;
+}
+
+int pws_os::RegReadValue(const TCHAR *section, const TCHAR *entry, int value)
+{
+  return ::AfxGetApp()->GetProfileInt(section, entry, value);
+}
+
+const TCHAR *pws_os::RegReadValue(const TCHAR *section, const TCHAR *entry,
+                                  const TCHAR *value)
+{
+  return ::AfxGetApp()->GetProfileString(section, entry, value);
+}
+
+void pws_os::RegDeleteSubtree(const TCHAR *stree)
+{
+  HKEY hSubkey;
+  LONG dw = ::RegOpenKeyEx(HKEY_CURRENT_USER,
+                           _T("Software"),
+                           NULL,
+                           KEY_ALL_ACCESS,
+                           &hSubkey);
+  if (dw != ERROR_SUCCESS) {
+    pws_os::Trace0(_T("pws_os::RegDeleteSubtree: RegOpenKeyEx failed\n"));
+    return;
+  }
+
+  dw = ::AfxGetApp()->DelRegTree(hSubkey, stree);
+  if (dw != ERROR_SUCCESS) {
+    pws_os::Trace0(_T("pws_os::RegDeleteSubtree: DelRegTree failed\n"));
+  }
+  dw = ::RegCloseKey(hSubkey);
+  if (dw != ERROR_SUCCESS) {
+    pws_os::Trace0(_T("pws_os::RegDeleteSubtree: RegCloseKey failed\n"));
+  }
+}
+
+static HKEY hSubkey; // not re-entrant, but who cares?
+
+bool pws_os::RegOpenSubtree(const TCHAR *stree)
+{
+  const stringT streeT(stree);
+  stringT OldAppKey(_T("Software\\"));
+  OldAppKey += streeT; OldAppKey += _T("\\Password Safe");
+  LONG dw = ::RegOpenKeyEx(HKEY_CURRENT_USER,
+                           OldAppKey.c_str(),
+                           NULL,
+                           KEY_ALL_ACCESS,
+                           &hSubkey);
+  return (dw == ERROR_SUCCESS);
+}
+
+bool pws_os::RegReadSTValue(const TCHAR *name, bool &value)
+{
+  int v;
+  bool retval = RegReadSTValue(name, v);
+  if (retval)
+    value = (v != 0);
+  return retval;
+}
+
+bool pws_os::RegReadSTValue(const TCHAR *name, int &value)
+{
+  bool retval = false;
+  LONG rv;
+  DWORD dwType, vData, DataLen(sizeof(vData));
+  rv = ::RegQueryValueEx(hSubkey,
+                         name,
+                         NULL,
+                         &dwType,
+                         LPBYTE(&vData),
+                         &DataLen);
+  if (rv == ERROR_SUCCESS && dwType == REG_DWORD) {
+    value = vData;
+    retval = true;
+  }
+  return retval;
+}
+
+bool pws_os::RegReadSTValue(const TCHAR *name, stringT &value)
+{
+  bool retval = false;
+  LONG rv;
+  DWORD dwType, DataLen;
+  rv = ::RegQueryValueEx(hSubkey,
+                         name,
+                         NULL,
+                         &dwType,
+                         NULL,
+                         &DataLen);
+  if (rv == ERROR_SUCCESS && dwType == REG_SZ) {
+        DataLen++;
+        TCHAR *pData = new TCHAR[DataLen];
+        ::memset(pData, 0, DataLen);
+        rv = ::RegQueryValueEx(hSubkey,
+                               name,
+                               NULL,
+                               &dwType,
+                               LPBYTE(pData),
+                               &DataLen);
+
+        if (rv == ERROR_SUCCESS) {
+          value = pData;
+          retval = true;
+        }
+        delete[] pData;
+  }
+  return retval;
+}
+
+bool pws_os::RegCloseSubtree()
+{
+  return (::RegCloseKey(hSubkey) == ERROR_SUCCESS);
+}
+
+#else /* __WX__ */
+
+// XXX All TBD...
+
+bool pws_os::RegCheckExists(const TCHAR *)
+{
+  return false;
+}
+
+bool pws_os::RegWriteValue(const TCHAR *, const TCHAR *, int)
+{
+  return false;
+}
+
+bool pws_os::RegWriteValue(const TCHAR *, const TCHAR *, const TCHAR *)
+{
+  return false;
+}
+bool pws_os::RegDeleteEntry(const TCHAR *)
+{
+  return false;
+}
+
+int pws_os::RegReadValue(const TCHAR *, const TCHAR *, int value)
+{
+  return value;
+}
+
+const pws_os::TCHAR *RegReadValue(const TCHAR *, const TCHAR *, const TCHAR *value)
+{
+  return value;
+}
+
+void pws_os::RegDeleteSubtree(const TCHAR *)
+{
+}
+
+bool pws_os::RegOpenSubtree(const TCHAR *)
+{
+  return false;
+}
+bool pws_os::RegReadSTValue(const TCHAR *, bool &)
+{
+  return false;
+}
+bool pws_os::RegReadSTValue(const TCHAR *, int &)
+{
+  return false;
+}
+bool pws_os::RegReadSTValue(const TCHAR *, stringT &)
+{
+  return false;
+}
+bool pws_os::RegCloseSubtree()
+{
+  return false;
+}
+#endif /* __WX__ */
