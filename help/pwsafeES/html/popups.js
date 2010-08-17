918caa60b331927a1ea9ec581c98b4ad2c1c5d53
Rony Shapiro <ronys@users.sourceforge.net>
2010-08-17 04:45:18 +0000

    More work on Spanish texts + added Spanish help files. Thanks to Jose.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@3622 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ help/pwsafeES/html/popups.js
@@ -0,0 +1,22 @@
+// ************ Andrew/Oct2004 ************
+// really just a list of variables
+// used by the ActiveX Help control and the popup method
+// All popup text should appear here for easy editing
+//
+// Modified: 07/08/2010
+
+popFont="Arial,9,,ITALIC"
+
+strPasswordPolicies="Una política de contraseñas determina como se crea la contraseña. La longitud, letras, números, y caracteres especiales que se usan para generar una contraseña se controlan en el diálogo Política de contraseñas del menú Opciones."
+
+strBruce="Autor de Criptografía aplicada y otros libros, creador del algoritmo Blowfish, y fundador y CTO de Counterpane Internet Security."
+
+strSpecialChars="   `   ~   !   @   #   $   %   ^   &   *   (   )   _   +   -   =   |   }   {   [   ]   <   >   ?   /   \   :   ;   |   "
+
+strCombination="En nuestra terminología, la combinación es la contraseñas que usas para acceder a la base de datos Password safe."
+
+strPopText1="Dummy Placeholder"
+
+strExternalFile="Formatos de archivo soportados: Base de datos Password Safe Versión 1.x; archivos de texto delimitado por tabuladores (.txt). Se recomienda CON INSISTENCIA que el uso de archivos de texto con información sensible de contraseñas sea evitada en lo posible y que el archivo sea borrado con seguridad después."
+
+strHexDec="Sólo los siguientes:\n 0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f"
