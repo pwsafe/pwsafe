7cfa1750306aa3f2f3bf331a1c3de8485ba85100
Saurav Ghosh <sauravg@users.sourceforge.net>
2010-09-03 10:28:10 +0000

    DragBar for wxWidgets.  Also simplified the font selection code a bit.
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@3665 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ src/ui/wxWidgets/PWSDragBar.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2003-2010 Rony Shapiro <ronys@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+
+/** \file PWSDragBar.h
+ * 
+ * Derives from the generic CDragBar class to provide the interfaces 
+ * CDragBar uses to get the drag & drop text.  
+ * 
+ * That doesn't require it to derive from CDragBar, but derivation
+ * lets PasswordSafeFrame deal with just this class alone
+ */
+
+#ifndef __PWSDRAGBAR_H__
+#define __PWSDRAGBAR_H__
+
+#include "./dragbar.h"
+
+class PasswordSafeFrame;
+
+class PWSDragBar : public CDragBar, public CDragBar::IDragSourceTextProvider
+{
+  PasswordSafeFrame* m_frame;
+  
+public:
+  PWSDragBar(PasswordSafeFrame* frame);
+  ~PWSDragBar();
+
+  //PWSDragBar::IDragSourceTextProvider override
+  virtual wxString GetText(int id) const;
+  virtual bool IsEnabled(int id) const;
+
+  DECLARE_CLASS(PWSDragBar)
+};
+
+#endif // __PWSDRAGBAR_H__
