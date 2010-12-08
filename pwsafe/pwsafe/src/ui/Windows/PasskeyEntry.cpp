f2f8b419ae66f388c746d336102d295f7443a196
DrK <c-273@users.sourceforge.net>
2010-12-08 17:20:31 +0000

    Fix crash in 3.24 - BR 3132055, 3132347 and bump to 3.24.1
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@3844 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/src/ui/Windows/PasskeyEntry.cpp
+++ pwsafe/pwsafe/src/ui/Windows/PasskeyEntry.cpp
@@ -328,10 +328,12 @@ BOOL CPasskeyEntry::OnInitDialog(void)
     return FALSE;
   }
 
+  CWnd *padv = GetDlgItem(IDC_ADVANCED);
   // Disable and hide Advanced checkbox if not valid
-  if (m_index != GCP_FIRST && m_adv_type == CAdvancedDlg::ADV_INVALID) {
-    GetDlgItem(IDC_ADVANCED)->EnableWindow(FALSE);
-    GetDlgItem(IDC_ADVANCED)->ShowWindow(SW_HIDE);
+  if ((m_index != GCP_ADVANCED || m_adv_type == CAdvancedDlg::ADV_INVALID) &&
+      padv != NULL) {
+    padv->EnableWindow(FALSE);
+    padv->ShowWindow(SW_HIDE);
   }
 
   // Do not enable the OK button until the user has accessed the Advanced
