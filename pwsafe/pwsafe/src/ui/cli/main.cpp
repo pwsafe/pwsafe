a317ff3247637fc0544c280c2759dde768581fa6
Rony Shapiro <ronys@users.sourceforge.net>
2011-02-01 20:38:00 +0000

    Start work on command line parsing of cli version, to support imp/exp xml/text, for starts. WiP
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@3931 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/src/ui/cli/main.cpp
+++ pwsafe/pwsafe/src/ui/cli/main.cpp
@@ -8,16 +8,22 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <getopt.h>
 
 #include "PWScore.h"
 #include "os/file.h"
 #include "core/UTF8Conv.h"
 
+#include <termios.h>
+
 using namespace std;
+
+static struct termios oldTermioFlags;
 
 static void usage(char *pname)
 {
-  cerr << "Usage: " << pname << " safe passkey" << endl;
+  cerr << "Usage: " << pname << " safe --imp [file] --text|--xml" << endl
+       << "\t safe --exp [file] --text|--xml" << endl;
 }
 
 static const char *status_text(int status)
@@ -42,35 +48,124 @@ static const char *status_text(int status)
   }
 }
 
+struct UserArgs {
+  UserArgs() : ImpExp(Unset), Format(Unknown) {}
+  StringX fname;
+  enum {Unset, Import, Export} ImpExp;
+  enum {Unknown, XML, Text} Format;
+};
+
+bool parseArgs(int argc, char *argv[], UserArgs &ua)
+{
+  if (argc != 4 && argc != 5)
+    return false;
+  CUTF8Conv conv;
+  if (!conv.FromUTF8((const unsigned char *)argv[1], strlen(argv[1]),
+                     ua.fname)) {
+    cerr << "Could not convert filename " << argv[1] << " to StringX" << endl;
+    exit(2);
+  }
+  while (1) {
+    int option_index = 0;
+    static struct option long_options[] = {
+      // name, has_arg, flag, val
+      {"import", 2, 0, 'i'},
+      {"export", 2, 0, 'e'},
+      {"text", 0, 0, 't'},
+      {"xml", 0, 0, 'x'},
+      {0, 0, 0, 0}
+    };
+
+    int c = getopt_long(argc, argv, "",
+                        long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 'i':
+      if (ua.ImpExp == UserArgs::Unset)
+        ua.ImpExp = UserArgs::Import;
+      else
+        return false;
+      if (optarg)
+        printf(" with arg %s", optarg);
+      break;
+    case 'e':
+      if (ua.ImpExp == UserArgs::Unset)
+        ua.ImpExp = UserArgs::Export;
+      else
+        return false;
+      if (optarg)
+        printf(" with arg %s", optarg);
+      break;
+    case 'x':
+      if (ua.Format == UserArgs::Unknown)
+        ua.Format = UserArgs::XML;
+      else
+        return false;
+      break;
+    case 't':
+      if (ua.Format == UserArgs::Unknown)
+        ua.Format = UserArgs::Text;
+      else
+        return false;
+      break;
+
+    default:
+      printf("?? getopt returned character code 0%o ??\n", c);
+    }
+  }
+
+  return true;
+}
+
+void echoOff()
+{
+  struct termios nflags;
+  tcgetattr(fileno(stdin), &oldTermioFlags);
+  nflags = oldTermioFlags;
+  nflags.c_lflag &= ~ECHO;
+  nflags.c_lflag |= ECHONL;
+
+  if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
+    cerr << "Couldn't turn off echo\n";
+  }
+}
+
+void echoOn()
+{
+  if (tcsetattr(fileno(stdin), TCSANOW, &oldTermioFlags) != 0) {
+    cerr << "Couldn't restore echo\n";
+  }
+}
+
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
+  UserArgs ua;
+  if (!parseArgs(argc, argv, ua)) {
     usage(argv[0]);
     return 1;
   }
 
   PWScore core;
-  StringX fname;
-  CUTF8Conv conv;
-  if (!conv.FromUTF8((const unsigned char *)argv[1], strlen(argv[1]), fname)) {
-    cerr << "Could not convert filename " << argv[1] << " to StringX" << endl;
-    return 2;
-  }
-  if (!pws_os::FileExists(fname.c_str())) {
+  if (!pws_os::FileExists(ua.fname.c_str())) {
     cerr << argv[1] << " - file not found" << endl;
     return 2;
   }
-  StringX pk;
-  if (!conv.FromUTF8((const unsigned char *)argv[2], strlen(argv[2]), pk)) {
-    cerr << "Could not convert passkey " << argv[2] << " to StringX" << endl;
-    return 2;
-  }
+  wstring wpk;
+  cout << "Enter Password: ";
+  echoOff();
+  wcin >> wpk;
+  echoOn();
+  StringX pk(wpk.c_str());
+
   int status;
-  status = core.CheckPasskey(fname, pk);
+  status = core.CheckPasskey(ua.fname, pk);
   cout << "CheckPasskey returned: " << status_text(status) << endl;
   if (status != PWScore::SUCCESS)
     goto done;
   {
+    CUTF8Conv conv;
     const char *user = getlogin() != NULL ? getlogin() : "unknown";
     StringX locker;
     if (!conv.FromUTF8((const unsigned char *)user, strlen(user), locker)) {
@@ -78,14 +173,14 @@ int main(int argc, char *argv[])
       return 2;
     }
     stringT lk(locker.c_str());
-    if (!core.LockFile(fname.c_str(), lk)) {
-      wcout << L"Couldn't lock file " << fname
+    if (!core.LockFile(ua.fname.c_str(), lk)) {
+      wcout << L"Couldn't lock file " << ua.fname
             << L": locked by " << locker << endl;
       status = -1;
       goto done;
     }
   }
-  status = core.ReadFile(fname, pk);
+  status = core.ReadFile(ua.fname, pk);
   cout << "ReadFile returned: " << status_text(status) << endl;
   if (status != PWScore::SUCCESS)
     goto done;
@@ -101,6 +196,6 @@ int main(int argc, char *argv[])
     }
   }
  done:
-  core.UnlockFile(fname.c_str());
+  core.UnlockFile(ua.fname.c_str());
   return status;
 }
