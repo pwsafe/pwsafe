c181e0f5596c1bffa49f58ea31e41a190aa8d95c
Andrey Kuznetsov <pm_kan@users.sourceforge.net>
2011-06-22 16:23:03 +0000

    Updates in Russian Help (till rev.4302, text only)
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@4303 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/help/pwsafeRU/omegat/sync_source.sh
+++ pwsafe/pwsafe/help/pwsafeRU/omegat/sync_source.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+pwd
 #clean old files
 rm -rf ./source/*
 mkdir -p ./source/html
 #copy files
 cp ../../default/pwsafe.hhc ./source/pwsafe.hhc
 cp ../../default/html/* ./source/html
-#apply helper pathes
+cat ../../default/html/auto*
+#apply helper patches
 patch -p0 < omegat_helper.diff
 #update ui tmx
-po2tmx --source-language=EN-US --language=RU-RU ../../../src/ui/Windows/I18N/pos/pwsafe_ru.po omegat/pwsafe_ru-ui.tmx
+po2tmx --source-language=EN-US --language=RU-RU ../../../src/ui/Windows/I18N/pos/pwsafe_ru.po tm/pwsafe_ru-ui.tmx
