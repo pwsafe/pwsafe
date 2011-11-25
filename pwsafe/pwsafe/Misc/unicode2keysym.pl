30d2d9ec83c7c5cd3d05f34ee3ba1932a400abb8
Saurav Ghosh <sauravg@users.sourceforge.net>
2011-11-25 11:30:44 +0000

    Added C++ source code for generating .cpp
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@4594 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/Misc/unicode2keysym.pl
+++ pwsafe/pwsafe/Misc/unicode2keysym.pl
@@ -21,6 +21,8 @@
 use strict;
 use warnings;
 
+sub StartFile;
+sub EndFile;
 
 sub usage {
     print "Usage: $0 <full path to keysymdef.h>\n";
@@ -32,6 +34,8 @@ sub usage {
 # These come from keysymdef.h itself, except that I had to escape the + after U to U\+
 my $regex1 = qr'^\#define XK_([a-zA-Z_0-9]+)\s+0x([0-9a-f]+)\s*\/\* U\+([0-9A-F]{4,6}) (.*) \*\/\s*$';
 my $regex2 = qr'^\#define XK_([a-zA-Z_0-9]+)\s+0x([0-9a-f]+)\s*\/\*\(U\+([0-9A-F]{4,6}) (.*)\)\*\/\s*$';
+
+&StartFile;
 
 while (<>) {
   if (/$regex1/ || /$regex2/) {
@@ -46,6 +50,53 @@ while (<>) {
     #print "{0x$wchar, 0x$keysym},\n" if (($keysym =~/^10+$wchar$/i) && hex $wchar > 255); # same as above, using regex
 
     # don't need in map if can be gotten by adding 0x01000000, or is LATIN1 (i.e. < 256)
-    print "{0x$wchar, 0x$keysym},\n" if ((hex $wchar | 0x01000000) == hex $keysym && hex $wchar > 255);
+    print "  keymap.insert(std::make_pair(0x$wchar, 0x$keysym));\n" if ((hex $wchar | 0x01000000) != hex $keysym && hex $wchar > 255);
   }
+}
+
+&EndFile;
+
+sub StartFile {
+  print << "EOT";
+/*
+* Copyright (c) 2003-2011 Rony Shapiro <ronys\@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+
+/*
+ * unicode2keysym - Contains a map between UNICODE wchar_t values and X keysyms
+ *
+ * This file is auto-generated using Misc/unicode2keysym.pl.  Do not edit by hand!!
+ *
+ */
+
+#include <map>
+#include <X11/Intrinsic.h>
+#include "./unicode2keysym.h"
+
+typedef std::map<wchar_t, KeySym> keymap_t;
+
+static keymap_t GetKeyMap()
+{
+  keymap_t keymap;
+EOT
+}
+
+sub EndFile {
+  print << "EOT";
+  return keymap;
+}
+
+KeySym unicode2keysym(wchar_t wc)
+{
+  static const keymap_t keymap = GetKeyMap();
+  keymap_t::const_iterator itr = keymap.find(wc);
+  if ( itr != keymap.end())
+    return itr->second;
+  return NoSymbol;
+}
+EOT
 }
