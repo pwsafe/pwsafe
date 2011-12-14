ce74f9ee28cdd7ceea3aa80dfe7792c7d2cf0fd1
DrK <c-273@users.sourceforge.net>
2011-12-14 20:11:34 +0000

    Hopefully make logging work with gcc (untested) - again!
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/branches/dk-logit@4632 1f79f812-37fb-46fe-a122-30589dd2bf55

--- src/os/logit.h
+++ src/os/logit.h
@@ -9,79 +9,67 @@
 #ifndef _OSLOGIT_H
 #define _OSLOGIT_H
 
-/*
-
-In MFC under Windows, the circular log is dumped in a UserStream for separate processing.
-
-In other environments, this is not possible and so each log entry is preceded by "PWSLOG"
-to make it easier to find in the memory dump.
-
-*/
-
 #include "typedefs.h"
 
 #ifdef _WIN32
 #include "wtypes.h"
 #endif
 
+/**
+  In MFC under Windows, the circular log is dumped in a UserStream for
+  separate processing (could probably can be implemented using wxWidgets under Windows)
+
+  In other environments, this is not possible and so each log entry is preceded
+  by "PWSLOG" to make it easier to find in the memory dump.
+**/
+
+/*
+  Maybe an issue if a user compiles with a different compiler than VC or gcc
+*/
+
 #if defined(_MSC_VER)
+// As __FILE__ and __FUNCTION__ are character strings, have to use %S rather than %s
+// in UNICODE.This is a Microsoft specific extension to "printf Type Field Characters"
+// and is not ANSI compatible.
+// Implies Windows!
 #ifdef UNICODE
-#define PWS_LOGIT_HEADER L"%s;\t%s; "
+#define PWS_LOGIT_HEADER L"%S;\t%S; "
 #else
 #define PWS_LOGIT_HEADER  "%s;\t%s; "
 #endif
-#else
+#endif
+
+#if defined(__GNUC__)
+// As __FILE__ and __FUNCTION__ are character strings, GCC treats %s as single-character
+// strings when in UNICODE.
+// Could be Windows or non-Windows!
 #ifdef UNICODE
-// Not MSC - prepend "PWSLOG" to each record
+// UNICODE
+#ifdef _WIN32
+#define PWS_LOGIT_HEADER L"%s;\t%s; "
+#else
 #define PWS_LOGIT_HEADER L"PWSLOG %s;\t%s; "
+#endif
+#else
+// Not UNICODE
+#ifdef _WIN32
+#define PWS_LOGIT_HEADER  "%s;\t%s; "
 #else
 #define PWS_LOGIT_HEADER  "PWSLOG %s;\t%s; "
 #endif
 #endif
-
-#define PWS_GET_FUNC_NAME(n) __PWS__FN__ ## n
+#endif
 
 #ifdef UNICODE
-#define WIDEN2(x) L ## x
-#define WIDEN(x) WIDEN2(x)
-
 #define PWS_LOGIT_CONCAT(x) PWS_LOGIT_HEADER L ## x
-#define __PWS_FILE__  WIDEN(__FILE__)
-
-#if defined(_MSC_VER)
-#define __PWS_FUNCTION__ __LPREFIX( __FUNCTION__ )
 #else
-#define __PWS_FUNCTION__ WIDEN( __FUNCTION__ )
+#define PWS_LOGIT_CONCAT(x) PWS_LOGIT_HEADER   ## x
 #endif
 
-#else
-#define PWS_LOGIT_CONCAT(x) PWS_LOGIT_HEADER ## x
-#define __PWS_FILE__ __FILE__
-#define __PWS_FUNCTION__ __FUNCTION__
-#endif
-
-#ifdef UNICODE
-#define PWS_LOGIT0(n) \
-  const std::wstring PWS_GET_FUNC_NAME(n)(__PWS_FUNCTION__); \
-  pws_os::Logit(PWS_LOGIT_HEADER, \
-            __PWS_FILE__, PWS_GET_FUNC_NAME(n).c_str());
-#define PWS_LOGIT0_ARGS(n, x, ...) \
-  const std::wstring PWS_GET_FUNC_NAME(n)(__PWS_FUNCTION__); \
-  pws_os::Logit(PWS_LOGIT_CONCAT(x), \
-            __PWS_FILE__, PWS_GET_FUNC_NAME(n).c_str(), __VA_ARGS__);
-#else
-#define PWS_LOGIT0(n) \
-  const std::string PWS_GET_FUNC_NAME(n)(__FUNCTION__); \
-  pws_os::Logit(PWS_LOGIT_HEADER, \
-            __PWS_FILE__, PWS_GET_FUNC_NAME(n).c_str());
-#define PWS_LOGIT0_ARGS(n, x, ...) \
-  const std::string PWS_GET_FUNC_NAME(n)(__FUNCTION__); \
-  pws_os::Logit(PWS_LOGIT_CONCAT(x), \
-            __PWS_FILE__, PWS_GET_FUNC_NAME(n).c_str(), __VA_ARGS__);
-#endif
-
-#define PWS_LOGIT              PWS_LOGIT0(__COUNTER__)
-#define PWS_LOGIT_ARGS(x, ...) PWS_LOGIT0_ARGS(__COUNTER__, x, __VA_ARGS__)
+// Now the actual logging macros
+#define PWS_LOGIT pws_os::Logit(PWS_LOGIT_HEADER, __FILE__, __FUNCTION__);
+#define PWS_LOGIT_ARGS(x, ...) pws_os::Logit(PWS_LOGIT_CONCAT(x), \
+            __FILE__, __FUNCTION__, __VA_ARGS__);
 
 namespace pws_os {
   extern void Logit(LPCTSTR lpszFormat, ...);
