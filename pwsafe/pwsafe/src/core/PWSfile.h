0e0d5675050b8117a1d4169e418f096c6364f7a0
Rony Shapiro <ronys@users.sourceforge.net>
2012-01-14 09:37:33 +0000

    Undid last commit, after discussion with dk
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@4689 1f79f812-37fb-46fe-a122-30589dd2bf55

--- pwsafe/pwsafe/src/core/PWSfile.h
+++ pwsafe/pwsafe/src/core/PWSfile.h
@@ -151,8 +151,8 @@ public:
   bool IsValid() {return m_iErrorCode == PWSfile::SUCCESS;}
   int GetErrorCode() {return m_iErrorCode;}
 
-  bool operator==(const PWSFileSig &that);
-  bool operator!=(const PWSFileSig &that) {return !(*this == that);}
+  bool operator==(const PWSFileSig &other);
+  bool operator!=(const PWSFileSig &other) {return !(*this == other);}
 
 private:
   long m_length; // -1 if file doesn't exist or zero length
