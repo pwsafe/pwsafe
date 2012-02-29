9a2c3e740480eb6bbbb99291652674b4fcd1e39f
Rony Shapiro <ronys@users.sourceforge.net>
2012-02-29 00:29:20 +0000

    latest.xml is now compliant xml
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk/pwsafe/pwsafe@4795 1f79f812-37fb-46fe-a122-30589dd2bf55

--- Misc/make-latest-xml.pl
+++ Misc/make-latest-xml.pl
@@ -31,13 +31,13 @@ while (<>) {
 };
 
 print "<VersionInfo>\n";
-print " <Product name=PasswordSafe variant=PC";
-print " major=$WinMajor minor=$WinMinor build=$WinBuild rev=$WinRevision />\n";
-print " <Product name=PasswordSafe variant=PPc";
-print " major=1 minor=9 build=2 rev=100 />\n";
-print " <Product name=PasswordSafe variant=U3";
-print " major=$WinMajor minor=$WinMinor build=$WinBuild rev=$WinRevision />\n";
-print " <Product name=PasswordSafe variant=Linux";
-print " major=$LinMajor minor=$LinMinor build=$LinBuild rev=$LinRevision />\n";
+print " <Product name=\"PasswordSafe\" variant=\"PC\"";
+print " major=\"$WinMajor\" minor=\"$WinMinor\" build=\"$WinBuild\" rev=\"$WinRevision\" />\n";
+print " <Product name=\"PasswordSafe\" variant=\"PPc\"";
+print " major=\"1\" minor=\"9\" build=\"2\" rev=\"100\" />\n";
+print " <Product name=\"PasswordSafe\" variant=\"U3\"";
+print " major=\"$WinMajor\" minor=\"$WinMinor\" build=\"$WinBuild\" rev=\"$WinRevision\" />\n";
+print " <Product name=\"PasswordSafe\" variant=\"Linux\"";
+print " major=\"$LinMajor\" minor=\"$LinMinor\" build=\"$LinBuild\" rev=\"$LinRevision\" />\n";
 print "</VersionInfo>\n";
 exit 0;
