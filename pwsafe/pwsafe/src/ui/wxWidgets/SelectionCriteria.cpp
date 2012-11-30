02903dbb6a8fb2f0f14a7f308b19990a701590c0
Saurav Ghosh <sauravg@users.sourceforge.net>
2012-11-30 09:43:07 +0000

    Separated out the implementation of SelectionCriteria from AdvancedSelectionDlg to its own source file
    git-svn-id: svn://svn.code.sf.net/p/passwordsafe/code/trunk@5130 1f79f812-37fb-46fe-a122-30589dd2bf55

---
+++ pwsafe/pwsafe/src/ui/wxWidgets/SelectionCriteria.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2003-2012 Rony Shapiro <ronys@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+
+/** \file SelectionCriteria.cpp
+* 
+*/
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include "./SelectionCriteria.h"
+
+CItemData::FieldType subgroups[] = {  CItemData::GROUP,
+                                      CItemData::GROUPTITLE,
+                                      CItemData::NOTES,
+                                      CItemData::TITLE,
+                                      CItemData::URL,
+                                      CItemData::USER
+                                  } ;
+
+struct _subgroupFunctions {
+  const charT* name;
+  PWSMatch::MatchRule function;
+  
+} subgroupFunctions[] = {                         {wxT("equals"),              PWSMatch::MR_EQUALS},
+                                                  {wxT("does not equal"),      PWSMatch::MR_NOTEQUAL},
+                                                  {wxT("begins with"),         PWSMatch::MR_BEGINS},
+                                                  {wxT("does not begin with"), PWSMatch::MR_NOTBEGIN},
+                                                  {wxT("ends with"),           PWSMatch::MR_ENDS},
+                                                  {wxT("does not end with"),   PWSMatch::MR_NOTEND},
+                                                  {wxT("contains"),            PWSMatch::MR_CONTAINS},
+                                                  {wxT("does not contain"),    PWSMatch::MR_NOTCONTAIN} } ;
+
+CItemData::FieldType selectableFields[] = { CItemData::GROUP,
+                                            CItemData::TITLE,
+                                            CItemData::USER,
+                                            CItemData::PASSWORD,
+                                            CItemData::NOTES,
+                                            CItemData::URL,
+                                            CItemData::AUTOTYPE,
+                                            CItemData::PWHIST,
+                                            CItemData::POLICY,
+                                            CItemData::POLICYNAME,
+                                            CItemData::SYMBOLS,
+                                            CItemData::RUNCMD,
+                                            CItemData::EMAIL,
+                                            CItemData::DCA,
+                                            CItemData::SHIFTDCA,
+                                            CItemData::PROTECTED,
+                                            CItemData::CTIME,
+                                            CItemData::ATIME,
+                                            CItemData::XTIME,
+                                            CItemData::XTIME_INT,
+                                            CItemData::PMTIME,
+                                            CItemData::RMTIME,
+                                      };
+
+////////////////////////////////////////////////////////////////////////////
+// SelectionCriteria implementation
+
+//static
+size_t SelectionCriteria::GetNumSubgroups(void) {
+  return WXSIZEOF(subgroups);
+}
+
+//static
+CItemData::FieldType SelectionCriteria::GetSubgroup(size_t idx) {
+  wxASSERT_MSG(idx < GetNumSubgroups(), wxT("Invalid index for GetSubgroups"));
+  return subgroups[idx];
+}
+
+//static
+wxString SelectionCriteria::GetSelectableFieldName(CItemData::FieldType ft) {
+  return towxstring(CItemData::FieldName(ft));
+}
+
+//static
+size_t SelectionCriteria::GetNumFieldsSelectable() {
+  return WXSIZEOF(selectableFields);
+}
+
+CItemData::FieldType SelectionCriteria::GetSelectableField(size_t idx) {
+  wxASSERT_MSG(idx < GetNumFieldsSelectable(), wxT("Invalid index for GetSelectableField"));
+  return selectableFields[idx];
+}
+
+wxString SelectionCriteria::GetGroupSelectionDescription() const
+{
+  if (!m_fUseSubgroups)
+    return _("All entries");
+  else
+    return wxString(_("Entries whose ")) << GetSelectableFieldName(subgroups[m_subgroupObject]) << wxT(' ')
+            << subgroupFunctions[m_subgroupFunction].name << wxT(" \"") << m_subgroupText
+            << wxT("\" [") << (m_fCaseSensitive? wxT("") : _("not ")) << _("case-sensitive]");
+}
+
+//static
+size_t SelectionCriteria::GetNumSubgroupFunctions()
+{
+  return WXSIZEOF(subgroupFunctions);
+}
+
+//static
+wxString SelectionCriteria::GetSubgroupFunctionName(size_t idx)
+{
+  wxASSERT_MSG(idx < GetNumSubgroupFunctions(), wxT("Invalid index for GetSubgroupFunctionName"));
+  return subgroupFunctions[idx].name;
+}
+//static
+PWSMatch::MatchRule SelectionCriteria::GetSubgroupFunction(size_t idx)
+{
+  wxASSERT_MSG(idx < GetNumSubgroupFunctions(), wxT("Invalid index for GetSubgroupFunction"));
+  return subgroupFunctions[idx].function;
+}
+
+//returns true if all fields have been selected
+bool SelectionCriteria::GetFieldSelection(wxArrayString& selectedFields, wxArrayString& unselectedFields)
+{
+  for (size_t idx = 0; idx < GetNumFieldsSelectable(); ++idx) {
+    if (m_bsFields.test(selectableFields[idx]))
+      selectedFields.Add(GetSelectableFieldName(selectableFields[idx]));
+    else
+      unselectedFields.Add(GetSelectableFieldName(selectableFields[idx]));
+  }
+  return m_bsFields.count() == NumberOf(selectableFields);
+}
+
