3b1055b756a1abc7152bdf8a9947f521c80a8439
ronys <ronys@users.sourceforge.net>
2013-07-10 20:53:39 +0100

    AES code added and unit tested

---
+++ src/core/AES.h
@@ -0,0 +1,40 @@
+/*
+* Copyright (c) 2013 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+// AES.h
+// C++ wrapper of AES for PasswordSafe, based on LibTomCrypt by
+// Tom St Denis, tomstdenis@iahu.ca, http://libtomcrypt.org
+//-----------------------------------------------------------------------------
+#ifndef __AES_H
+#define __AES_H
+
+#include "Fish.h"
+#include "os/typedefs.h"
+
+struct rijndael_key {
+   ulong32 eK[60], dK[60];
+   int Nr;
+};
+
+class AES : public Fish
+{
+public:
+  enum {BLOCKSIZE=16};
+  AES(const unsigned char* key, int keylen);
+  virtual ~AES();
+  virtual void Encrypt(const unsigned char *in, unsigned char *out);
+  virtual void Decrypt(const unsigned char *in, unsigned char *out);
+  virtual unsigned int GetBlockSize() const {return BLOCKSIZE;}
+
+private:
+  rijndael_key key_schedule;
+};
+#endif /* __AES_H */
+//-----------------------------------------------------------------------------
+// Local variables:
+// mode: c++
+// End:
