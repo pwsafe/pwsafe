0527ac7bc7facaf7df49a4cc88a83732a25723a3
Andrey Kuznetsov <pm_kan@mail.ru>
2014-01-13 19:48:15 +0400

    * Updated Russian help * Removed some empty tags for default help * Few adjustments to .gitignore

--- Misc/I18N/ru/omegat_help/check_images.sh
+++ Misc/I18N/ru/omegat_help/check_images.sh
@@ -3,14 +3,16 @@ REV=ee3022
 GIT_ROOT=`git rev-parse --show-toplevel`
 CDIR=$GIT_ROOT/help/pwsafeRU/html/images
 DDIR=$GIT_ROOT/help/default/html/images
-
+EXT1="jpg"
+EXT2="png"
 #check for updated files
 git diff --name-only $REV HEAD | grep help/default/html/images
 
 #check for new files
 cd $DDIR
 for f in `tree -fin --noreport .`; do
-    if [ -f $f ] && ! [ -e $CDIR/$f ]; then
+    bf=${f%.*}
+    if [ -f $f ] && ! [ -e $CDIR/${bf}.$EXT1 ] && ! [ -e $CDIR/${bf}.$EXT2 ]; then
 	echo "$f is absent in translation dir"
     fi
 done
@@ -18,7 +20,8 @@ done
 #check for old files
 cd $CDIR
 for f in `tree -fin --noreport .`; do
-    if [ -f $f ] && ! [ -e $DDIR/$f ]; then
+    bf=${f%.*}
+    if [ -f $f ] && ! [ -e $DDIR/${bf}.$EXT1 ] && ! [ -e $DDIR/${bf}.$EXT2 ]; then
 	echo "$f is absent in default dir"
     fi
 done
--- Misc/I18N/ru/omegat_help/recode_hhc.sh
+++ Misc/I18N/ru/omegat_help/recode_hhc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 GIT_ROOT=`git rev-parse --show-toplevel`
-HHC_PATH=$GIT_ROOT/help/pwsafeRU/html
+HHC_PATH=$GIT_ROOT/help/pwsafeRU
 iconv -f utf-8 -t cp1251 $HHC_PATH/pwsafe.hhc -o $HHC_PATH/pwsafeRU.hhc
 rm $HHC_PATH/pwsafe.hhc
