7495f8c6bcfd5e7b365cb9db8c3e1da320ba63a4
ronys <ronys@users.sourceforge.net>
2014-06-13 11:02:46 +0100

    Added PWStime utility class
    This class supports the V4 format representation of time (5 bytes,
    little-endian seconds from Linux epoch)

---
+++ src/core/PWStime.cpp
@@ -0,0 +1,83 @@
+/*
+* Copyright (c) 2003-2014 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+
+/** \file
+* Implementation of PWStime
+*/
+
+#include <ctime>
+#include <cstring>
+#include <algorithm>
+
+#include "PWStime.h"
+#include "Util.h"
+
+using namespace std;
+
+// Pesky WinDef.h...
+#ifdef min
+#undef min
+#endif
+
+PWStime::PWStime()
+{
+  setTime(std::time(NULL));
+}
+
+PWStime::PWStime(const PWStime &pwt)
+{
+  memcpy(m_time, pwt.m_time, sizeof(m_time));
+}
+
+PWStime::PWStime(std::time_t t)
+{
+  setTime(t);
+}
+
+PWStime::~PWStime()
+{
+}
+
+PWStime &PWStime::operator=(const PWStime &that)
+{
+  if (this != &that) 
+    memcpy(m_time, that.m_time, sizeof(m_time));
+  return *this;
+
+}
+
+PWStime &PWStime::operator=(std::time_t t)
+{
+  setTime(t);
+  return *this;
+}
+
+PWStime::operator time_t() const
+{
+  unsigned char ta[sizeof(time_t)] = {0};
+  memcpy(ta, m_time, std::min(sizeof(m_time), sizeof(ta)));
+  return getInt<time_t>(ta);
+}
+
+PWStime::operator const unsigned char *() const
+{
+  return m_time;
+}
+
+PWStime::operator const char *() const
+{
+  return reinterpret_cast<const char *>(m_time);
+}
+
+void PWStime::setTime(time_t t)
+{
+  unsigned char ta[sizeof(t)] = {0};
+  memset(m_time, 0, sizeof(m_time)); // needed if sizeof(t) < TIME_LEN
+  putInt(ta, t);
+  memcpy(m_time, ta, std::min(sizeof(m_time), sizeof(ta)));
+}
