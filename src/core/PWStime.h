fa1b8e8b8bdf4bb3c5bd1e1e5174f3debbe78685
ronys <ronys@users.sourceforge.net>
2014-06-13 11:02:46 +0100

    Added PWStime utility class
    This class supports the V4 format representation of time (5 bytes,
    little-endian seconds from Linux epoch)

---
+++ src/core/PWStime.h
@@ -0,0 +1,51 @@
+/*
+* Copyright (c) 2003-2014 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+// PWStime.h
+//-----------------------------------------------------------------------------
+#ifndef __PWSTIME_H
+#define __PWSTIME_H
+
+/** \file
+ * "Time is on my side, yes it is" - the Rolling Stones
+ *
+ * A class to convert between time_t (which can be 32 or 64 bits) and a
+ * 5 byte (40 bit) little-endian representation, which is what's stored
+ * in the PWSafe V4 format. This resolves the 2038 issue for my lifetime,
+ * at least, and provides platform-independence, to boot.
+ */
+
+#include <ctime>
+
+class PWStime
+{
+public:
+  enum {TIME_LEN = 5};
+  PWStime(); // default c'tor initiates value to current time
+  PWStime(const PWStime &);
+  PWStime(std::time_t);
+  ~PWStime();
+  PWStime &operator=(const PWStime &);
+  PWStime &operator=(std::time_t);
+
+  operator time_t() const;
+  operator const unsigned char *() const;
+  operator const char *() const;
+
+  int GetLength() const {return TIME_LEN;} // size of representation
+  
+
+private:
+  void setTime(time_t t);
+  unsigned char m_time[TIME_LEN];
+};
+
+#endif /* __PWSTIME_H */
+//-----------------------------------------------------------------------------
+// Local variables:
+// mode: c++
+// End:
