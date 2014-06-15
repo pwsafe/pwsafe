ba8bebc2b0802b7ddeebade4360065f27b389d0c
ronys <ronys@users.sourceforge.net>
2014-06-15 19:45:05 +0100

    V4 now supports named password policies

--- src/core/PWPolicy.h
+++ src/core/PWPolicy.h
@@ -40,12 +40,15 @@ struct PWPolicy {
     MakePronounceable   = 0x0200,
     Unused              = 0x01ff};
 
-  unsigned short flags; // bitwise-or of the above
+  uint16 flags; // bitwise-or of the above
+  // Following are limited by format to 2-byte values,
+  // but changing int to uint16 is unwarranted, too much ugliness.
   int length;
   int digitminlength;
   int lowerminlength;
   int symbolminlength;
   int upperminlength;
+
   StringX symbols; // policy-specific set of 'symbol' characters
   size_t usecount; // how many entries use this policy?
 
