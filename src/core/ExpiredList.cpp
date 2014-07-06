pwsafe/pwsafe

commit 7903b40a165853f4322b36b4758debcab399093e

Removed obsolete  #if (_MSC_VER >= 1400)
Since no one's complained where we've missed adding it,
it's fair to conclude that MSVS2008 no longer needs to
be supported.
(Review comments from Andrey)

src/core/ExpiredList.cpp
@@ -64,13 +64,9 @@ ExpiredList ExpiredList::GetExpired(const int &idays)
 
   time_t now, exptime;
   time(&now);
-#if (_MSC_VER >= 1400)
   errno_t err;
   err = localtime_s(&st, &now);  // secure version
   ASSERT(err == 0);
-#else
-  st = *localtime(&now);
-#endif
   st.tm_mday += idays;
   // Note: mktime will normalise the date structure before converting to time_t
   exptime = mktime(&st);

src/core/ItemData.cpp
@@ -1738,13 +1738,9 @@ bool CItemData::Matches(time_t time1, time_t time2, int iObject,
     time_t testtime;
     if (tValue) {
       struct tm st;
-#if (_MSC_VER >= 1400)
       errno_t err;
       err = localtime_s(&st, &tValue);  // secure version
       ASSERT(err == 0);
-#else
-      st = *localtime(&tValue);
-#endif
       st.tm_hour = 0;
       st.tm_min = 0;
       st.tm_sec = 0;
@@ -1805,13 +1801,9 @@ bool CItemData::WillExpire(const int numdays) const
     return false;
 
   struct tm st;
-#if (_MSC_VER >= 1400)
   errno_t err;
   err = localtime_s(&st, &now);  // secure version
   ASSERT(err == 0);
-#else
-  st = *localtime(&now);
-#endif
   st.tm_mday += numdays;
   exptime = mktime(&st);
   if (exptime == time_t(-1))

src/core/ItemField.cpp
@@ -80,11 +80,7 @@ void CItemField::Set(const unsigned char* value, size_t length,
 
     unsigned char *tempmem = new unsigned char[BlockLength];
     // invariant: BlockLength >= plainlength
-#if (_MSC_VER >= 1400)
     memcpy_s(tempmem, BlockLength, value, m_Length);
-#else
-    memcpy(tempmem, value, m_Length);
-#endif
 
     //Fill the unused characters in with random stuff
     PWSrand::GetInstance()->GetRandomData(tempmem + m_Length, static_cast<unsigned long>(BlockLength - m_Length));

src/core/PWScore.cpp
@@ -2777,13 +2777,8 @@ struct HistoryUpdateSetMax : public HistoryUpdater {
         m_mapSavedHistory[ci.GetUUID()] = st_pwhs;
         int status, old_max, num_saved;
         const wchar_t *lpszPWHistory = cs_tmp.c_str();
-#if (_MSC_VER >= 1400)
         int iread = swscanf_s(lpszPWHistory, _T("%01d%02x%02x"),
                                &status, &old_max, &num_saved);
-#else
-        int iread = swscanf(lpszPWHistory, _T("%01d%02x%02x"),
-                             &status, &old_max, &num_saved);
-#endif
         if (iread == 3 && status == 1 && num_saved <= m_new_default_max) {
           cs_tmp = m_text + cs_tmp.substr(3);
           ci.SetPWHistory(cs_tmp);

src/core/Util.cpp
@@ -341,12 +341,7 @@ size_t _readcbc(FILE *fp,
 
 void PWSUtil::strCopy(LPTSTR target, size_t tcount, const LPCTSTR source, size_t scount)
 {
-#if (_MSC_VER >= 1400)
   (void) _tcsncpy_s(target, tcount, source, scount);
-#else
-  UNREFERENCED_PARAMETER(tcount);
-  (void)_tcsncpy(target, source, scount);
-#endif
 }
 
 size_t PWSUtil::strLength(const LPCTSTR str)
@@ -363,18 +358,12 @@ StringX PWSUtil::ConvertToDateTimeString(const time_t &t, TMC result_format)
   if (t != 0) {
     TCHAR datetime_str[80];
     struct tm *st;
-#if (_MSC_VER >= 1400)
     struct tm st_s;
     errno_t err;
     err = localtime_s(&st_s, &t);  // secure version
     if (err != 0) // invalid time
       return ConvertToDateTimeString(0, result_format);
     st = &st_s; // hide difference between versions
-#else
-    st = localtime(&t);
-    if (st == NULL) // null means invalid time
-      return ConvertToDateTimeString(0, result_format);
-#endif
     switch (result_format) {
     case TMC_EXPORT_IMPORT:
       _tcsftime(datetime_str, sizeof(datetime_str) / sizeof(datetime_str[0]),
@@ -446,11 +435,7 @@ void PWSUtil::GetTimeStamp(stringT &sTimeStamp, const bool bShort)
 #ifdef _WIN32
   struct _timeb *ptimebuffer;
   ptimebuffer = new _timeb;
-#if (_MSC_VER >= 1400)
   _ftime_s(ptimebuffer);
-#else
-  _ftime(ptimebuffer);
-#endif
 #else
   struct timeb *ptimebuffer;
   ptimebuffer = new timeb;

src/Tools/Windows/I18N/ResPWSL/ResPWSL.cpp
@@ -141,14 +141,9 @@ bool UpdateRODLL(const CString csInFilePath, const LCID locale)
   TCHAR drive[_MAX_DRIVE];
   TCHAR dir[_MAX_DIR];
 
-#if _MSC_VER >= 1400
   _tsplitpath_s((LPCTSTR)csInFilePath, drive, _MAX_DRIVE, dir, _MAX_DIR,
     NULL, 0, NULL, 0);
   _tmakepath_s(path_buffer, _MAX_PATH, drive, dir, (LPCTSTR)csFilename, _T("dll"));
-#else
-  _tsplitpath((LPCTSTR)csInFilePath, drive, dir, NULL, NULL);
-  _tmakepath(path_buffer, drive, dir, (LPCTSTR)csFilename, _T("dll"));
-#endif
 
   csOutFilePath = path_buffer;
   if (!CopyFile(csInFilePath, csOutFilePath, FALSE)) {

src/core/PwsPlatform.h
@@ -69,11 +69,6 @@
 #define LTC_NO_ROLC
 #endif
 
-// stop MS VC++ 6 and earlier being quite so pedantic
-#if defined(_MSC_VER) && (_MSC_VER <= 1200) // i.e. only MSVC++ 6 and earlier
-#pragma warning ( disable : 4097 )
-#endif
-
 #undef PWS_PLATFORM
 #undef POCKET_PC
 

src/core/XML/MSXML/MFileSAX2Handlers.cpp
@@ -76,18 +76,10 @@ HRESULT STDMETHODCALLTYPE MFileSAX2ErrorHandler::error(struct ISAXLocator * pLoc
   int iLineNumber, iCharacter;
 
 #ifdef _UNICODE
-#if (_MSC_VER >= 1400)
   _tcscpy_s(szErrorMessage, MAX_PATH * 2, pwchErrorMessage);
 #else
-  _tcscpy(szErrorMessage, pwchErrorMessage);
-#endif
-#else
-#if (_MSC_VER >= 1400)
   size_t num_converted;
   wcstombs_s(&num_converted, szErrorMessage, MAX_PATH * 2, pwchErrorMessage, MAX_PATH);
-#else
-  wcstombs(szErrorMessage, pwchErrorMessage, MAX_PATH);
-#endif
 #endif
   pLocator->getLineNumber(&iLineNumber);
   pLocator->getColumnNumber(&iCharacter);
@@ -95,13 +87,8 @@ HRESULT STDMETHODCALLTYPE MFileSAX2ErrorHandler::error(struct ISAXLocator * pLoc
   stringT cs_format;
   LoadAString(cs_format, IDSC_MSXMLSAXGENERROR);
 
-#if (_MSC_VER >= 1400)
   _stprintf_s(szFormatString, MAX_PATH * 2, cs_format.c_str(),
               hrErrorCode, iLineNumber, iCharacter, szErrorMessage);
-#else
-  _stprintf(szFormatString, cs_format.c_str(),
-            hrErrorCode, iLineNumber, iCharacter, szErrorMessage);
-#endif
 
   m_strValidationResult += szFormatString;
 
@@ -201,22 +188,12 @@ TCHAR * FileProcessAttributes(
     pAttributes->getQName(i, &QName, &QName_length);
     pAttributes->getValue(i, &Value, &Value_length);
 #ifdef _UNICODE
-#if (_MSC_VER >= 1400)
     _tcsncpy_s(szQName, MAX_PATH + 1, QName, QName_length);
     _tcsncpy_s(szValue, MAX_PATH + 1, Value, Value_length);
 #else
-    _tcsncpy(szQName, QName, QName_length);
-    _tcsncpy(szValue, Value, Value_length);
-#endif
-#else
-#if (_MSC_VER >= 1400)
     size_t num_converted;
     wcstombs_s(&num_converted, szQName, MAX_PATH + 1, QName, QName_length);
     wcstombs_s(&num_converted, szValue, MAX_PATH + 1, Value, Value_length);
-#else
-    wcstombs(szQName, QName, QName_length);
-    wcstombs(szValue, Value, Value_length);
-#endif
 #endif
     if (_tcscmp(szQName, lpName) == 0) {
       return _tcsdup(szValue);
@@ -237,11 +214,7 @@ HRESULT STDMETHODCALLTYPE MFileSAX2ContentHandler::startElement(
 {
   wchar_t szCurElement[MAX_PATH + 1] = {0};
 
-#if (_MSC_VER >= 1400)
   wcsncpy_s(szCurElement, MAX_PATH + 1, pwchRawName, cchRawName);
-#else
-  wcsncpy(szCurElement, pwchRawName, cchRawName);
-#endif
 
   if (m_bValidation) {
     if (wcscmp(szCurElement, L"passwordsafe") == 0) {
@@ -295,18 +268,10 @@ HRESULT STDMETHODCALLTYPE MFileSAX2ContentHandler::characters(
   TCHAR* szData = new TCHAR[cchChars + 2];
 
 #ifdef _UNICODE
-#if (_MSC_VER >= 1400)
   _tcsncpy_s(szData, cchChars + 2, pwchChars, cchChars);
 #else
-  _tcsncpy(szData, pwchChars, cchChars);
-#endif
-#else
-#if (_MSC_VER >= 1400)
   size_t num_converted;
   wcstombs_s(&num_converted, szData, cchChars + 2, pwchChars, cchChars);
-#else
-  wcstombs(szData, pwchChars, cchChars);
-#endif
 #endif
 
   szData[cchChars] = 0;
@@ -329,11 +294,7 @@ HRESULT STDMETHODCALLTYPE MFileSAX2ContentHandler::endElement(
 {
   wchar_t szCurElement[MAX_PATH + 1] = {0};
 
-#if (_MSC_VER >= 1400)
   wcsncpy_s(szCurElement, MAX_PATH + 1, pwchQName, cchQName);
-#else
-  wcsncpy(szCurElement, pwchQName, cchQName);
-#endif
 
   if (m_bValidation) {
     if (wcscmp(szCurElement, L"entry") == 0)

commit 7b7252f0a0d45fa3be94d84291a89b09e991b32a

Fix Linux compile breakage from 7903b40
Over-zealous removal of #if (_MSC_VER >= 1400) broke
Linux build, as Linux doesn't have _s "secure" versions
of standard library functions. Fix is to localize these in
new os/funcwrap.h file, rather than re-introduce #ifdefs.

(Fix from Andrey, with minor tweaks)

src/core/ExpiredList.cpp
@@ -10,6 +10,7 @@
 
 #include "ExpiredList.h"
 #include "ItemData.h"
+#include "os/funcwrap.h"
 
 #include <functional>
 #include <algorithm>
@@ -62,14 +63,17 @@ ExpiredList ExpiredList::GetExpired(const int &idays)
   ExpiredList::iterator iter;
   struct tm st;
 
-  time_t now, exptime;
+  time_t now, exptime=time_t(-1);
   time(&now);
   errno_t err;
-  err = localtime_s(&st, &now);  // secure version
+  err = localtime_s(&st, &now);
   ASSERT(err == 0);
-  st.tm_mday += idays;
-  // Note: mktime will normalise the date structure before converting to time_t
-  exptime = mktime(&st);
+  if (!err) {
+    st.tm_mday += idays;
+    // Note: mktime will normalize the date structure before converting to time_t
+    exptime = mktime(&st);
+  }
+
   if (exptime == time_t(-1))
     exptime = now;
 

src/core/ItemData.cpp
@@ -1788,7 +1788,7 @@ bool CItemData::IsExpired() const
 
 bool CItemData::WillExpire(const int numdays) const
 {
-  time_t now, exptime, XTime;
+  time_t now, exptime=time_t(-1), XTime;
   time(&now);
 
   GetXTime(XTime);
@@ -1804,8 +1804,10 @@ bool CItemData::WillExpire(const int numdays) const
   errno_t err;
   err = localtime_s(&st, &now);  // secure version
   ASSERT(err == 0);
-  st.tm_mday += numdays;
-  exptime = mktime(&st);
+  if (!err){
+    st.tm_mday += numdays;
+    exptime = mktime(&st);
+  }
   if (exptime == time_t(-1))
     exptime = now;
 

src/core/ItemField.cpp
@@ -14,6 +14,7 @@
 #include "Util.h"
 #include "Fish.h"
 #include "PWSrand.h"
+#include "os/funcwrap.h"
 
 //Returns the number of bytes of 8 byte blocks needed to store 'size' bytes
 size_t CItemField::GetBlockSize(size_t size) const

src/core/PWScore.cpp
@@ -21,6 +21,7 @@
 #include "PWSfileV3.h" // XXX cleanup with dynamic_cast
 #include "StringXStream.h"
 
+#include "os/pws_tchar.h"
 #include "os/typedefs.h"
 #include "os/dir.h"
 #include "os/debug.h"
@@ -1810,13 +1811,13 @@ bool PWScore::Validate(const size_t iMAXCHARS, CReport *pRpt, st_ValidateResults
 bool PWScore::ValidateKBShortcut(int32 &iKBShortcut)
 {
   // Verify Entry Keyboard shortcut is valid
-  // Note: to support cross-platforms, can't use "Virtual Key Codes" 
+  // Note: to support cross-platforms, can't use "Virtual Key Codes"
   // as they differ between Windows, Linux & Mac.
   // However, the alphanumeric ASCII values are common
   // and so entry keyboard shortcuts are restricted to these values.
 
   if (iKBShortcut != 0) {
-    static const TCHAR *tcValidKeys = 
+    static const TCHAR *tcValidKeys =
             _T("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     static const WORD wValidModifiers = 0xffff ^ (
                             PWS_HOTKEYF_ALT   | PWS_HOTKEYF_CONTROL |
@@ -1829,7 +1830,7 @@ bool PWScore::ValidateKBShortcut(int32 &iKBShortcut)
 
     // If there are too many bits in the Modifiers or
     // Not a valid ASCII character (0-9, A-Z) - remove
-    if ((wValidModifiers & wPWSModifiers) || 
+    if ((wValidModifiers & wPWSModifiers) ||
           _tcschr(tcValidKeys, wVirtualKeyCode) == NULL) {
       // Remove Entry Keyboard Shortcut
       iKBShortcut = 0;
@@ -2775,7 +2776,7 @@ struct HistoryUpdateSetMax : public HistoryUpdater {
         m_mapSavedHistory[ci.GetUUID()] = st_pwhs;
         int status, old_max, num_saved;
         const wchar_t *lpszPWHistory = cs_tmp.c_str();
-        int iread = swscanf_s(lpszPWHistory, _T("%01d%02x%02x"),
+        int iread = _stscanf_s(lpszPWHistory, _T("%01d%02x%02x"),
                                &status, &old_max, &num_saved);
         if (iread == 3 && status == 1 && num_saved <= m_new_default_max) {
           cs_tmp = m_text + cs_tmp.substr(3);

src/core/Util.cpp
@@ -341,7 +341,8 @@ size_t _readcbc(FILE *fp,
 
 void PWSUtil::strCopy(LPTSTR target, size_t tcount, const LPCTSTR source, size_t scount)
 {
-  (void) _tcsncpy_s(target, tcount, source, scount);
+  UNREFERENCED_PARAMETER(tcount); //not used in now in non-MSVS wrapped version of _tcsncpy_s
+  _tcsncpy_s(target, tcount, source, scount);
 }
 
 size_t PWSUtil::strLength(const LPCTSTR str)
@@ -542,15 +543,15 @@ StringX PWSUtil::NormalizeTTT(const StringX &in, size_t maxlen)
 
 bool ValidateXMLCharacters(const StringX &value, ostringstream &ostInvalidPositions)
 {
-  // From: http://www.w3.org/TR/REC-xml/#sec-cdata-sect 
+  // From: http://www.w3.org/TR/REC-xml/#sec-cdata-sect
   // CDATA Sections
-  // [18]    CDSect   ::=    CDStart CData CDEnd  
-  // [19]    CDStart  ::=    '<![CDATA[' 
-  // [20]    CData    ::=    (Char* - (Char* ']]>' Char*))  
-  // [21]    CDEnd    ::=    ']]>' 
-  
+  // [18]    CDSect   ::=    CDStart CData CDEnd
+  // [19]    CDStart  ::=    '<![CDATA['
+  // [20]    CData    ::=    (Char* - (Char* ']]>' Char*))
+  // [21]    CDEnd    ::=    ']]>'
+
   // From: http://www.w3.org/TR/REC-xml/#NT-Char
-  //  Char    ::=    #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | 
+  //  Char    ::=    #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] |
   //                 [#x10000-#x10FFFF]
   // any Unicode character, excluding the surrogate blocks, FFFE, and FFFF.
 

src/os/funcwrap.h
@@ -1,0 +1,115 @@
+/*
+* Copyright (c) 2003-2014 Rony Shapiro <ronys@users.sourceforge.net>.
+* All rights reserved. Use of the code is allowed under the
+* Artistic License 2.0 terms, as specified in the LICENSE file
+* distributed with this code, or available from
+* http://www.opensource.org/licenses/artistic-license-2.0.php
+*/
+#ifndef _FUNCWRAP_H
+#define _FUNCWRAP_H
+
+#include "typedefs.h"
+#include <errno.h>
+#include <cstring>
+#include <ctime>
+
+/**
+ * Some function wrappers for cross platform/complier compatibility
+*/
+#ifndef _MSC_VER
+#ifndef _MAX__TIME64_T
+//number of seconds from 00:00:00, 01/01/1970 UTC to 23:59:59. 12/31/3000 UTC
+#define _MAX__TIME64_T 0x793406fffLL
+#endif
+#ifndef _MAX__TIME32_T
+//number of seconds from 00:00:00, 01/01/1970 UTC to 23:59:59, 01/18/2038 UTC
+#define _MAX__TIME32_T 0x7fffd27f
+#endif
+
+inline errno_t localtime_s(struct tm *_tm, const time_t *time)
+{
+  if (!_tm) {
+    return EINVAL;
+  } else if (!time || (*time > _MAX__TIME32_T) || (*time < 0)) {
+    _tm->tm_sec=_tm->tm_min=_tm->tm_hour=-1;
+    _tm->tm_mday=_tm->tm_mon=_tm->tm_year=-1;
+    _tm->tm_wday=_tm->tm_yday=_tm->tm_isdst=-1;
+    return EINVAL;
+  }
+  struct tm *res = std::localtime(time);
+  if (res) {
+    *_tm = *res;
+    return 0;
+  }
+  return EINVAL;
+}
+
+inline errno_t memcpy_s(void *dst, size_t dst_size, const void *src, size_t cnt)
+{
+  if (!dst) {
+    return EINVAL;
+  } else if (!src) {
+    std::memset(dst, 0, dst_size);
+    return EINVAL;
+  } else if (dst_size < cnt) {
+    std::memset(dst, 0, dst_size);
+    return ERANGE;
+  } else {
+    std::memcpy(dst, src, cnt);
+    return 0;
+  }
+}
+
+#define _mkgmtime32(ts) mktime(ts)
+
+inline errno_t _gmtime64_s(struct tm* _tm, const __time64_t* time)
+{
+  if (!_tm) {
+    return EINVAL;
+  } else if (!time || (*time > _MAX__TIME64_T)) {
+    _tm->tm_sec=_tm->tm_min=_tm->tm_hour=-1;
+    _tm->tm_mday=_tm->tm_mon=_tm->tm_year=-1;
+    _tm->tm_wday=_tm->tm_yday=_tm->tm_isdst=-1;
+    return EINVAL;
+  }
+  return gmtime64_r(time, _tm) ? 0 : EINVAL;
+}
+
+#ifdef UNICODE
+#include <cwchar>
+
+#ifndef _TRUNCATE
+#define _TRUNCATE static_cast<size_t>(-1)
+#endif
+
+inline errno_t wcsncpy_s(wchar_t *dst, size_t dst_size, const wchar_t *src, size_t cnt)
+{
+  if (cnt == _TRUNCATE)
+    cnt = dst_size - 1;
+
+  if (!dst || !dst_size) {
+    return EINVAL;
+  } else if (!src) {
+    dst[0] = L'\0';
+    return EINVAL;
+  } else if (dst_size <= cnt) { // '<=' because we need space for null character
+    dst[0] = L'\0';
+    return ERANGE;
+  }
+
+  std::wcsncpy(dst, src, cnt);
+  dst[cnt] = L'\0'; // don't forget null (dst[cnt] is available because dst_size > cnt)
+  return 0; // truncation state not reported
+}
+
+// to create full wrapper for swprintf_s/swscanf_s/vswprintf_s we need to parse
+// format specification and process varargs, so for now just substitute
+// non-secure version
+#define swprintf_s swprintf
+#define swscanf_s swscanf
+#define vswprintf_s vswprintf
+#endif
+
+#endif /* !_MSC_VER */
+
+#endif /* _FUNCWRAP_H */

src/os/pws_tchar.h
@@ -15,9 +15,6 @@
 #ifdef _WIN32
 #include <tchar.h>
 #else
-#define _gmtime64_s(ts64, tm64) gmtime64_r(tm64, ts64)
-#define _mkgmtime32(ts) mktime(ts)
-#define localtime_s(st, t) (localtime_r(t, st) != NULL ? 0 : 1)
 #ifdef UNICODE
 #include <wctype.h>
 #define _istalpha(x) iswalpha(x)
@@ -33,7 +30,9 @@
 #include "linux/pws_time.h"
 #define _tcsdup(s) wcsdup(s)
 #endif
+#include "funcwrap.h"
 #define _tcsncpy(t, s, sc) wcsncpy(t, s, sc)
+#define _tcsncpy_s wcsncpy_s
 #define _tcslen(s) wcslen(s)
 #define _tcsclen(s) wcslen(s)
 #define _tcscmp(s1, s2) wcscmp(s1, s2)
@@ -42,10 +41,11 @@
 #define _tcsftime wcsftime
 #define _tasctime_s(s, N, st) pws_os::asctime(s, N, st)
 #define _vsctprintf(fmt, args) vswprintf(NULL, 0, fmt, args)
-#define _vstprintf_s(str, size, fmt, args) vswprintf(str, size, fmt, args)
+#define _vstprintf_s vswprintf_s
 #define _ftprintf fwprintf
-#define _stprintf_s swprintf
+#define _stprintf_s swprintf_s
 #define _stscanf swscanf
+#define _stscanf_s swscanf_s
 #ifdef __PWS_MACINTOSH__
 # include "./mac/pws_str.h"
 #define _tcsicmp pws_os::wcscasecmp

src/os/typedefs.h
@@ -45,7 +45,7 @@ typedef char charT;
 #define PWS_HOTKEYF_CONTROL 0x02
 #define PWS_HOTKEYF_SHIFT   0x04
 #define PWS_HOTKEYF_EXT     0x08
- 
+
 // wxWidgets Only - Not used in Windows MFC
 #define PWS_HOTKEYF_ALTGR   PWS_HOTKEYF_ALT | PWS_HOTKEYF_CONTROL
 #define PWS_HOTKEYF_META    0x10
@@ -94,6 +94,7 @@ typedef unsigned long DWORD;
 #else /* !defined(_WIN32) */
 #include <stdint.h>
 #include <sys/types.h>
+#include <errno.h>
 typedef int8_t  int8;
 typedef int16_t int16;
 typedef int32_t int32;
@@ -104,7 +105,7 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
-typedef int errno_t;
+typedef error_t errno_t;
 
 #ifdef UNICODE
 #ifndef _T
@@ -130,14 +131,14 @@ typedef int32_t LONG;
 #define HIBYTE(w) ((BYTE)(((WORD)(w) >> 8) & 0xFF))
 #define LOWORD(ul) (WORD(DWORD(ul) & 0xffff))
 #define HIWORD(ul) (WORD(DWORD(ul) >> 16))
-#define MAKELONG(low, high) ((LONG) (((WORD) (low)) | ((DWORD) ((WORD) (high))) << 16)) 
+#define MAKELONG(low, high) ((LONG) (((WORD) (low)) | ((DWORD) ((WORD) (high))) << 16))
 #define MAKEWORD(low, high) ((WORD)((((WORD)(high)) << 8) | ((BYTE)(low))))
 #elif defined(PWS_BIG_ENDIAN)
 #define HIBYTE(w) ((BYTE)(w))
 #define LOBYTE(w) ((BYTE)(((WORD)(w) >> 8) & 0xFF))
 #define HIWORD(ul) (WORD(DWORD(ul) & 0xffff))
 #define LOWORD(ul) (WORD(DWORD(ul) >> 16))
-#define MAKELONG(low, high) ((LONG) (((WORD) (high)) | ((DWORD) ((WORD) (low))) << 16)) 
+#define MAKELONG(low, high) ((LONG) (((WORD) (high)) | ((DWORD) ((WORD) (low))) << 16))
 #define MAKEWORD(low, high) ((WORD)((((WORD)(low)) << 8) | ((BYTE)(high))))
 #else
 #error "One of PWS_LITTLE_ENDIAN or PWS_BIG_ENDIAN must be defined before including typedefs.h"