e87ea37d21899ac8b703777aaa8e5f03b0191d34
ronys <ronys@users.sourceforge.net>
2014-09-19 17:45:07 +0100

    v4 duplicating shortcut entry works

--- src/core/ItemData.cpp
+++ src/core/ItemData.cpp
@@ -1323,6 +1323,14 @@ void CItemData::SetField(FieldType ft, const unsigned char *value, size_t length
 void CItemData::CreateUUID(FieldType ft)
 {
   CUUID uuid;
+  if (ft == END) {
+    switch (m_entrytype) {
+    case ET_NORMAL: ft = UUID; break;
+    case ET_ALIAS: ft = ALIASUUID; break;
+    case ET_SHORTCUT: ft = SHORTCUTUUID; break;
+    default: ASSERT(0); ft = UUID; break;
+    }
+  }
   SetUUID(*uuid.GetARep(), ft);
 }
 
