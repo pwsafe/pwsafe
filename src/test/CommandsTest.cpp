42f70d94f60a1c4ca45cf1e9ce49d1b39ae69075
ronys <ronys@users.sourceforge.net>
2014-09-22 19:47:43 +0300

    Added CreateShortcutEntry unit test

--- src/test/CommandsTest.cpp
+++ src/test/CommandsTest.cpp
@@ -45,3 +45,53 @@ TEST_F(CommandsTest, AddItem)
   EXPECT_EQ(0, core.GetNumEntries());
 }
 
+TEST_F(CommandsTest, CreateShortcutEntry)
+{
+  CItemData bi, si;
+  bi.CreateUUID();
+  bi.SetTitle(L"base entry");
+  bi.SetPassword(L"base password");
+  const pws_os::CUUID base_uuid = bi.GetUUID();
+
+  si.SetTitle(L"shortcut to base");
+  si.SetPassword(L"[Shortcut]");
+  si.SetShortcut();
+  si.CreateUUID(); // call after setting to shortcut!
+  si.SetBaseUUID(base_uuid);
+
+  time_t t;
+  time(&t);
+  si.SetCTime(t);
+  si.SetXTime((time_t)0);
+  si.SetStatus(CItemData::ES_ADDED);
+
+  MultiCommands *pmulticmds = MultiCommands::Create(&core);
+  pmulticmds->Add(AddEntryCommand::Create(&core, bi));
+  pmulticmds->Add(AddEntryCommand::Create(&core, si));
+  core.Execute(pmulticmds);
+  EXPECT_EQ(2, core.GetNumEntries());
+
+  // Check that the base entry is correctly marked
+  ItemListConstIter iter = core.Find(base_uuid);
+  ASSERT_NE(core.GetEntryEndIter(), iter);
+  EXPECT_TRUE(core.GetEntry(iter).IsShortcutBase());
+
+  core.Undo();
+  EXPECT_EQ(0, core.GetNumEntries());
+  core.Redo();
+  EXPECT_EQ(2, core.GetNumEntries());
+
+  // Delete base, expect both to be gone
+  // Get base from core for correct type
+  const CItemData bi2 = core.GetEntry(core.Find(base_uuid));
+  core.Execute(DeleteEntryCommand::Create(&core, bi2));
+  EXPECT_EQ(0, core.GetNumEntries());
+  core.Undo();
+  EXPECT_EQ(2, core.GetNumEntries());
+
+  // Now just delete the shortcut, check that
+  // base is left, and that it reverts to a normal entry
+  core.Execute(DeleteEntryCommand::Create(&core, si));
+  ASSERT_EQ(1, core.GetNumEntries());
+  EXPECT_TRUE(core.GetEntry(core.Find(base_uuid)).IsNormal());
+}
