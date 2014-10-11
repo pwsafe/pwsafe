5924561e5dc91f35cfc54b717986cf5cc48b052f
c-273@users.surceforge.net <c-273@users.surceforge.net>
2014-10-11 22:45:44 +0100

    Fix compiler warnings in wxWidgets 3.0.x pwsclip.cpp
    Signed-off-by: ronys <ronys@users.sourceforge.net>

--- src/ui/wxWidgets/pwsclip.cpp
+++ src/ui/wxWidgets/pwsclip.cpp
@@ -5,9 +5,30 @@
  * distributed with this code, or available from
  * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
-#include "pwsclip.h"
+
 #include <wx/clipbrd.h>
 #include <wx/dataobj.h>
+
+/*
+
+  NOTE: In VS2013 wxWidgets 3.0.x builds:
+    Both <wx/clipbrd.h> & <wx/dataobj.h> cause 51 warnings about using unsecure
+    versions of standard calls, such as 'wcscpy' instead of 'wcscpy_s', if any
+    previously inluded header file includes <string> even though pre-processor
+    variables _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES and
+    _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES_COUNT are defined.
+
+    The solution is to ensure that any header files containing <string>, e.g.
+    "core/StringX.h", are placed after these two wxWidgets include statements.
+
+  This issue did not occur with wxWidgets 2.8.12.
+
+  For this reason, "pwsclip.h", which includes "core/StringX.h" that also includes
+  <string>, is placed here after <wx/clipbrd.h> & <wx/dataobj.h>.
+
+*/
+
+#include "pwsclip.h"
 
 #ifdef __WXMSW__
 #include <wx/msw/msvcrt.h>
