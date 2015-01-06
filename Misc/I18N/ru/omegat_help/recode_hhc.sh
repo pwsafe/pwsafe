52d7284977ef4d15606a4f38b764a2ec674e6a66
Andrey Kuznetsov <pm_kan@mail.ru>
2015-01-06 21:53:19 +0300

    Fix encoding for Russian help index (WX)

--- Misc/I18N/ru/omegat_help/recode_hhc.sh
+++ Misc/I18N/ru/omegat_help/recode_hhc.sh
@@ -2,4 +2,5 @@
 GIT_ROOT=`git rev-parse --show-toplevel`
 HHC_PATH=$GIT_ROOT/help/pwsafeRU
 iconv -f utf-8 -t cp1251 $HHC_PATH/pwsafe.hhc -o $HHC_PATH/pwsafeRU.hhc
+sed -i -e "s/<HEAD>/<HEAD><meta http-equiv='content-type' content='text\/html; charset=Windows-1251'>/" $HHC_PATH/pwsafeRU.hhc
 rm $HHC_PATH/pwsafe.hhc
