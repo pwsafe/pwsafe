aa87eb22ff50d5aa76ffd16424cab110fab626c9
c-273@users.surceforge.net <c-273@users.surceforge.net>
2015-09-11 16:06:12 +0100

    Text tweaks without affecting import/export
    Signed-off-by: ronys <ronys@pwsafe.org>

--- src/ui/Windows/filter/PWFilterLC.cpp
+++ src/ui/Windows/filter/PWFilterLC.cpp
@@ -1452,15 +1452,18 @@ void CPWFilterLC::SetUpComboBoxData()
         stf.ftype = FT_RUNCMD;
         vFcbx_data.push_back(stf);
 
-        stf.cs_text = CItemData::FieldName(CItemData::DCA).c_str();
+        //stf.cs_text = CItemData::FieldName(CItemData::DCA).c_str();
+        stf.cs_text.LoadString(IDS_DCALONG);
         stf.ftype = FT_DCA;
         vFcbx_data.push_back(stf);
 
-        stf.cs_text = CItemData::FieldName(CItemData::SHIFTDCA).c_str();
+        //stf.cs_text = CItemData::FieldName(CItemData::SHIFTDCA).c_str();
+        stf.cs_text.LoadString(IDS_SHIFTDCALONG);
         stf.ftype = FT_SHIFTDCA;
         vFcbx_data.push_back(stf);
 
-        stf.cs_text = CItemData::FieldName(CItemData::EMAIL).c_str();
+        //stf.cs_text = CItemData::FieldName(CItemData::EMAIL).c_str();
+        stf.cs_text.LoadString(IDS_EMAIL);
         stf.ftype = FT_EMAIL;
         vFcbx_data.push_back(stf);
 
@@ -1749,6 +1752,7 @@ void CPWFilterLC::DrawComboBox(const int iSubItem, const int index)
         m_pComboBox->AddString(vWCFcbx_data[i].cs_text);
         m_pComboBox->SetItemData(i, vWCFcbx_data[i].ftype);
       }
+      SetComboBoxWidth();
       break;
     case FLC_LGC_COMBOBOX:
       for (int i = 0; i < (int)vLcbx_data.size(); i++) {
@@ -1864,6 +1868,44 @@ void CPWFilterLC::DrawComboBox(const int iSubItem, const int index)
   m_pComboBox->SetCurSel(nindex);
   m_pComboBox->ShowWindow(SW_SHOW);
   m_pComboBox->BringWindowToTop();
+}
+
+void CPWFilterLC::SetComboBoxWidth()
+{
+  // Find the longest string in the combo box.
+  CString    str;
+  CSize      sz;
+  int        dx = 0;
+  TEXTMETRIC tm;
+  CDC *pDC = m_pComboBox->GetDC();
+  CFont *pFont = m_pComboBox->GetFont();
+
+  // Select the listbox font, save the old font
+  CFont *pOldFont = pDC->SelectObject(pFont);
+
+  // Get the text metrics for avg char width
+  pDC->GetTextMetrics(&tm);
+
+  for (int i = 0; i < m_pComboBox->GetCount(); i++) {
+    m_pComboBox->GetLBText(i, str);
+    sz = pDC->GetTextExtent(str);
+
+    // Add the avg width to prevent clipping
+    sz.cx += tm.tmAveCharWidth;
+
+    if (sz.cx > dx)
+      dx = sz.cx;
+  }
+
+  // Select the old font back into the DC
+  pDC->SelectObject(pOldFont);
+  m_pComboBox->ReleaseDC(pDC);
+
+  // Adjust the width for the vertical scroll bar and the left and right border.
+  dx += ::GetSystemMetrics(SM_CXVSCROLL) + 2 * ::GetSystemMetrics(SM_CXEDGE);
+
+  // Set the width of the list box so that every item is completely visible.
+  m_pComboBox->SetDroppedWidth(dx);
 }
 
 void CPWFilterLC::DeleteEntry(FieldType ftype)
