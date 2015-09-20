pwsafe/pwsafe

commit d04da24828f7854862938a88e075b1c6184818d1

[BR1265] Linux pws_os::splitpath
Returns 'harmless' dir and drive:
if dir's empty it's set to "./"
drive is set to "/" if path's absolute (i.e., if dir starts with '/'),
otherwise it's set to "./"

src/os/linux/dir.cpp
@@ -56,16 +56,19 @@ bool pws_os::chdir(const stringT &dir)
   return retval;
 }
 
-  // In following, drive will be empty on non-Windows platforms
+
 bool pws_os::splitpath(const stringT &path,
                        stringT &drive, stringT &dir,
                        stringT &file, stringT &ext)
 {
   if (path.empty())
     return false;
-  drive = _T("");
+
   stringT::size_type last_slash = path.find_last_of(_T("/"));
   dir = path.substr(0, last_slash + 1);
+  if (dir.empty())
+    dir = _T("./");
+  drive = (dir[0] == '/') ? _T("/") : _T("./");
   stringT::size_type last_dot = path.find_last_of(_T("."));
   if (last_dot != stringT::npos && last_dot > last_slash) {
     file = path.substr(last_slash + 1, last_dot - last_slash - 1);

commit 05f7d5744c76165be1f2d30f10341e03ef6a9355

Fixed regression caused by d04da24 (BR1265)
Caused wx to assert when creating a new database :-(

src/os/linux/dir.cpp
@@ -83,10 +83,18 @@ bool pws_os::splitpath(const stringT &path,
 stringT pws_os::makepath(const stringT &drive, const stringT &dir,
                          const stringT &file, const stringT &ext)
 {
-  stringT retval(drive);
-  retval += dir;
-  if (!dir.empty() && retval[retval.length()-1] != '/')
-    retval += _T("/");
+  stringT retval;
+  /**
+   * drive and dir can semantically be "./" with no ill effect,
+   * but wxFileDialog doesn't like a "filename" with "/"s.
+   */
+  if (drive != L"./")
+    retval = drive;
+  if (dir != L"./") {
+    retval += dir;
+    if (!dir.empty() && retval[retval.length()-1] != '/')
+      retval += _T("/");
+  }
   retval += file;
   if (!ext.empty()) {
     retval += _T(".");