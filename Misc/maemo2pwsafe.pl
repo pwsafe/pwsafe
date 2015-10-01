pwsafe/pwsafe

commit 3b45e5399f02cf1c910e3dea9111049844b1e040

First BETA release for FreeBSD
  * Some ifdefs for FreeBSD specifics in the linux code
  * Refactor ftime to gettimeofday (time.h vs timeb.h) this should be available on any OS since 2000

Refactoring src/os/linux to src/os/unix is still pending

Misc/mkversion.pl
@@ -86,7 +86,7 @@ close(VH);
 if (!-e $OUTFILE) {
     move($TMPFILE, $OUTFILE) || die "Couldn't move $TMPFILE to $OUTFILE: $!\n";
 } else {
-    `/usr/local/bin/diff -q $TMPFILE $OUTFILE > /dev/null`;
+    `/usr/bin/diff -q $TMPFILE $OUTFILE > /dev/null`;
     if ($VERSTRING ne "local" && $? != 0) {
         unlink $OUTFILE || die "Couldn't remove old $OUTFILE\n";
         move($TMPFILE, $OUTFILE) || die "Couldn't move $TMPFILE to $OUTFILE: $!\n";

Misc/unicode2keysym.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl -w
 #
 # Copyright (c) 2003-2015 Rony Shapiro <ronys@users.sourceforge.net>.
 # All rights reserved. Use of the code is allowed under the

Makefile.freebsd
@@ -42,6 +42,7 @@ export CFLAGS+=-I/usr/local/include -DNO_YUBI
 export CC=clang
 export CXX=clang++
 export CPP=clang++
+export PLATFORM=FreeBSD
 
 all: I18N unicodedebug unicoderelease
 

Misc/check_po.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf8 -*-
 
 __version__ = '$Id$'

Misc/pwsafe-decode.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 
 use Crypt::Blowfish;
 use Digest::SHA1;

Misc/pwsafe-decode2.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 
 # Updated April 5, 2005 by Jason Diamond.
 

README.FREEBSD
@@ -1,0 +1,23 @@
+= Introduction =
+The FreeBSD port of PasswordSafe is currently in BETA.
+This means that (1) you should take care to keep copies of the
+database to protect against possible loss of data due to bugs, and (2)
+there are several unimplemented features. Nonetheless, we feel that
+this is good enough to release as an early beta to gather feedback
+from a wider audience.
+
+= Supported =
+This has only been tested on FreeBSD 10.2 amd64 with wx 3.0 so far.
+== Known not working ==
+ * The help system
+ * Debug builds
+
+= Installation =
+ 1. Install x11-toolkits/wxgtk30 and devel/gmake-lite
+ 2. Run `NO_YUBI=1 gmake release
+ 3. Your `pwsafe` binary is in `src/ui/wxWidgets/GCCUnicodeRelease`
+ 4. At start you get a warning about the help system
+
+= Reporting bugs =
+Please submit bugs via https://github.com/pwsafe/pwsafe
+Make sure you include output of `uname -a`

src/core/Util.cpp
@@ -23,8 +23,8 @@
 #include "os/dir.h"
 
 #include <stdio.h>
-#include <sys/timeb.h>
 #include <time.h>
+#include <sys/time.h>
 #include <sstream>
 #include <iomanip>
 #include <errno.h>
@@ -432,11 +432,11 @@ void PWSUtil::GetTimeStamp(stringT &sTimeStamp, const bool bShort)
   ptimebuffer = new _timeb;
   _ftime_s(ptimebuffer);
 #else
-  struct timeb *ptimebuffer;
-  ptimebuffer = new timeb;
-  ftime(ptimebuffer);
+  struct timeval *ptimebuffer;
+  ptimebuffer = new timeval;
+  gettimeofday(ptimebuffer, NULL);
 #endif
-  StringX cmys_now = ConvertToDateTimeString(ptimebuffer->time, TMC_EXPORT_IMPORT);
+  StringX cmys_now = ConvertToDateTimeString(ptimebuffer->tv_sec, TMC_EXPORT_IMPORT);
 
   if (bShort) {
     sTimeStamp = cmys_now.c_str();
@@ -444,7 +444,7 @@ void PWSUtil::GetTimeStamp(stringT &sTimeStamp, const bool bShort)
     ostringstreamT *p_os;
     p_os = new ostringstreamT;
     *p_os << cmys_now << TCHAR('.') << setw(3) << setfill(TCHAR('0'))
-          << static_cast<unsigned int>(ptimebuffer->millitm);
+          << static_cast<unsigned int>(ptimebuffer->tv_usec);
 
     sTimeStamp = p_os->str();
     delete p_os;

commit c0e4d759038bf5d476f0c6c636babde8060778a2

Fix Linux build breakage from 3b45e53

Misc/maemo2pwsafe.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 #
 # Quick hack to convert text files exported by maemo pwsafe clone
 # to something that PasswordSafe can import as text.

Misc/make-latest-xml.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl
 #
 # Copyright (c) 2003-2015 Rony Shapiro <ronys@users.sourceforge.net>.
 # All rights reserved. Use of the code is allowed under the
@@ -11,6 +11,8 @@
 # The script parses the file passed as arg (version.h)
 # to get the information it needs.
 # For the XML format description, see ../src/ui/Windows/AboutDlg.cpp
+
+use warnings;
 
 my @verline;
 my ($WinMajor, $WinMinor, $WinBuild, $WinRevision);

Misc/mkversion.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl
 #
 # Copyright (c) 2003-2015 Rony Shapiro <ronys@users.sourceforge.net>.
 # All rights reserved. Use of the code is allowed under the

Misc/passwordsafe-db-remapper.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 #
 # passwordsafe-db-remapper v1.0.0
 #

Misc/rc2cpp.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl
 #
 # Copyright (c) 2003-2015 Rony Shapiro <ronys@users.sourceforge.net>.
 # All rights reserved. Use of the code is allowed under the

Misc/unicode2keysym.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl -w
+#!/usr/bin/env perl
 #
 # Copyright (c) 2003-2015 Rony Shapiro <ronys@users.sourceforge.net>.
 # All rights reserved. Use of the code is allowed under the