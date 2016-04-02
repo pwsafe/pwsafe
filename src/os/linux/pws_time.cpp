033092313ab1b9ac89c83a4390d6a3606245449e
Bill Blough <devel@blough.us>
2016-04-02 15:46:29 -0400

    Logic mismatch between gmtime64_r/gmtime_r and gmtime64_s     gmtime_r returns pointer to struct on success or NULL on error

--- src/os/linux/pws_time.cpp
+++ src/os/linux/pws_time.cpp
@@ -15,7 +15,7 @@
 
 int gmtime64_r(const __time64_t *timep, struct tm *result)
 {
-  return gmtime_r(reinterpret_cast<const time_t *>(timep), result) == 0;
+  return gmtime_r(reinterpret_cast<const time_t *>(timep), result) != 0;
 }
 
 int pws_os::asctime(TCHAR *s, size_t, tm const *t)
