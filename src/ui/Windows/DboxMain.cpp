577614b08479ea79083d0a56a964b7e3e5b3b7c7
c-273 <c-273@users.sourceforge.net>
2016-11-14 15:26:32 +0000

    Added comments in area where BR1124 is negated.

--- src/ui/Windows/DboxMain.cpp
+++ src/ui/Windows/DboxMain.cpp
@@ -1519,6 +1519,14 @@ void DboxMain::DoBrowse(const bool bDoAutotype, const bool bSendEmail)
       CItemData *pbci = GetBaseEntry(pci);
       ASSERT(pbci != NULL);
       sx_pswd = pbci->GetPassword();
+
+      // Shortcut everything is from base!
+      // This contradicts BR1124 fix in 3.32, which changed autotype 
+      // to use group/title/user from shortcut but not RunCmd.
+      // I believe that BR1124 should not have been implemented.  If user wants
+      // to use the group/title/user of a dependent entry, then they should use
+      // duplicate the base entry and make into an alias.
+      // In fact, it shouldn't have been a BR but a FR!
       if (pci->IsShortcut())
         pci = pbci;
     } else
