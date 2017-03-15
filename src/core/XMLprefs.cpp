pwsafe/pwsafe

commit 2a8d6446b5dc234815b1ed3fc5b568315c54d962

Sort application preferences in configuration files when saving Helps find particular preference during debugging. Update help re: username/hostname in config file when using -u and -h command line parameters Ensure all uses of of "Effective user & host" conform to XML standards.

src/core/XMLprefs.cpp
@@ -62,7 +62,7 @@ bool CXMLprefs::CreateXML(bool bLoad)
     return m_pXMLDoc != NULL;
 }
 
-bool CXMLprefs::Load()
+bool CXMLprefs::XML_Load()
 {
   // Already loaded?
   if (m_pXMLDoc != NULL)
@@ -108,11 +108,27 @@ bool CXMLprefs::Load()
   return true;
 }
 
-bool CXMLprefs::Store()
+void SortPreferences(pugi::xml_node parent)
+{
+  // Sort the application preferences of this host/user (case insensitive)
+  std::vector<pugi::xml_node> children(parent.begin(), parent.end());
+
+  std::sort(children.begin(), children.end(),
+    [](pugi::xml_node l, pugi::xml_node r) {
+          return _tcsicmp(l.name(), r.name()) < 0;
+        });
+
+  for (pugi::xml_node n : children) {
+    parent.append_move(n);
+  }
+}
+
+bool CXMLprefs::XML_Store(const stringT &csBaseKeyName)
 {
   bool retval = false;
   bool alreadyLocked = m_bIsLocked;
   pugi::xml_node decl;
+  pugi::xml_node nodePrefs;
 
   if (!alreadyLocked) {
     stringT locker;
@@ -140,6 +156,10 @@ bool CXMLprefs::Store()
   decl.append_attribute(_T("version")) = _T("1.0");
   decl.append_attribute(_T("encoding")) = _T("utf-8");
   decl.append_attribute(_T("standalone")) = _T("yes");
+
+  nodePrefs = m_pXMLDoc->first_element_by_path(csBaseKeyName.c_str(), _T('\\'));
+  if (nodePrefs != NULL)
+    SortPreferences(nodePrefs);
 
   retval = m_pXMLDoc->save_file(m_csConfigFile.c_str(), _T("  "),
                          pugi::format_default | pugi::format_write_bom,
@@ -212,8 +232,9 @@ int CXMLprefs::SetPreference(const stringT &sPath, const stringT &sValue)
   pugi::xml_node prefnode = (node.first_child().type() == pugi::node_pcdata) ?
      node.first_child() : node.append_child(pugi::node_pcdata);
 
-  if (!prefnode.set_value(sValue.c_str()))
-     iRetVal = XML_PUT_TEXT_FAILED;
+  if (!prefnode.set_value(sValue.c_str())) {
+    iRetVal = XML_PUT_TEXT_FAILED;
+  }
 
   return iRetVal;
 }
@@ -375,7 +396,7 @@ std::vector<st_prefShortcut> CXMLprefs::GetShortcuts(const stringT &csBaseKeyNam
     cur.cModifier |= itemp == 0 ? 0 : PWS_HOTKEYF_SHIFT;
 
     // wxWidgets only - set values so not lost in XML file 
-    // but not use them in Windows MFC - they are never tested in MFC code
+    // but not used in Windows MFC - they are never tested in MFC code
     // when creating the necessary hotkeys/shortcuts
     itemp = shortcut.attribute(_T("Win")).as_int();
     cur.cModifier |= itemp == 0 ? 0 : PWS_HOTKEYF_WIN;

help/default/html/cli.html
@@ -83,6 +83,17 @@ in the <a href="preferences.html">Preferences</a> Section). Note that if the fil
 and/or path has spaces, it should be enclosed in double quotes.</li> 
 </ul>
 
+<p>Note: As the configuration file uses XML, the username and hostname must satisfy the
+XML standards:
+<ul>
+<li>The first character must be alphanumeric. If not, a "u" will be prefixed to the username
+and a "h" will be prefixed to the hostname as appropriate.</li>
+<li>They must only contain alphanumeric characters or one or more of the underscore, hyphen,
+colon or full stop characters ("_-:."). If other characters are present, they will be replaced by the
+underscore ("_").</li>
+</ul>
+</p>
+
 <p>Finally, there are some special command line flags beginning with "--".  They are:
 </p>
 

src/core/PWSprefs.cpp
@@ -1367,7 +1367,7 @@ bool PWSprefs::LoadProfileFromFile()
   stringT ts, csSubkey;
 
   m_pXML_Config = new CXMLprefs(m_configfilename.c_str());
-  if (!m_pXML_Config->Load()) {
+  if (!m_pXML_Config->XML_Load()) {
     if (!m_pXML_Config->getReason().empty() &&
         m_pReporter != NULL)
       (*m_pReporter)(m_pXML_Config->getReason()); // show what went wrong
@@ -1502,7 +1502,7 @@ void PWSprefs::SaveApplicationPreferences()
     } else { // acquired lock
       // if file exists, load to get other values
       if (pws_os::FileExists(m_configfilename.c_str()))
-        m_pXML_Config->Load(); // we ignore failures here. why bother?
+        m_pXML_Config->XML_Load(); // we ignore failures here. why bother?
     }
   }
   UpdateTimeStamp();
@@ -1639,7 +1639,7 @@ void PWSprefs::SaveApplicationPreferences()
 
   if (m_ConfigOption == CF_FILE_RW ||
       m_ConfigOption == CF_FILE_RW_NEW) {
-    if (m_pXML_Config->Store()) // can't be new after succ. store
+    if (m_pXML_Config->XML_Store(m_csHKCU_PREF)) // can't be new after succ. store
       m_ConfigOption = CF_FILE_RW;
     else
     if (!m_pXML_Config->getReason().empty() &&
@@ -1674,7 +1674,7 @@ void PWSprefs::SaveShortcuts()
     } else { // acquired lock
       // if file exists, load to get other values
       if (pws_os::FileExists(m_configfilename.c_str()))
-        m_pXML_Config->Load(); // we ignore failures here. why bother?
+        m_pXML_Config->XML_Load(); // we ignore failures here. why bother?
     }
   }
 
@@ -1692,7 +1692,7 @@ void PWSprefs::SaveShortcuts()
 
   if (m_ConfigOption == CF_FILE_RW ||
       m_ConfigOption == CF_FILE_RW_NEW) {
-    if (m_pXML_Config->Store()) // can't be new after succ. store
+    if (m_pXML_Config->XML_Store(m_csHKCU_PREF)) // can't be new after succ. store
       m_ConfigOption = CF_FILE_RW;
     else
     if (!m_pXML_Config->getReason().empty() &&

src/core/XMLprefs.h
@@ -35,8 +35,8 @@ public:
   ~CXMLprefs() { UnloadXML(); }
 
   // Implementation
-  bool Load();
-  bool Store();
+  bool XML_Load();
+  bool XML_Store(const stringT &csBaseKeyName);
   bool Lock(stringT &locker); // if fails, locker points to culprit
   void Unlock();
 
@@ -63,7 +63,7 @@ public:
   // Remove a host/user from current configuration file
   bool RemoveHostnameUsername(const stringT &sHost, const stringT &sUser,
                               bool &bNoMoreNodes);
-  
+
   enum {XML_SUCCESS = 0,
         XML_LOAD_FAILED,
         XML_NODE_NOT_FOUND,

src/ui/Windows/MainMenu.cpp
@@ -381,11 +381,13 @@ void DboxMain::SetUpInitialMenuStrings()
         stxst.id == ID_MENUITEM_RENAMEGROUP) {
       continue;
     }
+
     iter = m_MapMenuShortcuts.find(stxst.id);
     if (iter == m_MapMenuShortcuts.end()) {
       // Unknown Control ID - ignore - maybe used by a later version of PWS
       continue;
     }
+
     // Check not already in use (ignore if deleting current shortcut)
     if (stxst.siVirtKey != 0) {
       st_mst.siVirtKey = stxst.siVirtKey;
@@ -400,9 +402,10 @@ void DboxMain::SetUpInitialMenuStrings()
         continue;
       }
     }
+
     if ((iter->second.siVirtKey  != stxst.siVirtKey  ||
          iter->second.cModifier != stxst.cModifier)) {
-      // User changed a added or shortcut
+      // User changed or added a shortcut
       iter->second.siVirtKey  = stxst.siVirtKey;
       iter->second.cModifier = stxst.cModifier;
     }

src/ui/Windows/MigratePrefs.cpp
@@ -101,18 +101,29 @@ bool PerformConfigMigration()
   CXMLprefs oldXMLConfig(wsExecDirCfgFile.c_str()); // for removing user/host from old
 
   // Create the new one from it just containing our host/user
-  if (!newXMLConfig.Load())
+  if (!newXMLConfig.XML_Load())
     return false; // WTF?!?
+
   const SysInfo *si = SysInfo::GetInstance();
-  bool rc = newXMLConfig.MigrateSettings(wsDefaultCfgFile,
-                                         si->GetEffectiveHost(), si->GetEffectiveUser());
+  stringT hn = si->GetEffectiveHost();
+  PWSprefs::XMLify(charT('H'), hn);
+  stringT un = si->GetEffectiveUser();
+  PWSprefs::XMLify(charT('u'), un);
+
+  stringT csHKCU_PREF = _T("Pwsafe_Settings\\");
+  csHKCU_PREF += hn.c_str();
+  csHKCU_PREF += _T("\\");
+  csHKCU_PREF += un.c_str();
+  csHKCU_PREF += _T("\\Preferences");
+
+  bool rc = newXMLConfig.MigrateSettings(wsDefaultCfgFile, hn, un);
   if (rc) {
     // That worked, now remove us from the old one config file
     // in the Installation directory
     newXMLConfig.Unlock();
 
     // Since we now have new config file, remove host/user from old.
-    if (!oldXMLConfig.Load()) {
+    if (!oldXMLConfig.XML_Load()) {
       rc = false;
       if (!oldXMLConfig.getReason().empty()) {
         CGeneralMsgBox gmb;
@@ -133,11 +144,9 @@ bool PerformConfigMigration()
     // Now remove this hostname/username from old configuration file in the
     // installation directory (as long as everything OK and it is not R-O)
     if (rc && !bExecCFRO) {
-      rc = oldXMLConfig.RemoveHostnameUsername(si->GetEffectiveHost(),
-                                               si->GetEffectiveUser(), bNoMoreNodes);
+      rc = oldXMLConfig.RemoveHostnameUsername(hn, un, bNoMoreNodes);
       if (rc) {
-        // Save it
-        oldXMLConfig.Store();
+        oldXMLConfig.XML_Store(csHKCU_PREF);
 
         // However, if no more host/user nodes in this file - delete the
         // configuration file from the installation directory!

commit 0cf464ace7ec9bae0886406ec987224c424baaa5

Linux now build again
Fixed breakage introduced in 2a8d644

src/core/XMLprefs.cpp
@@ -17,6 +17,7 @@
 #include "pugixml/pugixml.hpp"
 
 #include "os/typedefs.h"
+#include "os/pws_tchar.h"
 #include "os/sleep.h"
 #include "os/debug.h"
 #include "os/KeySend.h"
@@ -278,7 +279,7 @@ int CXMLprefs::SetPreference(const stringT &sPath, const stringT &sValue)
   if (!prefnode.set_value(sValue.c_str())) {
     iRetVal = XML_PUT_TEXT_FAILED;
   } else {
-    if (_tcscmp(sPath.substr(sPath.size() - 7).c_str(), _T("\\HotKey")) == 0) {
+    if (sPath.substr(sPath.size() - 7) == _T("\\HotKey")) {
       SetHotKeyComment(node, sValue);
     }
   }