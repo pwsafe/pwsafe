862cef8d3f6d99731e0eedb1cee88ed75321be60
ronys <ronys@pwsafe.org>
2017-06-06 21:08:30 +0300

    Added standard copyright notice to Saurav's cli files
    (Crediting Saurav and with his permission)

--- src/ui/cli/search.cpp
+++ src/ui/cli/search.cpp
@@ -1,10 +1,11 @@
-//
-//  search.cpp
-//  pwsafe-xcode6
-//
-//  Created by Saurav Ghosh on 19/06/16.
-//  Copyright (c) 2016 Open Source Software. All rights reserved.
-//
+/*
+ * Created by Saurav Ghosh on 19/06/16.
+ * Copyright (c) 2003-2017 Rony Shapiro <ronys@pwsafe.org>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 
 #include "./search.h"
 #include "./argutils.h"
