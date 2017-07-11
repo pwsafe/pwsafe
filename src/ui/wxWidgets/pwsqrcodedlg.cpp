43a1023967390dab159d3c1bebe457cb178fb7d5
saurav <sauravg@users.sf.net>
2017-07-11 18:00:52 +0530

    qrcode: WIP: can show a clipped QR code for now

--- src/ui/wxWidgets/pwsqrcodedlg.cpp
+++ src/ui/wxWidgets/pwsqrcodedlg.cpp
@@ -10,6 +10,8 @@
 #include "wx/wx.h"
 #endif
 
+#include <algorithm>
+
 #include <wx/statline.h>
 
 #include <qrencode.h>
@@ -46,32 +48,100 @@ wxBitmap QRCodeBitmap( const StringX &data )
 		return wxBitmap();
 
 	auto qc_data = reinterpret_cast<const char *>(utf8str);
-	QRcodePtr qc(QRcode_encodeString8bit(qc_data, 0, QR_ECLEVEL_H), &QRcode_free);
+	QRcodePtr qc(QRcode_encodeString8bit(qc_data, 2, QR_ECLEVEL_H), &QRcode_free);
 
-	const size_t nbytes = (qc->width + 7)/8;
+	//constexpr auto intendedImageWidth = 300;
+	constexpr auto margin = 24;
+	constexpr int scale = 8;//(intendedImageWidth - 2*margin)/qc->width;
+	//scale = scale <= 0? 1: scale;
+	const int imageWidth = 2*margin + scale*qc->width;
+
+
+	const size_t nbytes = (imageWidth*imageWidth + 7)/8;
+	std::cout << "QR code width: " << qc->width << ", version " << qc->version << '\n'
+			<<	"image bytes: " << nbytes << '\n'
+			<<	"image width: " << imageWidth << '\n';
+
+	//wxASSERT_MSG(imageWidth*imageWidth == nbytes, _T("QRcode image width calculation is incorrect"));
+
 	std::vector<char> xbmp(nbytes);
 
-	const auto max_idx = std::min(nbytes, static_cast<size_t>(qc->width/8));
-	for (auto n = 0; n < max_idx; ++n) {
-		auto start = n*8;
-		xbmp[n] = 	((qc->data[start+0] & 1) << 0) |
-					((qc->data[start+1] & 1) << 1) |
-					((qc->data[start+2] & 1) << 2) |
-					((qc->data[start+3] & 1) << 3) |
-					((qc->data[start+4] & 1) << 4) |
-					((qc->data[start+5] & 1) << 5) |
-					((qc->data[start+6] & 1) << 6) |
-					((qc->data[start+7] & 1) << 7);
-	}
+	auto write_bits = [&xbmp](size_t start, size_t count, int bitval) {
+		constexpr char rbits[] = {0, 0b1, 0b11, 0b111, 0b1111, 0b11111, 0b111111, 0b1111111};
 
-	if (nbytes > max_idx) {
-		for (auto i = 0; i < qc->width%8; ++i) {
-			xbmp[nbytes-1] |= (qc->data[max_idx*8 + i] & 1);
+		if (start % 8) {
+			// num bits set in the last byte
+			const auto nset = start%8;
+			const auto last_byte = start/8;
+			// clear out the leftmost bits to be set now
+			const char b = (rbits[nset] & xbmp[last_byte]);
+			if (bitval) {
+				const auto mask = (rbits[8 - nset] << nset);
+				xbmp[last_byte] |= mask ;
+			}
 		}
-	}
+
+		const char byte = bitval? 0xff: 0;
+		for (size_t n = 0; n < count/8; n++)
+			xbmp[start/8 + n] = byte;
+
+		if ((start + count) % 8) {
+			xbmp[start/8 + count/8 + 1] = rbits[count%8];
+		}
+	};
+
+	using std::fill_n;
+	using std::copy_n;
+
+	// fill top margin
+	write_bits(0, imageWidth*margin, 0);
+	std::cout << "Top margin covers " << imageWidth*margin << '\n';
 
 	const int height = qc->width;
-	return wxBitmap( xbmp.data(), qc->width, height);
+	for (auto row = 0; row < height; ++row) {
+
+		const auto bmpRowStart = (8*row + margin)*imageWidth;
+		std::cout << "Image row " << row << ": " << bmpRowStart << '\n';
+
+		wxASSERT_MSG(bmpRowStart < nbytes*8 - imageWidth, _T("QRcode image width calculation is incorrect"));
+		
+		// left margin
+		write_bits(bmpRowStart, margin, 0);
+
+		auto bmpQRDataIndex = bmpRowStart + margin;
+		for (auto col = 0; col < qc->width; ++col) {
+			wxASSERT(bmpQRDataIndex < bmpRowStart + imageWidth);
+			const auto qcrow = row;
+			const auto qcidx = qcrow*qc->width + col;
+			const auto bit = qc->data[qcidx] & 1;
+
+			// This relies on the fact that scale is 8
+			//const auto byte = bit? 0xff: 0;
+			//xbmp[bmpQRDataIndex + col] = byte;
+			//bmpQRDataIndex++;
+			write_bits(bmpQRDataIndex + col, scale, bit);
+			bmpQRDataIndex += scale;
+		}
+
+		// right margin
+		//fill_n(xbmp.begin() + bmpQRDataIndex, (imageWidth - (bmpQRDataIndex - bmpRowStart))/8, 0);
+		write_bits(bmpQRDataIndex, imageWidth - (bmpQRDataIndex - bmpRowStart), 0);
+
+		// copy the row "scale -1" times over
+		for (auto s = 1; s < scale; ++s) {
+			auto src = bmpRowStart/8;
+			auto dst = bmpRowStart/8 + s*imageWidth/8;
+			const auto nbytes = imageWidth/8;
+			std::cout << "copying " << nbytes << " bytes in image row " << s << " : " << src << " => " << dst <<  '\n';
+			copy_n(xbmp.begin() + src, nbytes, xbmp.begin() + dst);
+		}
+
+	}
+
+	// fill bottom margin
+	write_bits(nbytes - imageWidth*margin, imageWidth*margin, 0);
+
+	return wxBitmap( xbmp.data(), imageWidth, imageWidth);
 }
 
 PWSQRCodeDlg::PWSQRCodeDlg(wxWindow* parent,
@@ -94,21 +164,25 @@ void PWSQRCodeDlg::CreateControls(const StringX &data, const wxString &descripti
 	dlgSizer->AddSpacer(TopMargin);
 
 	dlgSizer->Add( new wxStaticText(this, wxID_ANY, description) );
-
-	dlgSizer->Add( new wxStaticLine(this) );
-
+	dlgSizer->AddSpacer(RowSeparation);
+	dlgSizer->Add( new wxStaticLine(this), wxSizerFlags().Expand() );
 	dlgSizer->AddSpacer(RowSeparation);
 
 	wxBitmap bmp = QRCodeBitmap(data);
 	if (bmp.IsOk() ) {
-		dlgSizer->Add( new wxStaticBitmap(this, wxID_ANY, bmp), wxSizerFlags().Expand() );
+		dlgSizer->Add( new wxStaticBitmap(this, wxID_ANY, bmp), wxSizerFlags().Expand().Proportion(1) );
 	}
 	else
 		dlgSizer->Add( new wxStaticText(this, wxID_ANY, _T("Could not generate QR code")) );
 
 	dlgSizer->AddSpacer(RowSeparation);
-	dlgSizer->Add( new wxStaticLine(this) );
-	dlgSizer->Add( new wxButton(this, wxID_CLOSE), wxSizerFlags().Right().Bottom() );
+	dlgSizer->Add( new wxStaticLine(this), wxSizerFlags().Expand() );
+	dlgSizer->AddSpacer(RowSeparation);
+	wxStdDialogButtonSizer *btnSizer = new wxStdDialogButtonSizer;
+	btnSizer->AddButton( new wxButton(this, wxID_CLOSE) );
+	btnSizer->Realize();
+	dlgSizer->Add( btnSizer, wxSizerFlags().Expand() );
+	dlgSizer->AddSpacer(BottomMargin);
 
 	SetSizerAndFit(dlgSizer);
 }
