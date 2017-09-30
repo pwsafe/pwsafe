7f77d3817ed5ab3440de14adbe0a4377a10a9318
libBletchley <alt.tk-foha8h3m@cool.fr.nf>
2017-09-30 15:56:42 +0200

    setup-deb-dev-env.sh modded to add repo if needed

--- Misc/setup-deb-dev-env.sh
+++ Misc/setup-deb-dev-env.sh
@@ -6,6 +6,27 @@
 # Running this after cloning the git repo (or unpacking the sources)
 # and you should be all set.
 
+# Set your distro name here.  E.g. distro=stretch
+distro=
+
+if ! apt-cache show libwxgtk3.0-dev
+then
+  # The app catalog is missing wxWidgets, so a repository must be added
+  if [ -z "${distro}" ]
+  then
+    # Try to guess the distro since it's unset.
+    distro=$(awk '/debian-security/{print $3}' /etc/apt/sources.list)
+  fi
+  if [ -z "${distro}" ]
+  then
+    printf '%s\n' "Error: distro name could not be parsed from /etc/apt/sources.list." "Defaulting to stable." "Edit $0 and set your distro name on the first line"
+    exit 1
+  fi
+  apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
+  apt-add-repository "deb http://repos.codelite.org/wx3.1.0/debian/ ${distro%%/*} libs"
+  aptitude update
+fi
+
 apt-get install cmake fakeroot g++ gettext git libgtest-dev \
 	libqrencode-dev libuuid1 libwxgtk3.0-dev libxerces-c-dev \
 	libxt-dev libxtst-dev libykpers-1-dev libyubikey-dev make \
