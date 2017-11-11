36cc180a25788f5dc5b8f706ad02b4e2ab103629
Andrey Kuznetsov <pm_kan@mail.ru>
2017-11-11 19:16:36 +0300

    Updates in Russian Help

--- Misc/I18N/ru/omegat_help/recode_hhc.sh
+++ Misc/I18N/ru/omegat_help/recode_hhc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 GIT_ROOT=`git rev-parse --show-toplevel`
 HHC_PATH=$GIT_ROOT/help/pwsafeRU
+# .hhc must be in cp1251
 iconv -f utf-8 -t cp1251 $HHC_PATH/pwsafe.hhc -o $HHC_PATH/pwsafeRU.hhc
-sed -i -e "s/<HEAD>/<HEAD><meta http-equiv='content-type' content='text\/html; charset=Windows-1251'>/" $HHC_PATH/pwsafeRU.hhc
+sed -i -e 's/charset=UTF-8/charset=Windows-1251/' $HHC_PATH/pwsafeRU.hhc
 rm $HHC_PATH/pwsafe.hhc
