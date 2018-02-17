f6e8bc2fdab0fd117cd85f1e8d8f7aad4b28c0f5
Andrey Kuznetsov <pm_kan@mail.ru>
2018-02-17 17:16:56 +0300

    [i18n] Converted Russian help to cp1251 (also related script updates)

---
+++ Misc/I18N/ru/omegat_help/fix_encoding.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+GIT_ROOT=`git rev-parse --show-toplevel`
+HELP_PATH=${GIT_ROOT}/help/pwsafeRU
+
+# make substitutions and change necoding
+for f in ${HELP_PATH}/html/*.html ${HELP_PATH}/pwsafe.{hhc,hhk}; do
+    sed -i -e "s/␣/\&nbsp;/g; s/→/\&roarr;/g; s/ü/\&uuml;/g; s/ß/\&szlig;/g;" "${f}"
+    iconv -f utf-8 -t cp1251 "${f}" -o "${f}.cp1251" && \
+    sed -i -e 's/charset=UTF-8/charset=Windows-1251/' "${f}.cp1251" && \
+    mv "${f}.cp1251" "${f}" || { echo "Error in ${f}"; exit 1; }
+done
+
+# fix names
+for ext in hhc hhk; do
+    mv ${HELP_PATH}/pwsafe.${ext} ${HELP_PATH}/pwsafeRU.${ext}
+done
+
+
--- Misc/I18N/ru/omegat_help/nbsp_fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-GIT_ROOT=`git rev-parse --show-toplevel`
-HELP_PATH=${GIT_ROOT}/help/pwsafeRU
-for f in ${HELP_PATH}/html/*.html ${HELP_PATH}/*.{hhc,hhk}; do
-    sed -i -e "s/␣/\&nbsp;/g" "${f}"
-done
