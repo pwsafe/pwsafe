992f469c7136186cddec61d53d797e6e5c28822d
Andrey Kuznetsov <pm_kan@mail.ru>
2018-02-17 17:16:56 +0300

    [i18n] Converted Russian help to cp1251 (also related script updates)

--- Misc/I18N/ru/omegat_help/recode_hhc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/base -e
-GIT_ROOT=`git rev-parse --show-toplevel`
-HELP_PATH=${GIT_ROOT}/help/pwsafeRU
-# hhc/hhk must be in cp1251
-for ext in hhc hhk; do
-    iconv -f utf-8 -t cp1251 ${HELP_PATH}/pwsafe.${ext} -o ${HELP_PATH}/pwsafeRU.${ext}
-    sed -i -e 's/charset=UTF-8/charset=Windows-1251/' ${HELP_PATH}/pwsafeRU.${ext}
-    rm ${HELP_PATH}/pwsafe.${ext}
-done
