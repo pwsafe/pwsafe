55cea09635772e5783dcea00193181e4defab769
Andrey Kuznetsov <pm_kan@mail.ru>
2018-04-01 12:25:52 +0300

    [i18n] Minor updates in Russian .po (WX)

--- Misc/I18N/ru/omegat_wx/sync_source.sh
+++ Misc/I18N/ru/omegat_wx/sync_source.sh
@@ -18,8 +18,11 @@ ${GIT_ROOT}/Misc/rc2cpp.pl ${GIT_ROOT}/src/core/core.rc2
 #update ui tmx
 po2tmx --source-language=EN-US --language=RU-RU $POFILE ../tm/pwsafe_ru-ui.tmx || exit 1
 #sync wxwidgets ui po
-SRC_BASE=${GIT_ROOT}/src
+SRC_BASE="${GIT_ROOT}/src"
 SRCS="${SRC_BASE}/ui/wxWidgets/*.h ${SRC_BASE}/ui/wxWidgets/*.cpp ${SRC_BASE}/core/*.h ${SRC_BASE}/core/*.cpp ${SRC_BASE}/os/unix/*.h ${SRC_BASE}/os/unix/*.cpp"
-xgettext --no-wrap --from-code=UTF-8 --default-domain=pwsafe --language=C++ --keyword=_ --keyword=wxTRANSLATE --output=./source/pwsafe.pot $SRCS || exit 1
-msgmerge --no-wrap --no-fuzzy-match --output=./source/pwsafe_ru_wx.po $POFILE ./source/pwsafe.pot || exit 1
-rm  ./source/pwsafe.pot
+OUT="./source/pwsafe_ru_wx.po"
+TMP_PO="./source/pwsafe.pot"
+xgettext --no-wrap --from-code=UTF-8 --default-domain=pwsafe --language=C++ --keyword=_ --keyword=wxTRANSLATE --output=${TMP_PO} ${SRCS} || exit 1
+msgmerge --no-wrap --no-fuzzy-match --output=${OUT} "${POFILE}" ${TMP_PO} || exit 1
+sed -i -e "s&${GIT_ROOT}/&&g" "${OUT}"
+rm "${TMP_PO}"
