6c42b6a83284d9d3373d615cc70946f8f0a43817
rafaelx <rafaelx@users.noreply.github.com>
2019-08-01 18:16:28 +0200

    [wx] Comment out unused validator implementation

--- src/ui/wxWidgets/addeditpropsheet.cpp
+++ src/ui/wxWidgets/addeditpropsheet.cpp
@@ -238,30 +238,30 @@ void AddEditPropSheet::Init()
 ////@end AddEditPropSheet member initialisation
 }
 
-class PolicyValidator : public MultiCheckboxValidator
-{
-public:
-  PolicyValidator(int rbID, int ids[], size_t num,
-      const wxString& msg, const wxString& title)
-    : MultiCheckboxValidator(ids, num, msg, title), m_rbID(rbID) {}
-  PolicyValidator(const PolicyValidator &other)
-    : MultiCheckboxValidator(other), m_rbID(other.m_rbID) {}
-  ~PolicyValidator() {}
+// class PolicyValidator : public MultiCheckboxValidator
+// {
+// public:
+//   PolicyValidator(int rbID, int ids[], size_t num,
+//       const wxString& msg, const wxString& title)
+//     : MultiCheckboxValidator(ids, num, msg, title), m_rbID(rbID) {}
+//   PolicyValidator(const PolicyValidator &other)
+//     : MultiCheckboxValidator(other), m_rbID(other.m_rbID) {}
+//   ~PolicyValidator() {}
 
-  wxObject* Clone() const {return new PolicyValidator(m_rbID, m_ids, m_count, m_msg, m_title);}
-  bool Validate(wxWindow* parent) {
-    wxWindow* win = GetWindow()->FindWindow(m_rbID);
-    if (win && win->IsEnabled()) {
-      wxRadioButton* rb = wxDynamicCast(win, wxRadioButton);
-      if (rb && rb->GetValue()) {
-  return true;
-      }
-    }
-    return MultiCheckboxValidator::Validate(parent);
-  }
-private:
-  int m_rbID;
-};
+//   wxObject* Clone() const {return new PolicyValidator(m_rbID, m_ids, m_count, m_msg, m_title);}
+//   bool Validate(wxWindow* parent) {
+//     wxWindow* win = GetWindow()->FindWindow(m_rbID);
+//     if (win && win->IsEnabled()) {
+//       wxRadioButton* rb = wxDynamicCast(win, wxRadioButton);
+//       if (rb && rb->GetValue()) {
+//   return true;
+//       }
+//     }
+//     return MultiCheckboxValidator::Validate(parent);
+//   }
+// private:
+//   int m_rbID;
+// };
 
 static void setupDCAStrings(wxArrayString &as)
 {
