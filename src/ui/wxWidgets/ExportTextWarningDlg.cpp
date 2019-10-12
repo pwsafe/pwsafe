40434508a82439ab5ef93ee73d8af1a557ae93c0
rafaelx <rafaelx@users.noreply.github.com>
2019-10-12 18:59:35 +0200

    [wx] Rename 'editshortcut.h/.cpp' to 'EditShortcutDlg.h/.cpp'

--- src/ui/wxWidgets/ExportTextWarningDlg.cpp
+++ src/ui/wxWidgets/ExportTextWarningDlg.cpp
@@ -6,7 +6,7 @@
  * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 
-/** \file editshortcut.cpp
+/** \file EditShortcutDlg.cpp
 *
 */
 // For compilers that support precompilation, includes "wx/wx.h".
