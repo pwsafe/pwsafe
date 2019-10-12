ae87f27cb4b45adb5617a9b3ccfaaef81c79741c
rafaelx <rafaelx@users.noreply.github.com>
2019-10-12 17:50:53 +0200

    [wx] Rename 'about.h/.cpp' to 'AboutDlg.h/.cpp'

--- src/ui/wxWidgets/AdvancedSelectionDlg.cpp
+++ src/ui/wxWidgets/AdvancedSelectionDlg.cpp
@@ -6,7 +6,7 @@
  * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 
-/** \file about.cpp
+/** \file AboutDlg.cpp
 *
 */
 // For compilers that support precompilation, includes "wx/wx.h".
