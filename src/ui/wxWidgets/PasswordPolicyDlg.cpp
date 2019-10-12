28b8be5f88da2e2bcf5b1ea84ba0e66e181cfa18
rafaelx <rafaelx@users.noreply.github.com>
2019-10-12 22:12:04 +0200

    [wx] Rename 'PasswordPolicy.h/.cpp' to 'PasswordPolicyDlg.h/.cpp'

--- src/ui/wxWidgets/PasswordPolicyDlg.cpp
+++ src/ui/wxWidgets/PasswordPolicyDlg.cpp
@@ -6,7 +6,7 @@
  * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 
-/** \file PasswordPolicy.cpp
+/** \file PasswordPolicyDlg.cpp
 *
 */
 // For compilers that support precompilation, includes "wx/wx.h".
@@ -23,7 +23,7 @@
 ////@begin includes
 ////@end includes
 
-#include "PasswordPolicy.h"
+#include "PasswordPolicyDlg.h"
 #include "core/PolicyManager.h"
 #include "core/PWCharPool.h"
 #include "pwsclip.h"
