15f2ae1209f1e4d08e7f78ce3d98b31db8e33f83
ronys <ronys@pwsafe.org>
2020-04-29 22:41:03 +0300

    Added rpmdevtools to rpm dev setup script

--- Misc/setup-rpm-dev-env.sh
+++ Misc/setup-rpm-dev-env.sh
@@ -6,10 +6,10 @@
 # Running this after cloning the git repo (or unpacking the sources)
 # and you should be all set.
 
-dnf install cmake gcc-c++ git gtest-devel libXt-devel libXtst-devel \
+dnf install cmake file-devel gcc-c++ git gtest-devel libXt-devel libXtst-devel \
     libcurl-devel libuuid-devel libyubikey-devel \
-    make openssl-devel wxGTK3-devel xerces-c-devel ykpers-devel qrencode-devel \
-    file-devel
+    make openssl-devel rpmdevtools wxGTK3-devel xerces-c-devel \
+    ykpers-devel qrencode-devel
 
 #cd /usr/src/gtest
 #mkdir build
