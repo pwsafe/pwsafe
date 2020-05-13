6e7b551856c6f4286c83a2dd7dd1889d3ce97350
ronys <ronys@pwsafe.org>
2020-05-13 19:23:35 +0300

    Support Ubuntu 20 build

--- Misc/setup-deb-dev-env.sh
+++ Misc/setup-deb-dev-env.sh
@@ -6,9 +6,15 @@
 # Running this after cloning the git repo (or unpacking the sources)
 # and you should be all set.
 
+# Ubuntu 20 decided to rename the following
+#LIBWXDEV="ibwxgtk3.0-gtk3-dev"
+#The rest of the Debiverse should use
+LIBWXDEV="ibwxgtk3.0-dev"
+# Two internet points for writing the correct if statement to handle the above...
+
 apt-get install cmake fakeroot g++ gettext git libgtest-dev \
         libcurl4-openssl-dev libqrencode-dev  libssl-dev libuuid1 \
-        libwxgtk3.0-dev libxerces-c-dev libxt-dev libxtst-dev \
+        $LIBWXDEV libxerces-c-dev libxt-dev libxtst-dev \
         libykpers-1-dev libyubikey-dev make pkg-config uuid-dev zip \
         libmagic-dev
 
