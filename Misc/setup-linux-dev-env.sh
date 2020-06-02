290800d0a96dbb4c2a4b489448af1dca3038afc5
ronys <ronys@pwsafe.org>
2020-06-02 07:46:12 +0300

    first shot at unified setup script for dev env

---
+++ Misc/setup-linux-dev-env.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# Silly script to setup the development pre-requisites for
+# building PasswordSafe on Debian and RPM-based distros.
+# Meant to be run as root or sudo'd
+# Running this after cloning the git repo (or unpacking the sources)
+# and you should be all set.
+#
+# Upgrade to this inspired by a simailar script by kreyren
+#
+# Command overrides
+[ -z "$UNAME" ] && UNAME="/usr/bin/uname"
+[ -z "$UNAME" ] && LSB_RELEASE="/usr/bin/lsb_release"
+
+die() {
+    echo $2
+    exit $1
+}
+
+# Exit on anything unexpected
+set -e
+
+# Check if we're effectively root
+if [ ! "$(id -u)" = 0 ]; then
+    die 1 "Run as root or under sudo"
+fi
+
+# See if we're Linux by checking the kernel
+if command -v $UNAME 1>/dev/null; then
+	KERNEL="$($UNAME -s)"
+else
+    die 2 "Could not find unname command, can't identify kernel"
+fi
+
+if [ "$KERNEL" = "Linux" ]; then
+    # OK, let's see what distro we have here
+    if command -v lsb_release 1>/dev/null; then
+        DISTRO="$(lsb_release -si | tr '[:upper:]' '[:lower:]')"
+    elif [ -f /etc/os-release ]; then
+        DISTRO="$(grep -o "^ID\=.*" /etc/os-release | sed 's#^ID\=##gm')"
+    elif ! command -v lsb_release 1>/dev/null && [ ! -f /etc/os-release ]; then
+        die 4 "Unable to identify distribution since command 'lsb_release' and file /etc/os-release are not present"
+    else
+        die 5 "Failed to identify distro in $0 running logic for Linux"
+    fi
+
+    # ... and the release number
+   if command -v lsb_release 1>/dev/null; then
+        RELEASE="$(lsb_release -rs)"
+    elif [ -f /etc/os-release ]; then
+        RELEASE=$(awk -F= '/VERSION_ID/ {print $2}' /etc/os-release |sed s/\"//g)
+    else
+        die 6 "Unable to determine release"
+    fi
+else
+    die 3 "Sorry, can't configure for $KERNEL systems (yet)."
+fi
+
+# We have distro and release, let's get to work
+
+case "$DISTRO" in
+    debian|ubuntu)
+        if [ "$DISTRO" = "ubuntu" ] && [ $RELEASE -ge 20 ]; then
+            LIBWXDEV="ibwxgtk3.0-gtk3-dev"
+        else
+            LIBWXDEV="ibwxgtk3.0-dev"
+        fi
+        apt-get install -qy cmake fakeroot g++ gettext git libgtest-dev \
+            libcurl4-openssl-dev libqrencode-dev  libssl-dev libuuid1 \
+            $LIBWXDEV libxerces-c-dev libxt-dev libxtst-dev \
+            libykpers-1-dev libyubikey-dev make pkg-config uuid-dev zip \
+            libmagic-dev
+        # ? Do we still need this ?
+        #cd /usr/src/gtest
+        #mkdir build
+        #cd build
+        #cmake ..
+    ;;
+    fedora)
+        dnf install cmake file-devel gcc-c++ git gtest-devel libXt-devel libXtst-devel \
+        libcurl-devel libuuid-devel libyubikey-devel \
+        make openssl-devel rpmdevtools wxGTK3-devel xerces-c-devel \
+        ykpers-devel qrencode-devel
+    ;;
+    *) die 10 "Don't know how to setup $DISTRO release $RELEASE (yet)."
+esac
+exit 0
