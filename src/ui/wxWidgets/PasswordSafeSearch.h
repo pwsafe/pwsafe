12581ddea01d2e1759eebab6f68213cd3b9c108f
ronys <ronys@users.noreply.github.com>
2021-03-24 21:07:20 +0200

    Update PasswordSafeSearch.h
    Mark IsModified() as const

--- src/ui/wxWidgets/PasswordSafeSearch.h
+++ src/ui/wxWidgets/PasswordSafeSearch.h
@@ -130,7 +130,7 @@ private:
   void UpdateStatusAreaWidth();
 
   void SetModified(bool modified) { m_modified = modified; }
-  bool IsModified() { return m_modified; }
+  bool IsModified() const { return m_modified; }
   
   template <class Iter, class Accessor>
   void OnDoSearchT( Iter begin, Iter end, Accessor afn);
