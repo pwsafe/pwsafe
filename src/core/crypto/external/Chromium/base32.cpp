880583244d91137d6d565f6adcb48109f1abf14a
Ashley R. Thomas <ashley.r.thomas.701@gmail.com>
2023-10-06 06:54:04 -0700

    Fix Base32 read overrun, get latest from 'main' * One byte base32 read overrun triggering address sanitizer failures.   https://issuetracker.google.com/issues/303736599 * Get latest EC Base32 from its 'main' (formatting/LICENSE changes, no   functional changes).

--- src/core/crypto/external/Chromium/base32.cpp
+++ src/core/crypto/external/Chromium/base32.cpp
@@ -1,12 +1,11 @@
-/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+/* Copyright 2017 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
  /*
  * This basis for this source file was originally obtained from the following URL:
- * https://chromium.googlesource.com/chromiumos/platform/ec/+/master/common/base32.c
- *
+ * https://chromium.googlesource.com/chromiumos/platform/ec/+/refs/heads/main/common/base32.c
  */
 
 #include <ctype.h>
@@ -28,6 +27,7 @@ uint8_t crc5_sym(uint8_t sym, uint8_t previous_crc)
 	uint8_t tmp = sym ^ previous_crc;
 	return crc5_table1[tmp & 0x0F] ^ crc5_table0[(tmp >> 4) & 0x0F];
 }
+
 /* A-Z0-9 with I,O,0,1 removed */
 // From original Chromium source code:
 //const char base32_map[33] = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
@@ -43,12 +43,15 @@ const char base32_map[33] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
 static int decode_sym(int sym)
 {
 	int i = 0;
+
 	for (i = 0; i < 32; i++) {
 		if (toupper(sym) == base32_map[i])
 			return i;
 	}
+
 	return -1;
 }
+
 bool base32_encode(char *dest, int destlen_chars,
 		  const void *srcbits, int srclen_bits,
 		  int add_crc_every)
@@ -74,21 +77,28 @@ bool base32_encode(char *dest, int destlen_chars,
 		int sym;
 		int sidx = i / 8;
 		int bit_offs = i % 8;
+		int byte_bits_remaining = 8 - bit_offs;
+
 		if (bit_offs <= 3) {
 			/* Entire symbol fits in that byte */
 			sym = src[sidx] >> (3 - bit_offs);
 		} else {
 			/* Use the bits we have left */
-			sym = src[sidx] << (bit_offs - 3);
+			sym = src[sidx] << (bit_offs - 3); /* This uses byte_bits_remaining bits. */
+
 			/* Use the bits from the next byte, if any */
-			if (i + 1 < srclen_bits)
+			if (i + byte_bits_remaining < srclen_bits)
 				sym |= src[sidx + 1] >> (11 - bit_offs);
 		}
+
 		sym &= 0x1f;
+
 		/* Pad incomplete symbol with 0 bits */
 		if (srclen_bits - i < 5)
 			sym &= 0x1f << (5 + i - srclen_bits);
+
 		dest[didx++] = base32_map[sym];
+
 		/* Add CRC if needed */
 		if (add_crc_every) {
 			crc = crc5_sym(static_cast<uint8_t>(sym), static_cast<uint8_t>(crc));
@@ -98,22 +108,28 @@ bool base32_encode(char *dest, int destlen_chars,
 			}
 		}
 	}
+
 	/* Terminate string and return */
 	dest[didx] = 0;
 	return true;
 }
+
 bool base32_decode(uint8_t *dest, int destlen_bits, const char *src,
 		  int crc_after_every)
 {
 	int crc = 0, crc_count = 0;
 	int out_bits = 0;
+
 	for (; *src; src++) {
 		int sym, sbits, dbits, b;
+
 		if (isspace(*src) || *src == '-')
 			continue;
+
 		sym = decode_sym(*src);
 		if (sym < 0)
 			return false;  /* Bad input symbol */
+
 		/* Check CRC if needed */
 		if (crc_after_every) {
 			if (crc_count == crc_after_every) {
@@ -126,30 +142,35 @@ bool base32_decode(uint8_t *dest, int destlen_bits, const char *src,
 				crc_count++;
 			}
 		}
+
 		/*
 		 * Stop if we're out of space.  Have to do this after checking
 		 * the CRC, or we might not check the last CRC.
 		 */
 		if (out_bits >= destlen_bits)
 			break;
+
 		/* See how many bits we get to use from this symbol */
 		sbits = std::min(5, destlen_bits - out_bits);
 		if (sbits < 5)
 			sym >>= (5 - sbits);
+
 		/* Fill up the rest of the current byte */
 		dbits = 8 - (out_bits & 7);
 		b = std::min(dbits, sbits);
 		if (dbits == 8)
-			dest[out_bits / 8] = 0;  /* Starting a new byte */
+			dest[out_bits / 8] = 0; /* Starting a new byte */
 		dest[out_bits / 8] |= (sym << (dbits - b)) >> (sbits - b);
 		out_bits += b;
 		sbits -= b;
+
 		/* Start the next byte if there's space */
 		if (sbits > 0) {
 			dest[out_bits / 8] = static_cast<uint8_t>(sym << (8 - sbits));
 			out_bits += sbits;
 		}
 	}
+
 	/* If we have CRCs, should have a full group */
 	if (crc_after_every && crc_count)
 		return false;
