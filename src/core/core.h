2058ff70b96a8c0b636af80ec90ba19bf24d5f1e
Ashley R. Thomas <ashley.r.thomas.701@gmail.com>
2023-12-16 11:09:55 -0800

    Use "Authentication Secret" in UI.

--- src/core/core.h
+++ src/core/core.h
@@ -77,10 +77,11 @@
 #define IDSC_FLDNMCCVV                  3077
 #define IDSC_FLDNMCCPIN                 3078
 #define IDSC_FLDNMTWOFACTORKEY          3079
-#define IDSC_FLDNMTOTPCONFIG            3080
-#define IDSC_FLDNMTOTPLENGTH            3081
-#define IDSC_FLDNMTOTPTIMESTEP          3082
-#define IDSC_FLDNMTOTPSTARTTIME         3083
+#define IDSC_FLDNMTWOFACTORKEY_UI       3080
+#define IDSC_FLDNMTOTPCONFIG            3081
+#define IDSC_FLDNMTOTPLENGTH            3082
+#define IDSC_FLDNMTOTPTIMESTEP          3083
+#define IDSC_FLDNMTOTPSTARTTIME         3084
 
 #define IDSC_CONFIG_NONE                3230
 #define IDSC_CONFIG_REGISTRY            3231
