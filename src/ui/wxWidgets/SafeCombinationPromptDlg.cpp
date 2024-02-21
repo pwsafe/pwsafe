1caa429fbfc9e2903f7edfad00f7a8ce0ddcbee1
igor2x <igor2x@gmail.com>
2024-02-21 19:35:50 +0100

    Incorrect master password, add what is the problem

--- src/ui/wxWidgets/SafeCombinationPromptDlg.cpp
+++ src/ui/wxWidgets/SafeCombinationPromptDlg.cpp
@@ -286,7 +286,7 @@ void SafeCombinationPromptDlg::ProcessPhrase()
       errmess = _("Three strikes - yer out!");
     } else {
       m_tries++;
-      errmess = _("Most commont issues:\n - incorrect master password,\n - not a Password Safe database,\n - a corrupt database, or\n - backup database has been opened instead of original, ending with '~' character.");
+      errmess = _("Password database can't be opened.\n\nMost commont issues:\n - incorrect master password,\n - not a Password Safe database,\n - a corrupt database, or\n - backup database has been opened instead of original, ending with '~' character.");
     }
     wxMessageDialog err(this, errmess,
                         _("Error"), wxOK | wxICON_EXCLAMATION);
