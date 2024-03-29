58e92de94ab771cf80187fa37d06c2cb30259757
igor2x <igor2x@gmail.com>
2024-03-29 20:47:59 +0100

    Change dialog title, remove word Sorry and unify the same error with the same message

--- src/ui/wxWidgets/PasswordPolicyDlg.cpp
+++ src/ui/wxWidgets/PasswordPolicyDlg.cpp
@@ -679,8 +679,8 @@ void PasswordPolicyDlg::OnPronouceableCBClick( wxCommandEvent& event )
     // Check if ezread is also set - forbid both
     if (m_pwpEasyCtrl->GetValue()) {
       m_pwpPronounceCtrl->SetValue(false);
-      wxMessageBox(_("Sorry, \"pronounceable\" and \"easy-to-read\" cannot be both selected"),
-                   _("Error"), wxOK|wxICON_ERROR, this);
+      wxMessageBox(_("\"Pronounceable\" and \"easy-to-read\" cannot be both selected"),
+                   _("Duplicate selection"), wxOK|wxICON_ERROR, this);
       return;
     }
   }
@@ -698,8 +698,8 @@ void PasswordPolicyDlg::OnEZreadCBClick( wxCommandEvent& event )
     // Check if pronounceable is also set - forbid both
     if (m_pwpPronounceCtrl->GetValue()) {
       m_pwpEasyCtrl->SetValue(false);
-      wxMessageBox(_("Sorry, \"easy-to-read\" and \"pronounceable\" cannot be both selected"),
-                   _("Error"), wxOK|wxICON_ERROR, this);
+      wxMessageBox(_("\"Pronounceable\" and \"easy-to-read\" cannot be both selected"),
+                   _("Duplicate selection"), wxOK|wxICON_ERROR, this);
       return;
     }
   }
