0d751f212a8c6d6cb3f10bc9288fb6ebcd6bef7a
ronys <ronys@pwsafe.org>
2024-05-19 20:47:49 +0300

    [GH1120] Apply to Master Password Setup Dialog

--- src/ui/Windows/PasskeySetup.cpp
+++ src/ui/Windows/PasskeySetup.cpp
@@ -133,7 +133,12 @@ void CPasskeySetup::OnOK()
 #ifndef PWS_FORCE_STRONG_PASSPHRASE
     cs_text.LoadString(IDS_USEITANYWAY);
     cs_msg += cs_text;
-    INT_PTR rc = gmb.AfxMessageBox(cs_msg, NULL, MB_YESNO | MB_ICONSTOP);
+    std::vector<std::tuple<int, int>> tuples = {
+      std::make_tuple(IDCANCEL, IDS_CANCEL),
+      std::make_tuple(IDYES, IDS_USEANYWAY)
+    };
+    INT_PTR rc = gmb.AfxMessageBox(cs_msg, nullptr, tuples, 0, MB_ICONSTOP);
+
     if (rc == IDNO)
       return;
 #else
