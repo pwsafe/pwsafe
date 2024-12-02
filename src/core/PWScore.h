e8a89841ba7e30cdd0d526c329a5eabd1d4e639e
ronys <ronys@pwsafe.org>
2024-12-02 21:03:40 +0200

    minor cleanups to PWScore::ParseAliasPassword()

--- src/core/PWScore.h
+++ src/core/PWScore.h
@@ -345,6 +345,8 @@ public:
                               const CItemData::EntryType type);
   // Takes apart a 'special' password into its components:
   bool ParseAliasPassword(const StringX &passwd, BaseEntryParms &pl);
+  // Check an Alias
+  bool PWScore::CheckAliasValidity(const BaseEntryParms& pl, StringX& errmess);
 
   const CItemData *GetBaseEntry(const CItemData *pAliasOrSC) const;
   CItemData *GetBaseEntry(const CItemData *pAliasOrSC);
