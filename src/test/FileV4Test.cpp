205b7c5ec2d8bf7f9e81f357dbb92c12989c937f
Richard Huveneers <richard@huveneers.com>
2025-01-22 18:56:46 +0100

    Use filename extension to detect V4 database

--- src/test/FileV4Test.cpp
+++ src/test/FileV4Test.cpp
@@ -293,7 +293,7 @@ TEST_F(FileV4Test, CoreRWTest)
   EXPECT_EQ(PWSfile::SUCCESS, core.WriteFile(fname.c_str(), PWSfile::V40));
 
   core.ClearDBData();
-  EXPECT_EQ(PWSfile::FAILURE, core.ReadFile(fname.c_str(), L"WrongPassword", true));
+  EXPECT_EQ(PWSfile::WRONG_PASSWORD, core.ReadFile(fname.c_str(), L"WrongPassword", true));
   EXPECT_EQ(PWSfile::SUCCESS, core.ReadFile(fname.c_str(), passkey, true));
   ASSERT_EQ(1U, core.GetNumEntries());
   ASSERT_EQ(1U, core.GetNumAtts());
