032a91819b8606564c1598fdcb2b42866fa77876
ronys <ronys@pwsafe.org>
2025-03-05 19:11:40 +0200

    - Add DMG and PKG support to post-build.sh (requires MACOS_SIGN_IDENTITY)
    - Added step to test deployment. Disabled for now, as dmg isn't signed.

--- Misc/post-build.sh
+++ Misc/post-build.sh
@@ -3,26 +3,38 @@
 # post-build script
 # Meant to be called by cpack after package creation to finish up the build process, but can also work standalone.
 #
-# Currently just signs the created package (deb or rpm, type and name passed as argument) using gpg key.
+# Currently just signs the created Linux package (deb or rpm, type and name passed as argument) using gpg key,
+# or the created Mac OS X package using the Apple Developer ID Installer certificate.
 # Signing key defaults to current project admin, but this can be overrode via PWS_SIGNER and PWS_KEYID
 # environment variables.
+# For Mac OS X, set MACOS_SIGN_IDENTITY to the name of the certificate to use.
 #
 
 SIGNER=${PWS_SIGNER:="ronys@pwsafe.org"}
 KEYID=${PWS_KEYID:="7F2F1BB9"}
 
+MACOS_SIGN_IDENTITY=${MACOS_SIGN_IDENTITY:=""}
+
+
 Usage () {
-    echo "Usage: $0 DEB|RPM package-to-sign"
+    echo "Usage: $0 DEB|RPM|PKG|DMG package-to-sign"
     exit 1
 }
 
 
 [ $# -eq 2 ] || Usage
-[ "$1" = "DEB" ] || [ "$1" = "RPM" ] || Usage
+[ "$1" = "DEB" ] || [ "$1" = "RPM" ] || [ "$1" = "PKG" ] || [ "$1" = "DMG" ] || Usage
 [ -e $2 ] || Usage
 
 TYPE=$1
 PACKAGE=$2
+
+if [ "$TYPE" = "PKG" ] || [ "$TYPE" = "DMG" ]; then
+    if [ -z "$MACOS_SIGN_IDENTITY" ]; then
+        echo "MACOS_SIGN_IDENTITY is not defined. Please set the signing identity."
+        exit 2
+    fi
+fi
 
 case "$TYPE" in
 RPM)
@@ -44,6 +56,25 @@ DEB)
         exit 2
     fi
     $DEBSIGN --sign builder -k $KEYID $PACKAGE
+    ;;
+PKG)
+    if command -v productsign >/dev/null 2>&1; then
+        PRODUCTSIGN=$(command -v productsign)
+    else
+        echo "Couldn't find productsign"
+        exit 2
+    fi
+    $PRODUCTSIGN --sign "$MACOS_SIGN_IDENTITY" $PACKAGE signed-$PACKAGE
+    mv signed-$PACKAGE $PACKAGE
+    ;;
+DMG)
+    if command -v codesign >/dev/null 2>&1; then
+        CODESIGN=$(command -v codesign)
+    else
+        echo "Couldn't find codesign"
+        exit 2
+    fi
+    $CODESIGN --sign "$MACOS_SIGN_IDENTITY" $PACKAGE
     ;;
 *)
     Usage #Should have been caught earlier...
