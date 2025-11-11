ab620cf62358d127739e3ad41b2fc36ef18424bd
ronys <ronys@pwsafe.org>
2025-11-11 19:10:58 +0200

    Indicate V3 attachment in tree view
    Also replace protected indication '#' with unicode padlock

--- src/ui/wxWidgets/TreeCtrl.cpp
+++ src/ui/wxWidgets/TreeCtrl.cpp
@@ -570,13 +570,18 @@ wxString TreeCtrlBase::ItemDisplayString(const CItemData &item) const
   }
 
   if (item.IsProtected()) { 
-    disp += wxT(" #");
-#ifdef NOTYET
-    wxUniChar padlock(0x1f512);
-    disp +=padlock;
-#endif
+    // disp += wxT(" #");
+
+    const wxUniChar padlock(0x1f512);
+    disp += padlock;
   }
 
+  if (item.HasAttRef() || item.HasAttachment())
+  {
+    const wxUniChar paperclip(0x1f4ce);
+    disp += paperclip;
+  }
+  
   return disp;
 }
 
