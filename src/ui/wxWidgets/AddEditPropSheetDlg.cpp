f7e6ae43b773b4ffa826dbd1ffdc9eb299ef8826
froggie2523 <milan@zabka.top>
2026-04-22 06:16:40 +0200

    Address review feedback and update English Help

--- src/ui/wxWidgets/AddEditPropSheetDlg.cpp
+++ src/ui/wxWidgets/AddEditPropSheetDlg.cpp
@@ -455,11 +455,11 @@ wxPanel* AddEditPropSheetDlg::CreateAdditionalPanel()
   mainSizer->Add(vBoxSizer, 1, wxEXPAND|wxALL, 10);
 
   auto *itemStaticText41 = new wxStaticText(panel, wxID_STATIC, _("Autotype"), wxDefaultPosition, wxDefaultSize, 0);
-  wxUtilities::DisableIfUnsupported(wxUtilities::Feature::Autotype, itemStaticText41);
+  wxUtilities::NotifyIfUnsupported(wxUtilities::Feature::Autotype, itemStaticText41);
   vBoxSizer->Add(itemStaticText41, 0, wxALIGN_LEFT|wxBOTTOM, 5);
 
   auto *itemTextCtrl42 = new wxTextCtrl(panel, ID_TEXTCTRL_AUTOTYPE, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
-  wxUtilities::DisableIfUnsupported(wxUtilities::Feature::Autotype, itemTextCtrl42);
+  wxUtilities::NotifyIfUnsupported(wxUtilities::Feature::Autotype, itemTextCtrl42);
   vBoxSizer->Add(itemTextCtrl42, 0, wxALIGN_LEFT|wxEXPAND|wxBOTTOM, 12);
 
   auto *itemStaticText43 = new wxStaticText(panel, wxID_STATIC, _("Run Command"), wxDefaultPosition, wxDefaultSize, 0);
