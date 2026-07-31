4e1f8657c9c5b8ffd08a271efb0d05dd803a85c3
ykne <6250736+ykne@users.noreply.github.com>
2026-07-30 22:42:35 -0400

    Misc: modernize mock-build.sh
    - Drop CentOS-7/devtoolset-7 special-casing; target current Fedora only
    - Add -v WXVERSION option to pin a specific installed wxGTK version
      instead of always using the latest, copying matching wx* RPMs into
      the mock chroot and force-installing them
    - Clone from the current git remote/branch when run inside a repo,
      falling back to the upstream pwsafe.git otherwise
    - Build with cmake --build/cpack directly instead of a fixed CentOS
      toolchain invocation; disable gtest in the mock build
    - Add libayatana-appindicator-gtk3-devel to the installed build deps
    - Mark the script executable

--- Misc/mock-build.sh
+++ Misc/mock-build.sh
@@ -4,18 +4,76 @@
 # With no argument compiles for current system.
 # If added -r mock_arch, where mock_arch is a file name in /etc/mock without .cfg,
 # then rpm will be compiled on specified architecture.
-# Tested on Fedora-30 and CentOS-7
-# Build on or for CentOS-7 requires current mock* for devtoolset-7-gcc-c++ access.
-#
-# To install the resulting RPM on CentOS7, the https://fedoraproject.org/wiki/EPEL repository
-# should be installed.
+# Use -v WXVERSION to pin a specific wxGTK version instead of the latest.
+# Tested on Fedora
+
+usage() {
+    echo "Usage: $0 [-v WXVERSION] [mock options]" >&2
+    echo "  -v WXVERSION  Install this exact wxGTK version instead of the latest" >&2
+    exit 1
+}
+
+WANT_VERSION=""
+
+while getopts ":v:" opt; do
+    case $opt in
+        v) WANT_VERSION="$OPTARG" ;;
+        :) echo "ERROR: -$OPTARG requires an argument" >&2; usage ;;
+        \?) break ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+CLONE_URL="https://github.com/pwsafe/pwsafe.git"
+CLONE_OPTS=""
+RPMDIR=$(rpm --eval '%_rpmdir')
+RPMARCH=$(rpm --eval '%_arch')
+DIST=$(rpm --eval '%{dist}' | sed 's/^\.//')
+
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    UPSTREAM=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+    if [ -n "$UPSTREAM" ]; then
+        REMOTE=$(echo "$UPSTREAM" | cut -d/ -f1)
+        CLONE_URL=$(git remote get-url "$REMOTE")
+        CLONE_OPTS="--branch $BRANCH --single-branch"
+    fi
+fi
 
 mock --init "$@"
-if [ "$2" = epel-7-x86_64 -o \( -z "$2" -a $(readlink /etc/mock/default.cfg) = epel-7-x86_64.cfg \) ];then
-    mock "$@" install cmake gcc-c++ git gtest-devel libXt-devel libXtst-devel libcurl-devel libuuid-devel libyubikey-devel make openssl-devel wxGTK3-devel xerces-c-devel ykpers-devel qrencode-devel file-devel file-devel redhat-lsb-core devtoolset-7-gcc-c++
-    mock "$@" --enable-network --chroot 'su - mockbuild -c "git clone https://github.com/pwsafe/pwsafe.git; mkdir -p pwsafe/build; cd pwsafe/build; scl enable devtoolset-7 -- cmake -D wxWidgets_CONFIG_EXECUTABLE=/usr/libexec/wxGTK3/wx-config ..; scl enable devtoolset-7 -- cpack -G RPM"'
+mock "$@" install cmake dpkg gcc-c++ git libXt-devel libXtst-devel libcurl-devel libuuid-devel libyubikey-devel make openssl-devel wxGTK-devel xerces-c-devel ykpers-devel qrencode-devel file-devel ImageMagick libayatana-appindicator-gtk3-devel
+
+latest_rpm() {
+    ls -1v "$@" 2>/dev/null | tail -n1
+}
+
+if [ -n "$WANT_VERSION" ]; then
+    VER_GLOB="${WANT_VERSION}-*"
 else
-    mock "$@" install cmake gcc-c++ git gtest-devel libXt-devel libXtst-devel libcurl-devel libuuid-devel libyubikey-devel make openssl-devel wxGTK3-devel xerces-c-devel ykpers-devel qrencode-devel file-devel file-devel redhat-lsb-core
-    mock "$@" --enable-network --chroot 'su - mockbuild -c "git clone https://github.com/pwsafe/pwsafe.git; mkdir -p pwsafe/build; cd pwsafe/build; cmake ..; cpack -G RPM"'
+    VER_GLOB="[0-9]*"
 fi
+
+if ls "$RPMDIR/$RPMARCH"/wxGTK-${VER_GLOB}."$DIST.$RPMARCH.rpm" >/dev/null 2>&1; then
+    WXBASE=$(latest_rpm "$RPMDIR/$RPMARCH"/wxBase-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+    WXBASE_DEVEL=$(latest_rpm "$RPMDIR/$RPMARCH"/wxBase-devel-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+    WXGTK=$(latest_rpm "$RPMDIR/$RPMARCH"/wxGTK-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+    WXGTK_DEVEL=$(latest_rpm "$RPMDIR/$RPMARCH"/wxGTK-devel-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+    WXGTK_GL=$(latest_rpm "$RPMDIR/$RPMARCH"/wxGTK-gl-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+    WXGTK_I18N=$(latest_rpm "$RPMDIR/noarch"/wxGTK-i18n-${VER_GLOB}."$DIST.noarch.rpm")
+    WXGTK_MEDIA=$(latest_rpm "$RPMDIR/$RPMARCH"/wxGTK-media-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+    WXGTK_WEBVIEW=$(latest_rpm "$RPMDIR/$RPMARCH"/wxGTK-webview-${VER_GLOB}."$DIST.$RPMARCH.rpm")
+
+    mock "$@" --copyin \
+        "$WXBASE" \
+        "$WXBASE_DEVEL" \
+        "$WXGTK" \
+        "$WXGTK_DEVEL" \
+        "$WXGTK_GL" \
+        "$WXGTK_I18N" \
+        "$WXGTK_MEDIA" \
+        "$WXGTK_WEBVIEW" \
+        /tmp/
+    mock "$@" --chroot "rpm -Uvh --force /tmp/wxBase-3*.rpm /tmp/wxBase-devel*.rpm /tmp/wxGTK-3*.rpm /tmp/wxGTK-devel*.rpm /tmp/wxGTK-gl*.rpm /tmp/wxGTK-i18n*.rpm /tmp/wxGTK-media*.rpm /tmp/wxGTK-webview*.rpm"
+fi
+mock "$@" --enable-network --unpriv --chroot "cd /builddir && git clone $CLONE_OPTS $CLONE_URL && mkdir -p pwsafe/build && cd pwsafe/build && cmake .. -DNO_GTEST=ON && cmake --build . -j\$(nproc) && cpack -G RPM"
 mock "$@" --copyout '/builddir/pwsafe/build/passwordsafe*.rpm' .
